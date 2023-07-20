b9b2b0c2f356742d4c5085acfb6da6ffd10dc0e7
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-07-20 22:07:02 +0300

    Добавлен пример char.

---
+++ Education/example/char.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <type_traits>
+
+int main()
+{
+	std::cout << std::boolalpha << std::is_signed_v < char > << std::endl;
+
+	for (int i = -128; i < 128; ++i) // for (int i = 0; i < 256; ++i)
+	{
+		std::cout << static_cast < char > (i) << '\n';
+	}
+
+	auto c = '8';
+
+	std::cout << c - '0' << std::endl;
+
+	system("pause");
+
+	return 0;
+}
