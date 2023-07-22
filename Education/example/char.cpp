e3d0805cfddce7882ae0f0b91246769fa528a8fb
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-07-22 23:25:36 +0300

    Доработан пример char.

--- Education/example/char.cpp
+++ Education/example/char.cpp
@@ -7,9 +7,17 @@ int main()
 
 	std::cout << std::boolalpha << std::is_signed_v < char > << std::endl;
 
+	signed   char c_1 = 'a';
+	unsigned char c_2 = 'b';
+
 	for (int i = -128; i < 128; ++i) // for (int i = 0; i < 256; ++i)
 	{
 		std::cout << static_cast < char > (i) << '\n';
+	}
+
+	for (char c = ' '; c <= '~'; ++c)
+	{
+		std::cout << c << '\n';
 	}
 
 	auto c = '8';
