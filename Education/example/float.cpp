cf6a1516b120983d0396b374792a1a780b891314
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-07-24 16:56:05 +0300

    Добавлен пример float.

--- Education/example/float.cpp
+++ Education/example/float.cpp
@@ -6,6 +6,12 @@ int main()
 	std::cout << std::numeric_limits < int > ::min() << std::endl;
 	std::cout << std::numeric_limits < int > ::max() << std::endl;
 
+	std::cout << 3.14 << std::endl;
+
+	auto f = 3.14f;
+
+
+
 	system("pause");
 
 	return 0;
