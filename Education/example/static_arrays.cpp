4f3f589ca390cc8899142450554116a87913c1fc
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-08-04 00:53:08 +0300

    Добавлен пример static_arrays.

---
+++ Education/example/static_arrays.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int main()
+{
+	const std::size_t size = 1024;
+
+	int a[size]{};
+	int b[size]{1, 2, 3};
+	int c[]{1, 2, 3};
+
+	std::size_t n;
+
+	std::cin >> n;
+
+	if (n <= size)
+	{
+		for (std::size_t i = 0; i < n; ++i)
+		{
+			std::cin >> a[i];
+		}
+
+		for (std::size_t i = 0; i < n - 1; ++i)
+		{
+			for (std::size_t j = i + 1; j < n; ++j)
+			{
+				if (a[i] > a[j])
+				{
+					std::swap(a[i], a[j]);
+				}
+			}
+		}
+
+		for (std::size_t i = 0; i < n; ++i)
+		{
+			std::cout << a[i] << ' ';
+		}
+	}
+
+	system("pause");
+
+	return 0;
+}
