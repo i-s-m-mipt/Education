831862d99d304647dc4921ff0d780f780a83ae57
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-08-03 21:28:47 +0300

    Добавлен пример pointers_void.

---
+++ Education/example/pointers_void.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void* allocate(std::size_t n)
+{
+	// ...
+}
+
+int main()
+{
+	auto x = 0;
+	auto ptr_x = &x;
+
+	void* ptr = ptr_x;
+
+	auto ptr_object = static_cast < int* > (ptr);
+
+	const std::size_t n = 10;
+
+	auto ptr_ints = static_cast < int* > (allocate(n * sizeof(int)));
+
+	system("pause");
+
+	return 0;
+}
