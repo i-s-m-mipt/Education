a110300df07bfa28453b758c550b7be1df87a531
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-08-05 19:09:10 +0300

    Добавлен пример new_delete.

---
+++ Education/example/new_delete.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+	const std::size_t size = 10;
+
+	auto ptr_int       = new int(42);
+	auto ptr_int_array = new int[size]{};
+
+	++(*ptr_int);
+
+	ptr_int_array[5] = 42;
+
+	std::cout << *ptr_int << std::endl;
+
+	for (std::size_t i = 0; i < size; ++i)
+	{
+		std::cout << ptr_int_array[i] << ' ';
+	}
+
+	delete   ptr_int;
+	delete[] ptr_int_array;
+
+	system("pause");
+
+	return 0;
+}
