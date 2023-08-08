1ec712ef4f3a0842ad2a64e60b893a082ff51d3b
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-08-08 23:59:23 +0300

    Добавлен пример structs.

---
+++ Education/example/structs.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct Person
+{
+	std::string name = "default";
+	std::size_t age  = 0;
+};
+
+struct Node;
+
+struct Link
+{
+	Node * left;
+	Node * right;
+
+	// ...
+};
+
+struct Node
+{
+	// ...
+};
+
+struct Point
+{
+	double x = 0.0;
+	double y = 0.0;
+};
+
+int main()
+{
+	Person p_1;
+	Person p_2 = { "Matthias", 27 };
+
+	p_1.name = "Matthias";
+
+	auto ptr = &p_1;
+
+	ptr->age = 28;
+
+	std::vector < Point > points =
+	{
+		{1.0, 1.0},
+		{2.0, 2.0},
+		{3.0, 3.0}
+	};
+
+	return 0;
+}
