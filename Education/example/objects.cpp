4bd66c428d8fa0262c96a2da73463f4488780672
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-09-06 18:34:48 +0300

    Добавлен пример objects.

---
+++ Education/example/objects.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	bool   variable_1 = true;
+	char   variable_2 = 'a';
+	int    variable_3 = 42;
+	double variable_4 = 3.14;
+
+	const double pi = 3.14;
+
+	return 0;
+}
