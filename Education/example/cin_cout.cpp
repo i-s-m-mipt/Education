64f3f6798822fb7debeff3f54f7ff070f4d0e724
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-09-08 19:48:42 +0300

    Доработан пример cin_cout.

--- Education/example/cin_cout.cpp
+++ Education/example/cin_cout.cpp
@@ -5,9 +5,11 @@ int main()
 	int x = 0;
 	int y = 0;
 
+	std::cout << "Enter two numbers separated by a space symbol: ";
+
 	std::cin >> x >> y;
 
-	std::cout << x + y << '\n';
+	std::cout << "You entered " << x << " and " << y << '\n';
 
 	return 0;
 }
