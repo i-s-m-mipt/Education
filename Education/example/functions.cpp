d6706929d34129d320bc2cae62ef924e01094abb
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-09-09 02:10:43 +0300

    Доработан пример functions.

--- Education/example/functions.cpp
+++ Education/example/functions.cpp
@@ -1,45 +1,63 @@
 #include <iostream>
 
+[[nodiscard]] int f(int x);
+
+[[nodiscard]] int g(int x, int y, int z = 1)
+{
+	return (x + y + z);
+}
+
+void h()
+{
+	auto x = 0;
+
+	static auto y = 0;
+
+	std::cout << x++ << ' ' << y++ << '\n';
+
+	return;
+}
+
+void print(bool x)
+{
+	std::cout << x << '\n';
+}
+
+void print(double x)
+{
+	std::cout << x << '\n';
+}
+
 int factorial(int n)
 {
 	return (n < 2 ? 1 : n * factorial(n - 1));
 }
 
-void print(int x)
-{
-	std::cout << x << '\n';
-
-	return;
-}
-
-[[nodiscard]] int f(int x);
-
-int max(int x, int y)
+inline auto max(int x, int y)
 {
 	return (x > y ? x : y);
 }
 
-void g(int x)
-{
-	int y = 42;
-}
-
 int main()
 {
+	[[maybe_unused]] auto result = g(f(4), f(7));
+
+	h(); 
+	h(); 
+	h();
+
+	print(true);
+	print(3.14);
+//	print(1234); // error
+
 	std::cout << factorial(5) << '\n';
 
-	print(42);
-
-	f(1); // warning
-
-	std::cout << max(13, 19) << '\n';
-
-	std::cout << max(2 + 5, 3 + 6) << '\n';
+	std::cout << max(4, 7) << '\n';
 
 	return 0;
 }
 
 int f(int x)
 {
-	return x + 42;
+	return (x + 1);
 }
