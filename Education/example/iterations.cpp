c8987671bf6e690c17ade42e219830db0375f043
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-09-09 00:50:15 +0300

    Добавлен пример iterations.

---
+++ Education/example/iterations.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+
+int main()
+{
+	const auto size = 5;
+
+	for (auto i = 0; i < size; ++i)
+	{
+		std::cout << "for 1: " << i << '\n';
+	}
+
+	for (auto i = 0, j = 0; i < size; ++i, j += 2)
+	{
+		std::cout << "for 2: " << i << ' ' << j << '\n';
+	}
+
+	for (auto i = 0; auto x = (i + 1) % size; ++i)
+	{
+		std::cout << "for 3: " << i << '\n';
+	}
+
+	for (auto i = 0; i < size; ++i)
+	{
+		if (i % 2 == 0)
+		{
+			continue;
+		}
+
+		std::cout << "for 4: " << i << '\n';
+	}
+
+	for (auto i = 0; i < size; ++i)
+	{
+		std::cout << "for 5: ";
+
+		for (auto j = 0; j < size; ++j)
+		{
+			if (j > i)
+			{
+				break;
+			}
+
+			std::cout << j << ' ';
+		}
+
+		std::cout << '\n';
+	}
+
+	for (auto i = 0; i < size; ++i)
+	{
+		for (auto j = 0; j < size; ++j)
+		{
+			for (auto k = 0; k < size; ++k)
+			{
+				std::cout << "for 6: " << i << ' ' << j << ' ' << k << '\n';
+
+				if (auto stop = size / 2; i == stop && j == stop && k == stop)
+				{
+					goto exit;
+				}
+			}
+		}
+	}
+
+	exit:
+
+	char c{};
+
+	while (c != 'n')
+	{
+		std::cout << "Continue (y/n) ? ";
+
+		std::cin >> c;
+	}
+
+	do
+	{
+		std::cout << "Continue (y/n) ? ";
+
+		std::cin >> c;
+	} 
+	while (c != 'n');
+
+	for (;;)
+	{
+		std::cout << "Continue (y/n) ? ";
+
+		std::cin >> c;
+
+		if (c == 'n')
+		{
+			break;
+		}
+	}
+
+	while (true)
+	{
+		std::cout << "Continue (y/n) ? ";
+
+		std::cin >> c;
+
+		if (c == 'n')
+		{
+			break;
+		}
+	}
+
+	return 0;
+}
