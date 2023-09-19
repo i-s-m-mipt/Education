160428197a97e1407432232657d51b2d2b850a54
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-09-19 21:37:50 +0300

    Объединены примеры selections и iterations.

--- Education/example/statements.cpp
+++ Education/example/statements.cpp
@@ -2,6 +2,60 @@
 
 int main()
 {
+    std::cout << "Enter integer value: ";
+
+    int x{}; // good: zero initialized variable for input
+
+    std::cin >> x;
+
+    if (auto y = x * 4 + 10; 42 == y) // good: 42 == y prevents error 42 = y
+    {
+        std::cout << "path 1\n";
+    }
+    else if (y > 0)
+    {
+        std::cout << "path 2\n";
+    }
+    else
+    {
+        if (x < 0)
+        {
+            std::cout << "path 3\n";
+        }
+        else
+        { 
+            std::cout << "path 4\n";
+        }
+    }
+
+    std::cout << (x > 0 ? x : 42) << '\n'; // good: compact selection
+
+    switch (auto y = x / 4 - 1; y)
+    {
+    [[likely]] case 0: // good: optimally generated machine code
+    [[likely]] case 1:
+    [[likely]] case 2:
+    {
+        std::cout << "path 1" << std::endl;
+        [[fallthrough]]; // good: suppressed warning, clear intentions
+    }
+    case 3:
+    {
+        std::cout << "path 2" << std::endl;
+        break;
+    }
+    [[unlikely]] case 4: // good: optimally generated machine code
+    {
+        std::cout << "path 3" << std::endl;
+        break;
+    }
+    default: // good: secure code
+    {
+        std::cout << "path 4" << std::endl;
+        break;
+    }
+    }
+
 	const auto size = 5;
 
 	for (auto i = 0; i < size; ++i) // note: clear counter
@@ -14,7 +68,7 @@ int main()
 		std::cout << "for 2: " << i << ' ' << j << '\n';
 	}
 
-//	for (auto i = 0; auto x = (i + 1) % size; ++i) {} // bad: declaration instead of condition
+	//	for (auto i = 0; auto x = (i + 1) % size; ++i) {} // bad: declaration instead of condition
 
 	for (auto i = size; i >= 0; --i)
 	{
@@ -59,7 +113,7 @@ int main()
 		}
 	}
 
-	exit:
+exit:
 
 	char c{};
 
@@ -102,5 +156,5 @@ int main()
 		}
 	}
 
-	return 0;
+    return 0;
 }
