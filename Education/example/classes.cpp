a0e4265fe5120b1c82f3815b50a155c32d5739bc
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-09-19 15:55:13 +0300

    Доработан пример classes.

--- Education/example/classes.cpp
+++ Education/example/classes.cpp
@@ -2,9 +2,21 @@
 
 class Date
 {
-public: // good: public members last
+public: // good: public members first
 
 	using integer_t = int;
+
+public:
+
+	class Printer // note: nested types can’t be forward declared
+	{
+	public:
+
+		void print(const Date & date)
+		{
+			std::cout << date.m_year << '/' << date.m_month << '/' << date.m_day << '\n';
+		}
+	};
 
 public:
 
@@ -17,7 +29,7 @@ public:
 		initialize(); // good: don't repeat yourself
 	}
 
-	Date(integer_t year, integer_t month, integer_t day) : // note: default arguments?
+	Date(integer_t year, integer_t month, integer_t day) : 
 		m_year(year), m_month(month), m_day(day) // note: member initialization order
 	{
 		initialize();
@@ -28,16 +40,26 @@ public:
 		initialize();
 	}
 
-public:
-
-	void initialize()
+	~Date()
 	{
-		// ...
+		uninitialize();
 	}
 
 public:
 
-	void print_v1() const // note: defined in class is inline by default
+	void initialize()
+	{
+		++counter;
+	}
+
+	void uninitialize()
+	{
+		--counter;
+	}
+
+public:
+
+	void print_v1() const // note: small function is defined in the class as inline
 	{
 		std::cout << m_year << '/' << m_month << '/' << m_day << '\n';
 	}
@@ -60,7 +82,7 @@ public:
 
 	void set_year(integer_t year) // good: setter with additional actions
 	{
-		m_year = (year < 0 ? 0 : year > 9999 ? 9999 : year);
+		m_year = (year < 0 ? 0 : year > max_year ? max_year : year);
 	}
 
 	void set_year_from_date(const Date & date)
@@ -68,14 +90,29 @@ public:
 		set_year(date.m_year); // note: partial assignment
 	}
 
+public:
+
+	static auto get_counter()
+	{
+		return counter;
+	}
+
+public:
+
+	static inline const int max_year = 9999; // note: common for all instances
+
+private:
+
+	static inline int counter = 0;
+
 private: // good: private members last
 
-	integer_t m_year  = 0; // good: name begins with m_
+	integer_t m_year  = 0; // good: name begins with m_ prefix
 	integer_t m_month = 0;
 	integer_t m_day   = 0;
 };
 
-void Date::print_v2() const
+void Date::print_v2() const // good: large function is defined outside the class
 {
 	const auto separator = '/';
 
@@ -94,6 +131,58 @@ void Date::print_v2() const
 	std::cout << m_day << '\n';
 }
 
+class A
+{
+public:
+
+	friend void print(const A & a); 
+
+	friend class B;
+
+private:
+
+	int m_data = 42;
+};
+
+void print(const A & a)
+{
+	std::cout << a.m_data << '\n';
+}
+
+class B; // note: forward declaration required
+
+class C
+{
+public:
+
+	void print(const B & b) const;
+
+private:
+
+	int m_data = 42;
+};
+
+class B
+{
+public:
+
+	void print(const A & a) const
+	{
+		std::cout << a.m_data << '\n';
+	}
+
+	friend void C::print(const B & b) const; // note: friendship is not transitive
+
+private:
+
+	int m_data = 42;
+};
+
+void C::print(const B & b) const
+{
+	std::cout << b.m_data << '\n';
+}
+
 int main()
 {
 	Date date; // note: default constructor
@@ -107,10 +196,27 @@ int main()
 
 	date.set_year(2023);
 
+	const Date c_date;
+
+//	c_date.set_year(2023); // error: non-constant member function
+
 	Date date_1;
 	Date date_2(2023, 19, 9);
 
-//	Date date_3(); // bad: most vexing parse, not an instance of class
+//	Date date_3(); // bad: most vexing parse, not an instance of the class
+
+	Date::Printer printer;
+
+	printer.print(date_2);
+
+	std::cout << date_1.max_year << '\n';
+	std::cout << date_2.max_year << '\n';
+
+	std::cout << Date::max_year << '\n'; // good: preferred syntax
+
+	std::cout << Date::get_counter() << '\n';
+
+	print(A(), B());
 
 	return 0;
 }
