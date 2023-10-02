953544e1879bbecbe2d266297838ca2c13fe416f
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-10-02 17:13:20 +0300

    Добавлен пример rvalue_references.

---
+++ Education/examples/section_03/rvalue_references.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+int get_prvalue() 
+{ 
+	return 42; 
+}
+
+/*
+int && get_xvalue() // warning: returning a reference to a local object
+{ 
+	return 42; 
+}
+*/
+
+int & get_lvalue()
+{
+	static int x = 42;
+
+	return x;
+}
+
+int main()
+{
+	[[maybe_unused]] double d = 0.0;
+
+	const int i = 0;
+
+	// ============================================================================
+
+//	int & lvalue_1 = get_prvalue(); // error: lvalue reference cannot be bound to prvalue
+
+	[[maybe_unused]] int & lvalue_2 = get_lvalue();
+
+//	int & lvalue_3 = 0; // error: lvalue reference cannot be bound to prvalue
+//	int & lvalue_4 = d; // error: lvalue reference cannot be bound to prvalue
+//	int & lvalue_5 = i; // error: lvalue reference cannot be bound to constant lvalue
+
+	// ============================================================================
+
+	[[maybe_unused]] int && rvalue_1 = get_prvalue();
+
+//	int && rvalue_2 = get_lvalue(); // error: rvalue reference cannot be bound to lvalue
+
+	[[maybe_unused]] int && rvalue_3 = 0;
+
+//	int && rvalue_4 = d; // warning: dangerous double to int conversion
+
+//	int && rvalue_5 = i; // error: rvalue reference cannot be bound to lvalue
+
+	// ============================================================================
+	
+	[[maybe_unused]] const int & const_lvalue_1 = get_prvalue();
+	[[maybe_unused]] const int & const_lvalue_2 = get_lvalue ();
+
+	[[maybe_unused]] const int & const_lvalue_3 = 0;
+
+//	const int & const_lvalue_4 = d; // warning: dangerous double to int conversion
+
+	[[maybe_unused]] const int & const_lvalue_5 = i;
+
+	// ============================================================================
+	
+	[[maybe_unused]] const int && const_rvalue_1 = get_prvalue();
+
+//	const int && const_rvalue_3 = get_lvalue(); // error: constant rvalue reference cannot be bound to lvalue
+	
+	[[maybe_unused]] const int && const_rvalue_4 = 0;
+
+//	const int && const_rvalue_5 = d; // warning: dangerous double to int conversion
+
+//	const int && const_rvalue_6 = i; // error: constant rvalue reference cannot be bound to lvalue
+
+	// ============================================================================
+
+	return 0;
+}
