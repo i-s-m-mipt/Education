c748f479ffb6d92dbc53860856de599c620b0342
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-10-02 23:08:18 +0300

    Добавлен пример overload_resolution.

---
+++ Education/examples/section_03/overload_resolution.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+int get_prvalue()
+{
+	return 42;
+}
+
+const int get_const_prvalue()
+{
+	return 42;
+}
+
+/*
+int && get_xvalue() // warning: returning a reference to a local object
+{
+	return 42;
+}
+*/
+
+int & get_lvalue()
+{
+	static int x = 42;
+
+	return x;
+}
+
+const int & get_const_lvalue()
+{
+	static int x = 42;
+
+	return x;
+}
+
+void f([[maybe_unused]] int & arg)
+{
+	std::cout << "int &" << std::endl;
+}
+
+void f([[maybe_unused]] int && arg)
+{
+	std::cout << "int &&" << std::endl;
+}
+
+void f([[maybe_unused]] const int & arg)
+{
+	std::cout << "const int &" << std::endl;
+}
+
+void f([[maybe_unused]] const int && arg)
+{
+	std::cout << "const int &&" << std::endl;
+}
+
+int main()
+{
+	[[maybe_unused]] double d = 0.0;
+
+	const int i = 0;
+
+	f(get_prvalue());
+	f(get_const_prvalue()); // note: verify int && overload
+	f(get_lvalue());
+	f(get_const_lvalue());
+
+	f(0);
+//	f(d); // error: ambiguous function overloading 
+	f(i);
+	
+	return 0;
+}
