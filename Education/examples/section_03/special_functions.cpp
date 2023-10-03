48d1f8ee9bc6b0bcb9a1921c7fecfecc8293c164
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-10-03 03:01:26 +0300

    Доработан пример special_functions.

--- Education/examples/section_03/special_functions.cpp
+++ Education/examples/section_03/special_functions.cpp
@@ -103,6 +103,18 @@ private:
 	size_t   m_size;
 };
 
+Container f()
+{
+	return Container(); // note: copy elision, return value optimization
+}
+
+Container g()
+{
+	Container c = Container(); // note: guaranteed copy elision
+
+	return c; // note: copy elision, named return value optimization
+}
+
 int main()
 {
 	Container c1;
@@ -113,6 +125,9 @@ int main()
 
 	c1 = c3;
 	c1 = Container(10);
+
+	[[maybe_unused]] auto c5 = f(); // note: guaranteed copy elision
+	[[maybe_unused]] auto c6 = g(); // note: guaranteed copy elision
 
 	return 0;
 }
