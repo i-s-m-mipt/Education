3d790b28888b2d8f4fd261981cb299dd459dad34
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-10-14 00:19:53 +0300

    Доработан пример function_templates.

--- Education/examples/section_04/function_templates.cpp
+++ Education/examples/section_04/function_templates.cpp
@@ -36,6 +36,47 @@ auto max_v6(T1 x, T2 y) // good: compact solution
 	return (x < y ? y : x);
 }
 
+template < typename T >
+void f([[maybe_unused]] T value) // note: basic template
+{
+	std::cout << "template for T" << std::endl;
+}
+
+template <>
+void f < int > ([[maybe_unused]] int value) // note: full specialization for int
+{
+	std::cout << "full specialization for int" << std::endl;
+}
+
+void f([[maybe_unused]] char value) // note: overload for char
+{
+	std::cout << "overload for char" << std::endl;
+}
+
+void f([[maybe_unused]] int value) // note: overload for double
+{
+	std::cout << "overload for int" << std::endl;
+}
+
+void f([[maybe_unused]] double value) // note: overload for double
+{
+	std::cout << "overload for double" << std::endl;
+}
+
+template < typename T1, typename T2 >
+void g([[maybe_unused]] T1 x, [[maybe_unused]] T2 y) // note: basic template
+{
+	std::cout << "g for T1, T2" << std::endl;
+}
+
+/*
+template < typename T >
+void g < int, T > ([[maybe_unused]] int x, [[maybe_unused]] T y) // error: prohibited partial specialization
+{
+	std::cout << "g for int, T" << std::endl;
+}
+*/
+
 int main()
 {
 	std::cout << max_v1('a', 'b') << std::endl;
@@ -59,6 +100,14 @@ int main()
 	std::cout << max_v5(100, 2.0) << std::endl;
 
 	std::cout << max_v6(100, 2.0) << std::endl;
+
+	f            ('a'); // note: overload for char
+	f            (100); // note: overload for int
+	f            (1.0); // note: overload for double
+	f <>         (100); // note: full specialization for int
+	f < int >    (100); // note: full specialization for int
+	f <>         (1.0); // note: template for T
+	f < double > (1.0); // note: template for T
 
 	return 0;
 }
