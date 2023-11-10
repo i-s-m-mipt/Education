1a640cdcf2c9108bab57406ce2a5c6c1636da8b0
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-11-10 23:18:36 +0300

    Добавлены примеры по CRTP.

---
+++ examples/section_05/crtp_concept.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+template < class Derived > class Shape
+{
+public:
+
+	void draw() const
+	{
+		static_cast < const Derived * > (this)->draw_implementation();
+	}
+
+}; // template < class Derived > class Shape
+
+class Triangle : public Shape < Triangle >
+{
+public:
+
+	void draw_implementation() const
+	{
+		std::cout << "triangle" << std::endl;
+	}
+
+}; // class Triangle : public Shape < Triangle >
+
+template < typename T > void handle(const T & t)
+{
+	t.draw();
+}
+
+int main()
+{
+	handle(Triangle());
+
+	return 0;
+}
---
+++ examples/section_05/crtp_counter.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+template < typename T > class Counter
+{
+protected:
+
+	Counter()                             { ++m_counter; }
+	Counter(const Counter < T > &  other) { ++m_counter; }
+	Counter(      Counter < T > && other) { ++m_counter; }
+
+	~Counter() { --m_counter; }
+
+public:
+
+	static auto counter()
+	{
+		return m_counter;
+	}
+
+private:
+
+	static inline auto m_counter = 0;
+
+}; // template < typename T > class Counter
+
+template < typename T >
+class Container_1 : public Counter < Container_1 < T > > {};
+
+template < typename T >
+class Container_2 : public Counter < Container_2 < T > > {};
+
+int main()
+{
+	Container_1 < int > ci_1;
+	Container_1 < int > ci_2(ci_1);
+
+	Container_2 < int > ci_3;
+
+	std::cout << Container_1 < int > ::counter() << std::endl;
+	std::cout << Container_2 < int > ::counter() << std::endl;
+
+	return 0;
+}
