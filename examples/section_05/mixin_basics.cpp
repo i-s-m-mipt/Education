55a6371a9b4e8f222b9013de423bc7571c967e12
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-11-11 14:26:56 +0300

    Доработан пример mixin_basics.

--- examples/section_05/mixin_basics.cpp
+++ examples/section_05/mixin_basics.cpp
@@ -24,9 +24,34 @@ public:
 
 }; // template < typename Base > class Mixin_2 : public Base 
 
+class Color {};
+class Label {};
+
+template < typename ... Bases > class Point : private Bases... // note: modern mixin
+{
+public:
+
+    explicit Point(double x, double y) : Bases()..., m_x(x), m_y(y)
+    {
+        std::cout << sizeof...(Bases) << std::endl;
+    }
+
+private:
+
+    double m_x;
+    double m_y;
+
+}; // template < typename T, typename ... Bases > class Point : private Bases...
+
 int main() 
 {
-    Mixin_2 < Mixin_1 < Base > > ().h();
+    Mixin_2 < Mixin_1 < Base > > mixin; // note: old style mixin
+    
+    mixin.h();
+
+    [[maybe_unused]] Point < Color, Label > p1(1.0, 1.0);
+    [[maybe_unused]] Point < Color        > p2(2.0, 2.0);
+    [[maybe_unused]] Point <              > p3(3.0, 3.0);
 
     return 0;
 }
