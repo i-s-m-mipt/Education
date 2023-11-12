fa517532acb090b82b1681f92f065585e03e3935
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-11-12 15:07:27 +0300

    Доработан пример mixin_basics.

--- examples/section_05/mixin_basics.cpp
+++ examples/section_05/mixin_basics.cpp
@@ -1,57 +1,59 @@
 #include <iostream>
 
-class Base 
+struct Number
 {
-public:
+    using value_type = int;
 
-    void f() const { std::cout << "Base" << std::endl; };
-
-}; // class Base 
-
-template < typename Base > class Mixin_1 : public Base
-{
-public:
-
-    void g() const { std::cout << "Mixin_1" << std::endl; Base::f(); }
-
-}; // template < typename Base > class Mixin_1 : public Base 
-
-template < typename Base > class Mixin_2 : public Base
-{
-public:
-
-    void h() const { std::cout << "Mixin_2" << std::endl; Base::g(); }
-
-}; // template < typename Base > class Mixin_2 : public Base 
-
-class Color {};
-class Label {};
-
-template < typename ... Bases > class Point : private Bases... // note: modern mixin
-{
-public:
-
-    explicit Point(double x, double y) : Bases()..., m_x(x), m_y(y)
-    {
-        std::cout << sizeof...(Bases) << std::endl;
+    auto get() const { return value; }
+    
+    void set(value_type new_value) 
+    { 
+        value = new_value; 
     }
 
-private:
+    value_type value;
 
-    double m_x;
-    double m_y;
+}; // struct Number
 
-}; // template < typename T, typename ... Bases > class Point : private Bases...
+template < typename Base > struct Undoable : public Base
+{
+    using value_type = typename Base::value_type;
+
+    void undo() { Base::set(value_before); }
+    
+    void set(value_type new_value) 
+    { 
+        value_before = Base::get(); Base::set(new_value); 
+    }
+    
+    value_type value_before;
+
+}; // template < typename Base > struct Undoable : public Base
+
+template < typename Base > struct Redoable : public Base
+{
+    using value_type = typename Base::value_type;
+
+    void redo() { Base::set(value_after); }
+    
+    void set(value_type new_value) 
+    { 
+        value_after = new_value; Base::set(new_value); 
+    }
+    
+    value_type value_after;
+
+}; // template < typename Base > struct Redoable : public Base
 
 int main() 
 {
-    Mixin_2 < Mixin_1 < Base > > mixin; // note: old style mixin
-    
-    mixin.h();
+    Redoable < Undoable < Number > > number;
 
-    [[maybe_unused]] Point < Color, Label > p1(1.0, 1.0);
-    [[maybe_unused]] Point < Color        > p2(2.0, 2.0);
-    [[maybe_unused]] Point <              > p3(3.0, 3.0);
+    number.set(42); number.set(84);
+
+    std::cout << number.get() << std::endl; number.undo();
+    std::cout << number.get() << std::endl; number.redo();
+    std::cout << number.get() << std::endl;
 
     return 0;
 }
