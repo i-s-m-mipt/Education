98ee836006347c2bccae3c3a2935a90945bc8bb5
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-11-12 15:47:11 +0300

    Доработан пример variadic_bases.

---
+++ examples/variadic_bases.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+class Color_v1 {};
+class Label_v1 {};
+
+template < typename ... Bases > 
+class Point_v1 : public Bases... // note: modern mixin with variadic base classes
+{
+public:
+
+    explicit Point_v1(double x, double y) : Bases()..., m_x(x), m_y(y)
+    {
+        std::cout << sizeof...(Bases) << std::endl;
+    }
+
+private:
+
+    double m_x;
+    double m_y;
+
+}; // class Point_v1 : public Bases...
+
+template < typename T > class Color_v2 {};
+template < typename T > class Label_v2 {};
+
+template < template < typename > typename ... Bases > // note: template template parameters
+class Point_v2 : public Bases < Point_v2 < Bases ... > > ... 
+{
+public:
+
+    explicit Point_v2(double x, double y) : Bases < Point_v2 > ()..., m_x(x), m_y(y)
+    {
+        std::cout << sizeof...(Bases) << std::endl;
+    }
+
+private:
+
+    double m_x;
+    double m_y;
+
+}; // class Point_v2 : public Bases < Point_v2 > ...
+
+int main()
+{
+    [[maybe_unused]] Point_v1 < Color_v1, Label_v1 > p1_v1(1.0, 1.0);
+    [[maybe_unused]] Point_v1 < Color_v1           > p2_v1(2.0, 2.0);
+    [[maybe_unused]] Point_v1 <                    > p3_v1(3.0, 3.0);
+
+    [[maybe_unused]] Point_v2 < Color_v2, Label_v2 > p1_v2(1.0, 1.0);
+    [[maybe_unused]] Point_v2 < Color_v2           > p2_v2(2.0, 2.0);
+    [[maybe_unused]] Point_v2 <                    > p3_v2(3.0, 3.0);
+
+    return 0;
+}
