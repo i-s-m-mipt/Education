3f407b1f102e4677e761e8c55714379f2d76f32a
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-11-18 02:07:47 +0300

    Доработан пример optional_basics.

---
+++ examples/section_07/optional_basics.cpp
@@ -0,0 +1,33 @@
+#include <iomanip>
+#include <iostream>
+#include <optional>
+#include <string>
+
+auto create(bool b) // note: optional returned value
+{
+	return (b ? std::optional < std::string > ("object") : std::nullopt);
+}
+
+int main()
+{
+    std::optional < int > o1; // note: same as std::nullopt
+
+    std::cout << o1.has_value() << std::endl;
+
+    auto o2 = std::make_optional(42);
+
+    o2.reset(); // note: same as std::nullopt
+
+    std::optional < std::string > o3(std::in_place, 5, 'a'); // note: in-place construction
+
+    std::cout << *o3 << ' ' << o3->length() << std::endl;
+
+    std::cout << create(false).value_or("empty") << std::endl;
+
+    if (auto object = create(true))
+    {
+        std::cout << *object << std::endl;
+    }
+
+	return 0;
+}
