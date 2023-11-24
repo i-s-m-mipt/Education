12f1368ddaeba8158b4f93007b6db8022644a31d
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-11-24 22:23:23 +0300

    Доработан пример metaprogramming.

--- examples/section_04/metaprogramming.cpp
+++ examples/section_04/metaprogramming.cpp
@@ -10,21 +10,21 @@ template <> struct Factorial < 0 > { static const int value = 1; };
 
 template < int N > const int factorial_v = Factorial < N > ::value; // good: variable template
 
-template < int p, int d > struct Check_Is_Prime
+template < int P, int D > struct Check_Is_Prime
 {
-	static const bool value = (p % d != 0) && Check_Is_Prime < p, d - 1 > ::value;
+	static const bool value = (P % D != 0) && Check_Is_Prime < P, D - 1 > ::value;
 
-}; // template < int p, int d > struct Check_Is_Prime
+}; // template < int P, int D > struct Check_Is_Prime
 
-template < int p > struct Check_Is_Prime < p, 2 >
+template < int P > struct Check_Is_Prime < P, 2 >
 {
-	static const bool value = (p % 2 != 0);
+	static const bool value = (P % 2 != 0);
 
-}; // template < int p > struct Check_Is_Prime < p, 2 >
+}; // template < int P > struct Check_Is_Prime < P, 2 >
 
-template < int p > struct Is_Prime
+template < int P > struct Is_Prime
 {
-	static const bool value = Check_Is_Prime < p, p / 2 > ::value;
+	static const bool value = Check_Is_Prime < P, P / 2 > ::value;
 
 }; // template < int p > struct Is_Prime
 
@@ -33,7 +33,7 @@ template <> struct Is_Prime < 1 > { static const bool value = false; };
 template <> struct Is_Prime < 2 > { static const bool value =  true; };
 template <> struct Is_Prime < 3 > { static const bool value =  true; };
 
-template < int p > const bool is_prime_v = Is_Prime < p > ::value; // good: variable template
+template < int P > const bool is_prime_v = Is_Prime < P > ::value; // good: variable template
 
 int main()
 {
