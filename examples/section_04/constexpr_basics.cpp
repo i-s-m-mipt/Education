bb731bceb490601fd6adc520d721aeba89a3e269
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-11-24 21:31:43 +0300

    Доработан пример constexpr_basics.

--- examples/section_04/constexpr_basics.cpp
+++ examples/section_04/constexpr_basics.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <type_traits>
 
 constexpr auto is_prime(int p) // note: can be computed at compile-time
 {
@@ -13,21 +14,42 @@ constexpr auto is_prime(int p) // note: can be computed at compile-time
 	return (p > 1);
 }
 
+constexpr auto f([[maybe_unused]] int x)
+{
+	return std::is_constant_evaluated(); // note: see type traits
+}
+
+template < typename T, typename ... Types > void print(const T & arg, const Types & ... args)
+{
+	std::cout << arg << std::endl;
+
+	if constexpr (sizeof...(args) > 0) // note: compile-time instantiation
+	{
+		print(args...);
+	}
+}
+
 int main()
 {
-	constexpr auto x = 43; // note: compile-time constant
+	constexpr auto x = 42; // note: compile-time constant
 
-	[[maybe_unused]] constexpr auto result_1 = is_prime(5); // note: compile-time
-	[[maybe_unused]] constexpr auto result_2 = is_prime(x); // note: compile-time
+	[[maybe_unused]] constexpr auto is_prime_1 = is_prime(5); // note: compile-time
+	[[maybe_unused]] constexpr auto is_prime_2 = is_prime(x); // note: compile-time
 
-	auto y = 42;
+	auto y = 43;
 
-//	constexpr auto result_3 = is_prime(y); // error: not compile-time constant
+//	constexpr auto is_prime_3 = is_prime(y); // error: not compile-time constant
 
-	[[maybe_unused]] auto result_4 = is_prime(5); // note: can be computed at compile-time
-	[[maybe_unused]] auto result_5 = is_prime(x); // note: can be computed at compile-time
+	[[maybe_unused]] auto is_prime_4 = is_prime(5); // note: can be computed at compile-time
+	[[maybe_unused]] auto is_prime_5 = is_prime(x); // note: can be computed at compile-time
 
-	[[maybe_unused]] auto result_6 = is_prime(y); // note: runtime only
+	[[maybe_unused]] auto is_prime_6 = is_prime(y); // note: runtime only
+
+	constexpr auto result = f(x);
+
+	std::cout << result << ' ' << f(y) << std::endl;
+
+	print('a', 42, 3.14);
 
 	return 0;
 }
