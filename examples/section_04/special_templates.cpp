aecb1146c952a9c2750314c44a664d1fe7c31adf
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-11-26 23:58:53 +0300

    Доработан пример special_templates.

--- examples/section_04/special_templates.cpp
+++ examples/section_04/special_templates.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
-#include <vector>
 
-template < typename T, typename C > class basic_stack {};
+template < typename T, auto N > class Container {};
 
-template < typename T > using Stack = basic_stack < T, std::vector < T > > ;
+template < typename T > using Buffer = Container < T, 1024 > ;
 
 template < typename T > const T pi = T(3.141592);
 
 template < typename T > T area(T r)
 {
-	return pi < T > * r * r;
+	return pi < T > * r * r; // note: example from standard
 }
 
 int main()
 {
-	[[maybe_unused]] Stack < int > stack;
+	[[maybe_unused]] Buffer < int > buffer;
 
-	std::cout << area(100) << std::endl;
+	std::cout << area(1  ) << std::endl;
 	std::cout << area(1.0) << std::endl;
 
 	return 0;
