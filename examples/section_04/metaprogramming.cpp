23e15f24481863f485154fd567151daee5724dbd
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-11-27 00:01:49 +0300

    Доработан пример metaprogramming.

--- examples/section_04/metaprogramming.cpp
+++ examples/section_04/metaprogramming.cpp
@@ -8,7 +8,7 @@ template < int N > struct Factorial
 
 template <> struct Factorial < 0 > { static const int value = 1; };
 
-template < int N > const int factorial_v = Factorial < N > ::value; // good: variable template
+template < int N > const int factorial_v = Factorial < N > ::value;
 
 template < int P, int D > struct Check_Is_Prime
 {
@@ -33,7 +33,7 @@ template <> struct Is_Prime < 1 > { static const bool value = false; };
 template <> struct Is_Prime < 2 > { static const bool value =  true; };
 template <> struct Is_Prime < 3 > { static const bool value =  true; };
 
-template < int P > const bool is_prime_v = Is_Prime < P > ::value; // good: variable template
+template < int P > const bool is_prime_v = Is_Prime < P > ::value;
 
 int main()
 {
