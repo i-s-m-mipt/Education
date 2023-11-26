b587c33f20df2e45edccf52a42ba83fa8469af9d
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-11-27 00:13:27 +0300

    Доработан пример variadic_templates.

--- examples/section_04/variadic_templates.cpp
+++ examples/section_04/variadic_templates.cpp
@@ -5,22 +5,19 @@ void print_v1()
 	std::cout << std::endl; 
 }
 
-template < typename T, typename ... Ts >
-void print_v1(T arg, Ts ... args)
+template < typename T, typename ... Ts > void print_v1(T arg, Ts ... args)
 {
 	std::cout << arg << ' ';
 
 	print_v1(args...);
 }
 
-template < typename T >
-void print_v2(T arg)
+template < typename T > void print_v2(T arg)
 {
 	std::cout << arg << ' ';
 }
 
-template < typename T, typename ... Ts >
-void print_v2(T arg, Ts ... args)
+template < typename T, typename ... Ts > void print_v2(T arg, Ts ... args)
 {
 	print_v2(arg);
 
@@ -36,48 +33,41 @@ struct Point
 
 }; // struct Point
 
-template < typename T, typename ... Ts >
-auto make_object(Ts ... args)
+template < typename T, typename ... Ts > auto make_object(Ts ... args)
 {
 	std::cout << sizeof...(args) << std::endl;
 
 	return new T(args...);
 }
 
-template < typename ... Ts >
-auto sum_v1(Ts ... args)
+template < typename ... Ts > auto sum_v1(Ts ... args)
 {
 	return (... + args); // note: (... + args) -> (((arg_1 + arg_2) + arg_3) + ...)
 }
 
-template < typename ... Ts >
-auto sum_v2(Ts ... args)
+template < typename ... Ts > auto sum_v2(Ts ... args)
 {
 	return (args + ...); // note: (args + ...) -> (... + (arg_n-2 + (arg_n-1 + arg_n)))
 }
 
-template < typename ... Ts >
-auto sum_v3(Ts ... args)
+template < typename ... Ts > auto sum_v3(Ts ... args)
 {
 	return (42 + ... + args); // note: (42 + ... + args) -> (((42 + arg_1) + arg_2) + ...)
 }
 
-template < typename ... Ts >
-auto sum_v4(Ts ... args)
+template < typename ... Ts > auto sum_v4(Ts ... args)
 {
 	return (args + ... + 42); // note: (args + ... + 42) -> (... + (arg_n-1 + (arg_n + 42)))
 }
 
-template < typename ... Ts >
-void f(Ts ... args)
+template < typename ... Ts > void f(Ts ... args)
 {
 //	print_v1(args + 1...); // error: invalid syntax
 
 	print_v1((args + 1)...); // note: or args + 1 ...
 }
 
-template < typename ... Ts >
-void g(Ts ... args)
+template < typename ... Ts > void g(Ts ... args)
 {
 	print_v1(args + args...);
 }
