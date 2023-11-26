b9d681699ce9ea8dfabbbe0cc13061fb411d1e06
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-11-27 01:37:07 +0300

    Доработан пример selection_basics.

--- examples/section_02/selection_basics.cpp
+++ examples/section_02/selection_basics.cpp
@@ -2,17 +2,13 @@
 
 int main()
 {
-    std::cout << "Enter integer value: ";
+    std::cout << "Enter integer value: "; int x{};
 
-    int x{}; // good: zero initialized variable for input
-
-    std::cin >> x;
-
-    if (auto y = x * 4 + 10; 42 == y) // good: 42 == y prevents error 42 = y
+    if (std::cin >> x; 42 == x) // good: 42 == y prevents error 42 = y
     {
         std::cout << "path 1" << std::endl;
     }
-    else if (y > 0)
+    else if (x > 0)
     {
         std::cout << "path 2" << std::endl;
     }
@@ -30,7 +26,7 @@ int main()
 
     std::cout << (x > 0 ? x : 42) << std::endl; // good: compact selection
 
-    switch (auto y = x / 4 - 1; y)
+    switch (std::cin >> x; x)
     {
     [[likely]] case 0: // good: optimally generated machine code
     [[likely]] case 1:
