954009534f7f4820463e61afc11a426358e64382
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-12-01 22:20:52 +0300

    Доработан пример bitwise_operations.

--- examples/section_08/bitwise_operations.cpp
+++ examples/section_08/bitwise_operations.cpp
@@ -1,49 +1,77 @@
+#include <bitset>
+#include <cassert>
+#include <cstddef>
 #include <cstdint>
+#include <iomanip>
 #include <iostream>
+#include <type_traits>
 
-std::uint16_t middle(std::uint32_t x)
+constexpr std::uint16_t middle(std::uint32_t x)
 {
     return (x >> 8) & 0xffff;
 }
 
+struct Datetime
+{
+    unsigned int milli  : 10;
+    unsigned int second : 6;
+    unsigned int minute : 6;
+    unsigned int hour   : 5;
+    unsigned int day    : 5;
+    unsigned int month  : 4;
+    unsigned int year   : 28; // good: 64 bits in total, no holes
+
+}; // struct Datetime
+
 int main()
 {
-    auto d = 42;
-    auto o = 052;
-    auto x = 0x2a;
-    auto b = 0b101010;
+    [[maybe_unused]] auto b = 0b101010; // note: binary
+    [[maybe_unused]] auto o = 052;      // note: octal
+    [[maybe_unused]] auto d = 42;       // note: decimal
+    [[maybe_unused]] auto x = 0x2a;     // note: hexadecimal
 
+    std::cout << std::showbase;
+
+    std::cout << 
+        std::oct << 42 << std::endl << 
+        std::dec << 42 << std::endl << 
+        std::hex << 42 << std::endl;
+
+    auto data = 0x1234;
+    auto mask = 0x00f0;
     
+    std::cout << 
+        (         mask) << std::endl << 
+        ( data        ) << std::endl << 
+        ( data |  mask) << std::endl << 
+        ( data & ~mask) << std::endl << 
+        ( data &  mask) << std::endl << 
+        ( data ^  mask) << std::endl << 
+        (~data        ) << std::endl;
 
-    std::cout << std::oct << 42 << std::endl
-              << std::dec << 42 << std::endl
-              << std::hex << 42 << std::endl;
+    auto m = 0x123; assert((m << 1) == 0x246); // good: bit shift as multiplication
+    auto n = -4000; assert((n >> 2) == -1000); // good: bit shift as multiplication
 
-    std::cout << sizeof(std::uint64_t) << std::endl;
+    static_assert(sizeof(std::uint64_t) == 8); // note: 8 byte(s) exactly
 
     std::cout << std::boolalpha << std::is_same_v < int, std::int32_t > << std::endl;
 
-    std::uint16_t mask = 0x00f0;
-    std::uint32_t value = 0x12345678;
+    assert(middle(0x12345678) == 0x3456);
 
-    std::cout << std::hex << std::showbase
-        << mask << std::endl
-        << value << std::endl
-        << (value | mask) << std::endl
-        << (value & ~mask) << std::endl
-        << (value & mask) << std::endl
-        << (value ^ mask) << std::endl
-        << (~value) << std::endl;
+    std::bitset < 8 > bitset(0b1101); // note: see reference for detailed description
+        
+    bitset |= 0b0010; assert(bitset == 0b1111);
+    bitset &= 0b0011; assert(bitset == 0b0011);
 
-    auto v1 = 0x123;
+    std::cout << std::bitset < 8 > (42) << std::endl;
+   
+    assert(std::bitset < 8 > (        42).to_string() == "00101010");
+    assert(std::bitset < 8 > ("00101010").to_ullong() ==         42);
 
-    std::cout << (v1 << 1) << std::endl;
+    std::byte byte{ 42 }; // note: see reference for detailed description
 
-    auto v2 = -1000;
-
-    std::cout << std::dec << (v2 >> 2) << std::endl;
-
-    bitset, std::byte, bits, bit fields
+    byte |= std::byte{ 0b1111'0000 }; assert(std::to_integer < int > (byte) == 0b1111'1010);
+    byte &= std::byte{ 0b1111'0000 }; assert(std::to_integer < int > (byte) == 0b1111'0000);
 
     return 0;
 }
