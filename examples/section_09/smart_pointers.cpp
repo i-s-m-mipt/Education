8c8c7e8d73b6e9c653535091884c73bf2eebbb0e
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-12-16 22:24:27 +0300

    Доработан пример smart_pointers.

--- examples/section_09/smart_pointers.cpp
+++ examples/section_09/smart_pointers.cpp
@@ -1,4 +1,9 @@
+#include <cassert>
+#include <exception>
 #include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <type_traits>
 
 template < typename T > class Resource // good: RAII idiom
 {
@@ -17,7 +22,60 @@ private:
 
 	T * m_ptr; // note: copying is prohibited for example
 
-}; // template < typename T > class Resource
+}; // template < typename T > class Resource 
+
+void f(std::shared_ptr < int > x, [[maybe_unused]] int y) {}
+
+int bad(bool make_error = true) 
+{
+	if (make_error)
+	{
+		throw std::runtime_error("error");
+	}
+
+	return 0;
+}
+
+struct B; // note: forward declaration as for plain pointers
+
+struct A
+{
+	~A() { std::cout << "A destroyed" << std::endl; }
+
+	std::shared_ptr < B > b;
+
+}; // struct A
+
+struct B
+{
+	~B() { std::cout << "B destroyed" << std::endl; }
+
+	std::weak_ptr < A > a; // note: immortal connection if shared_ptr
+
+}; // struct B
+
+class Base 
+{
+public:
+
+	virtual ~Base() noexcept = default;
+
+	virtual void print() const { std::cout << "Base" << std::endl; }
+
+}; // class Base 
+
+class Derived : public Base 
+{
+public:
+
+	void print() const override { std::cout << "Derived" << std::endl; }
+
+}; // class Derived : public Base 
+
+std::unique_ptr < Base > make_derived() // note: modern factory
+{
+	return std::make_unique < Derived > ();
+}
 
 int main()
 {
@@ -27,10 +85,75 @@ int main()
 
 		throw std::runtime_error("error");
 	}
-	catch (const std::exception & exception)
+	catch (...) {}
+
+	std::shared_ptr < int > sptr_1; // note: same as nullptr
+
+	if (sptr_1) // note: check if not nullptr as for plain pointer
 	{
-		std::cerr << exception.what() << std::endl;
+		std::cout << sptr_1 << std::endl;
 	}
+
+	std::shared_ptr < int > sptr_2(new int(42)); // note: two new calls instead of one
+
+	std::shared_ptr < int > sptr_3(sptr_2);
+
+	assert(sptr_2.use_count() == 2 && *sptr_2 == 42);
+	assert(sptr_3.use_count() == 2 && *sptr_3 == 42);
+
+	sptr_3.reset(new int(43));
+
+	assert(sptr_2.use_count() == 1 && *sptr_2 == 42);
+	assert(sptr_3.use_count() == 1 && *sptr_3 == 43);
+
+	auto ptr = new int(42);
+
+//	std::shared_ptr < int > sptr_4(ptr); // bad: don't mix with plain pointers
+
+	delete ptr;
+
+//	f(std::shared_ptr < int > (new int(42)), bad()); // bad: possible memory leak
+
+	auto sptr_5 = std::make_shared < int > (42); // good: one new call instead of two
+
+	try
+	{
+		f(std::make_shared < int > (42), bad()); // good: memory leak impossible
+	}
+	catch (...) {}
+
+	const std::size_t size = 10;
+
+//	std::shared_ptr < int > sptr_6(new int[size]{}); // bad: undefined behavior
+
+	std::shared_ptr < int > sptr_7(new int[size]{}, std::default_delete < int[] > ());
+
+//	*(sptr_7++) = 42; // note: pointer arithmetic is prohibited, prefer iterators
+
+	auto sptr_8 = std::make_shared < int[] > (size, 0); // note: is it useful?
+
+	sptr_8[0] = 42; // note: allowed only for array types
+
+	auto sptr_9 = std::make_shared < int > (42);
+
+	std::weak_ptr < int > wptr = sptr_9; // note: weak_ptr doesn't own object
+
+	assert(wptr.use_count() == 1);
+
+	*wptr.lock() = 43; // note: create shared_ptr from weak_ptr
+
+	{
+		auto a = std::make_shared < A > (); 
+		a->b   = std::make_shared < B > ();
+
+		a->b->a = a; // note: cyclic dependency, destroy correctly
+	}
+
+	auto uptr_1 = std::make_unique < int > (42); // note: much similar to shared_ptr
+
+	auto uptr_2 = std::move(uptr_1); // note: move-only type
+
+	make_derived()->print(); // note: modern runtime polymorphism with factory
 
 	return 0;
 }
