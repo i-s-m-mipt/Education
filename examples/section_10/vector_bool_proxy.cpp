4053c3a2ad318fc864f28a9c32dc30d5874a8b4c
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-12-18 16:57:05 +0300

    Доработан пример vector_bool_proxy.

---
+++ examples/section_10/vector_bool_proxy.cpp
@@ -0,0 +1,37 @@
+#include <cassert>
+#include <memory>
+#include <vector>
+
+class Data {};
+
+class Loader // note: proxy class for lazy loading
+{
+public:
+
+	void load() 
+	{
+		if (!m_data) m_data = std::make_unique < Data > ();
+	}
+
+	std::unique_ptr < Data > m_data; // note: nullptr
+
+}; // class Loader
+
+int main()
+{
+	constexpr std::size_t size = 8;
+
+	bool array[size]{};
+
+	assert(sizeof(array) == size);
+
+	std::vector < bool > vector(size); // good: 1 bit per bool
+
+	constexpr std::size_t middle = size / 2;
+
+	vector[middle] = true;
+
+	auto value = vector[middle]; // note: auto -> std::vector < bool > ::reference
+	
+	return 0;
+}
