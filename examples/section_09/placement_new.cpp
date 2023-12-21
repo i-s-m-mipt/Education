54e6a8cc9428f9475f5c92e901f440b3fc05ac51
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-12-21 21:29:57 +0300

    Доработан пример placement_new.

--- examples/section_09/placement_new.cpp
+++ examples/section_09/placement_new.cpp
@@ -1,94 +1,77 @@
+#include <cassert>
 #include <iostream>
 #include <string>
-#include <vector>
 
 class X
 {
 public:
 
-	using data_t = int;
-
-public:
-
-	X(data_t data) : m_data(data)
+	explicit X(std::size_t index) : m_index(index)
 	{
-		std::cout << "ctor: " << m_data << std::endl;
+		std::cout << "X::CTOR called, index: " << m_index << std::endl;
 	}
 
 	~X()
 	{
-		std::cout << "dtor: " << m_data << std::endl;
-	}
-
-public:
-
-	const auto data() const
-	{
-		return m_data;
+		std::cout << "X::DTOR called, index: " << m_index << std::endl;
 	}
 
 private:
 
-	data_t m_data;
-};
+	std::size_t m_index;
+
+}; // class X
 
 union U
 {
-	U() : s() {}
+	U() : s1() {}
 
-	~U() {} // ?
+	~U() {} // note: what should be destroyed here?
 
-	std::string s;
-	std::vector < int > v;
-};
+	std::string s1;
+	std::string s2;
+
+}; // union U
 
 int main()
 {
-	std::cout << sizeof(X) << std::endl;
+	assert(sizeof(X) == sizeof(std::size_t));
 
-	const std::size_t size = 10;
+	const std::size_t size = 5;
 
-	auto ptr = static_cast < X * > (operator new(sizeof(X) * size));
-
-	for (std::size_t i = 0; i < size; ++i)
-	{
-		new (ptr + i) X(static_cast < X::data_t > (i));
-	}
+	auto ptr = static_cast < X * > (operator new(sizeof(X) * size)); // note: uninitialized memory
 
 	for (std::size_t i = 0; i < size; ++i)
 	{
-		std::cout << ptr[i].data() << std::endl;
+		new (ptr + i) X(i); // note: placement new, construction without allocation
 	}
 
 	const std::size_t offset = size / 2;
 
 	(ptr + offset)->~X();
 
-	new (ptr + offset) X(42);
+	new (ptr + offset) X(42); // note: object reconstruction in the same memory cell
 
 	for (std::size_t i = 0; i < size; ++i)
 	{
 		ptr[i].~X();
 	}
 
-	operator delete(ptr);
-
-	std::cout << sizeof(std::string)         << std::endl;
-	std::cout << sizeof(std::vector < int >) << std::endl;
+	operator delete(ptr); // note: uninitialized memory
 
 	std::cout << sizeof(U) << std::endl;
 
-	U u;
+	U u; // note: prefer using std::variant
 
-	u.s = "hello";
+	u.s1 = "hello";
 
-	u.s.~basic_string();
+	u.s1.~basic_string(); // good: explicit member destructor call
 
-	new (&u.v) std::vector < int > ;
+	new (&u.s2) std::string;
 
-	u.v.push_back(42);
+	u.s2 = "world";
 
-	u.v.~vector();
+	u.s2.~basic_string(); // good: explicit member destructor call
 
 	return 0;
 }
