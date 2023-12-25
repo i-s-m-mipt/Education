462e0a5b1c6eca905ba299e77e85cee61815129f
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-12-25 21:34:47 +0300

    Доработан пример crtp_counter.

--- examples/section_05/crtp_counter.cpp
+++ examples/section_05/crtp_counter.cpp
@@ -1,43 +1,51 @@
 #include <iostream>
 
-template < typename T > class Counter
+template < typename T, std::size_t M > class Counter 
 {
 protected:
 
-	Counter()                       { ++m_counter; }
-	Counter(const Counter < T > & ) { ++m_counter; }
-	Counter(      Counter < T > &&) { ++m_counter; }
+	Counter()                          { initialize(); }
+	Counter(const Counter < T, M > & ) { initialize(); }
+	Counter(      Counter < T, M > &&) { initialize(); }
 
-	~Counter() { --m_counter; }
+	void initialize() // good: better than singleton
+	{
+		if (++counter > M)
+		{
+			std::cout << "Error: too many objects" << std::endl;
+		}
+	}
+
+	~Counter() { --counter; } // note: non-polymorphic class
 
 public:
 
-	static auto counter()
+	static auto get_counter()
 	{
-		return m_counter;
+		return counter;
 	}
 
 private:
 
-	static inline std::size_t m_counter = 0;
+	static inline std::size_t counter = 0;
 
 }; // template < typename T > class Counter
 
 template < typename T >
-class Container_1 : public Counter < Container_1 < T > > {};
+class Container_1 : public Counter < Container_1 < T > , 1 > {};
 
 template < typename T >
-class Container_2 : public Counter < Container_2 < T > > {};
+class Container_2 : public Counter < Container_2 < T > , 2 > {};
 
 int main()
 {
 	Container_1 < int > ci_1;
-	Container_1 < int > ci_2(ci_1);
+	Container_1 < int > ci_2(ci_1); // note: second instance
 
 	Container_2 < int > ci_3;
 
-	std::cout << Container_1 < int > ::counter() << std::endl;
-	std::cout << Container_2 < int > ::counter() << std::endl;
+	std::cout << Container_1 < int > ::get_counter() << std::endl;
+	std::cout << Container_2 < int > ::get_counter() << std::endl;
 
 	return 0;
 }
