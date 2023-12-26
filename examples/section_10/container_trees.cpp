b4bf508b92962da1ca82c9f70b76291bfbe1a8d2
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-12-27 00:20:49 +0300

    Доработан пример container_trees.

--- examples/section_10/container_trees.cpp
+++ examples/section_10/container_trees.cpp
@@ -4,28 +4,21 @@
 
 int main()
 {
-	std::set < int > set;
+	std::set < int > set; // note: O(log(N)) complexity for single operations
 
-	set.insert(1);
+	set.insert(1); 
 	set.insert(2);
 	set.insert(4);
 	set.insert(5);
 
-	assert(!set.insert(1).second); // note: differs for std::multiset
+	assert(!set.insert(1).second); // note: insert differs for std::multiset
 
-	assert(!set.erase(3));
+	assert(!set.erase(3)); // note: removes all elements with provided key
 
 	assert(!set.contains(3)); // note: better than set.find(3) == std::end(set)
 
-	assert(*set.lower_bound(3)        == 4);
-	assert(*set.upper_bound(3)        == 4);
-	assert(*set.equal_range(3).first  == 4);
-	assert(*set.equal_range(3).second == 4);
-
-	assert(*set.lower_bound(4)        == 4);
-	assert(*set.upper_bound(4)        == 5);
-	assert(*set.equal_range(4).first  == 4);
-	assert(*set.equal_range(4).second == 5);
+	assert(*set.lower_bound(3) == 4 && *set.upper_bound(3) == 4);
+	assert(*set.lower_bound(4) == 4 && *set.upper_bound(4) == 5);
 
 	auto node = set.extract(1); node.value() = 3; set.insert(std::move(node));
 
