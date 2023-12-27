9bc6e527dcdc368b65feb40f0fb98c1701b43813
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-12-28 02:02:26 +0300

    Доработан пример custom_hash_equal.

--- examples/section_10/custom_hash_equal.cpp
+++ examples/section_10/custom_hash_equal.cpp
@@ -1,90 +1,43 @@
-#include <functional>
-#include <iostream>
 #include <string>
 #include <unordered_set>
 
-// http://www.boost.org/doc/libs/1_35_0/doc/html/hash/combine.html
-
-template < typename T >
-void hash_combine(std::size_t& seed, const T& value) noexcept
+class Person
 {
-	seed ^= std::hash < T >()(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-}
-
-template < typename T >
-void hash_value(std::size_t& seed, const T& value) noexcept
-{
-	hash_combine(seed, value);
-}
-
-template < typename T, typename ... Types >
-void hash_value(std::size_t& seed, const T& value, const Types & ... args) noexcept
-{
-	hash_combine(seed, value);
-	hash_value(seed, args...);
-}
-
-template < typename ... Types >
-std::size_t hash_value(const Types & ... args) noexcept
-{
-	std::size_t seed = 0;
-	hash_value(seed, args...);
-	return seed;
-}
-
-class Customer
-{
-private:
-	friend struct Customer_Hash;
-	friend struct Customer_Equal;
-
 public:
-	explicit Customer(const std::string& name, const std::size_t mark) :
-		m_name(name), m_mark(mark)
-	{}
 
-	~Customer() noexcept = default;
+	explicit Person(const std::string & name) : m_name(name) {}
 
-public:
-	friend std::ostream& operator << (std::ostream& stream, const Customer& customer)
-	{
-		return (stream << customer.m_name << "," << customer.m_mark);
-	}
+	const auto & name() const noexcept { return m_name; }
 
 private:
+
 	std::string m_name;
-	std::size_t m_mark;
-};
 
-struct Customer_Hash
+}; // class Person
+
+struct Hash
 {
-	std::size_t operator() (const Customer& customer) const noexcept
+	std::size_t operator()(const Person & person) const noexcept
 	{
-		return hash_value(customer.m_name, customer.m_mark);
-	}
-};
-
-struct Customer_Equal
-{
-	bool operator() (const Customer& lhs, const Customer& rhs) const noexcept
-	{
-		return (lhs.m_name == rhs.m_name);
-	}
-};
-
-int main(int argc, char** argv)
-{
-	std::unordered_set < Customer, Customer_Hash, Customer_Equal > customers;
-
-	customers.insert(Customer("Ivan", 42));
-	customers.insert(Customer("Jens", 66));
-
-	for (const auto& customer : customers)
-	{
-		std::cout << customer << std::endl;
+		return std::hash < std::string > ()(person.name());
 	}
 
-	system("pause");
+}; // struct Hash
 
-	return EXIT_SUCCESS;
+struct Equal
+{
+	bool operator()(const Person & lhs, const Person & rhs) const noexcept
+	{
+		return (lhs.name() == rhs.name());
+	}
+
+}; // struct Equal
+
+int main()
+{
+	std::unordered_set < Person, Hash, Equal > persons; // note: same for other containers
+
+	persons.emplace("Matthias");
+
+	return 0;
 }
