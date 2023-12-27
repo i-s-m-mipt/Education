73b9010d2541a85b36b6b987a246e9aeae96a9e0
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-12-27 04:35:57 +0300

    Доработан пример hash_functions.

--- examples/section_10/hash_functions.cpp
+++ examples/section_10/hash_functions.cpp
@@ -1,22 +1,25 @@
 #include <functional>
 #include <iostream>
 #include <string>
+#include <vector>
+
+#include <boost/container_hash/hash.hpp>
 
 auto hash(const std::string & string) // note: sequential hash function for strings
 {
-	unsigned int hash = 0;
+	unsigned int seed = 0;
 
 	for (std::size_t i = 0; i < std::size(string); ++i)
 	{
-		(hash *= 59) += static_cast < unsigned int > (string[i]); // note: prime number
+		(seed *= 43) += static_cast < unsigned int > (string[i]); // note: prime number
 	}
 
-	return hash;
+	return seed;
 }
 
 template < typename T > void bind(std::size_t & seed, const T & value) noexcept
 {
-	seed ^= std::hash < T > ()(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2); // note: ^= instead of +=
+	seed ^= (seed + 0x9e3779b9 + std::hash < T > ()(value)); // note: ^= instead of +=
 }
 
 template < typename T > void hash(std::size_t & seed, const T & value) noexcept
@@ -43,18 +46,37 @@ struct S
 {
 	std::string string_1;
 	std::string string_2;
+	std::string string_3;
 
 }; // struct S
 
+std::size_t hash_value(const S & s)
+{
+	std::size_t seed = 0;
+
+	boost::hash_combine(seed, s.string_1);
+	boost::hash_combine(seed, s.string_2);
+	boost::hash_combine(seed, s.string_3);
+
+	return seed;
+}
+
 int main()
 {
-	std::cout << std::hash < std::string > ()("hello") << std::endl;
+	std::cout << hash("hello, world!") << std::endl;
 
-	std::cout << hash("hello") << std::endl;
+	std::cout <<   std::hash < std::string > ()("hello, world!") << std::endl;
+	std::cout << boost::hash < std::string > ()("hello, world!") << std::endl;
 
-	S s = { "hello", "world" };
+	S s = { "hello", "hashing", "world" };
 
-	std::cout << combined_hash(s.string_1, s.string_2) << std::endl;
+	std::cout << combined_hash(s.string_1, s.string_2, s.string_3) << std::endl;
+
+	std::cout << boost::hash < S > ()(s) << std::endl;
+
+	std::vector < std::string > vector({ "hello", "hashing", "world" });
+
+	std::cout << boost::hash_range(std::begin(vector), std::end(vector)) << std::endl;
 
 	return 0;
 }
