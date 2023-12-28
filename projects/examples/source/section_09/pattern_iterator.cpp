176e0110b51c293f6a06bfb3db55e4b2007112e6
Ivan Makarov <i.s.m.mipt@yandex.ru>
2023-12-28 08:36:28 +0300

    Доработан пример pattern_iterator.

---
+++ projects/examples/source/section_09/pattern_iterator.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <memory>
+
+template < typename T > class List
+{
+private:
+
+	struct Node // note: can be implemented as separate struct template
+	{ 
+		T value; std::shared_ptr < Node > next; // note: no allocators
+
+	}; // struct Node
+
+public:
+
+	class Iterator // note: can be implemented as separate class template
+	{
+	public:
+
+		using iterator_category = std::forward_iterator_tag; // note: forward iterator
+
+		explicit Iterator(std::shared_ptr < Node > node = nullptr) noexcept : m_node(node) {}
+
+	public:
+
+		Iterator & operator++() noexcept 
+		{ 
+			m_node = m_node->next; return *this; // note: undefined behaviour if end
+		}
+
+		Iterator operator++(int) noexcept 
+		{ 
+			auto prev = *this; ++(*this); return prev; 
+		}
+
+		[[nodiscard]] T & operator* () const noexcept { return  (m_node->value); }
+		[[nodiscard]] T * operator->() const noexcept { return &(m_node->value); }
+
+		[[nodiscard]] auto operator==(const Iterator & other) const noexcept 
+		{ 
+			return (m_node == other.m_node); 
+		}
+
+	private:
+
+		std::shared_ptr < Node > m_node; // note: no allocators
+
+	}; // class Iterator
+
+public:
+
+	[[nodiscard]] constexpr Iterator begin() noexcept { return Iterator(m_head); }
+	[[nodiscard]] constexpr Iterator end  () noexcept { return Iterator();       }
+
+	void push_back(T value)
+	{
+		if (auto new_node = std::make_shared < Node > (value, nullptr); !m_head)
+		{
+			m_head = new_node;
+		}
+		else
+		{
+			auto p = m_head; for (; p->next; p = p->next); p->next = new_node;
+		}
+	}
+
+private:
+
+	std::shared_ptr < Node > m_head; // note: no allocators
+
+}; // template < typename T > class List
+
+int main()
+{
+	List < int > list;
+
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+	list.push_back(4);
+	list.push_back(5);
+
+	for (auto iterator = std::begin(list); iterator != std::end(list); ++iterator)
+	{
+		std::cout << *iterator << " ";
+	}
+
+	return 0;
+}
