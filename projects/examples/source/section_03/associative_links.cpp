40c8db85d782d4a4482126f5a7100d08283cd92b
Ivan Makarov <i.s.m.mipt@yandex.ru>
2024-01-09 21:15:49 +0300

    Добавлен пример associative_links.

---
+++ projects/examples/source/section_03/associative_links.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Lecture;
+
+class Student
+{
+public:
+
+	Student(const std::string & name) : m_name(name) {}
+
+public:
+
+	const auto & name() const
+	{
+		return m_name;
+	}
+
+	void add_lecture(const Lecture & lecture)
+	{
+		m_lectures.push_back(&lecture);
+	}
+
+	void show_lectures() const;
+
+private:
+
+	std::string m_name;
+
+	std::vector < const Lecture * > m_lectures;
+};
+
+class Lecture
+{
+public:
+
+	Lecture(const std::string & name) : m_name(name) {}
+
+public:
+
+	const auto & name() const
+	{
+		return m_name;
+	}
+
+	void add_student(const Student & student)
+	{
+		m_students.push_back(&student);
+	}
+
+	void show_students() const;
+
+private:
+
+	std::string m_name;
+
+	std::vector < const Student * > m_students;
+};
+
+void Student::show_lectures() const
+{
+	std::cout << "Student " << m_name << " visits: " << std::endl;
+
+	for (std::size_t i = 0; i < m_lectures.size(); ++i)
+	{
+		std::cout << i + 1 << ": " << m_lectures[i]->name() << std::endl;
+	}
+}
+
+void Lecture::show_students() const
+{
+	std::cout << "Lecture " << m_name << " is visited by: " << std::endl;
+
+	for (std::size_t i = 0; i < m_students.size(); ++i)
+	{
+		std::cout << i + 1 << ": " << m_students[i]->name() << std::endl;
+	}
+}
+
+void connect(Student & s, Lecture & l)
+{
+	s.add_lecture(l); 
+	l.add_student(s);
+}
+
+int main()
+{
+	Student s1("Ivan");
+	Student s2("Dmitriy");
+	Student s3("Alexander");
+
+	Lecture l1("Math");
+	Lecture l2("Physics");
+	Lecture l3("Literature");
+
+	connect(s1, l1);
+	connect(s2, l2);
+	connect(s3, l3);
+	connect(s1, l3);
+	connect(s2, l1);
+	connect(s3, l2);
+
+	s1.show_lectures();
+
+	l1.show_students();
+
+	return 0;
+}
