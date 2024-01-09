9d4f402b2700e74adc04a8c810a981b2c541fdd8
Ivan Makarov <i.s.m.mipt@yandex.ru>
2024-01-09 21:10:30 +0300

    Доработан пример container_trees.

--- projects/examples/source/section_10/container_trees.cpp
+++ projects/examples/source/section_10/container_trees.cpp
@@ -1,11 +1,22 @@
 #include <cassert>
+#include <iostream>
 #include <map>
 #include <set>
 #include <string>
+#include <type_traits>
 
 int main()
 {
-	std::set set({1, 2, 3, 4, 5}); // note: O(log(N)) complexity for operations
+	std::set < int > set({1, 5, 3, 4, 2}); // note: O(log(N)) complexity for operations
+
+	using category_t = typename decltype(set)::iterator::iterator_category;
+
+	static_assert(std::is_same_v < category_t, std::bidirectional_iterator_tag > );
+
+	for (auto iterator = std::cbegin(set); iterator != std::cend(set); ++iterator)
+	{
+		std::cout << *iterator << ' ';
+	}
 
 	set.insert(std::begin(set), 0); // good: O(1) complexity (amortized) at best
 
@@ -16,8 +27,8 @@ int main()
 
 	assert(!set.contains(3)); // note: better than set.find(3) == std::end(set)
 
-	assert(*set.lower_bound(3) == 4 && *set.upper_bound(3) == 4);
-	assert(*set.lower_bound(4) == 4 && *set.upper_bound(4) == 5);
+	assert(*set.lower_bound(3) == 4 && *set.upper_bound(3) == 4); // 0 1 2 4 5
+	assert(*set.lower_bound(4) == 4 && *set.upper_bound(4) == 5); // 0 1 2 4 5
 
 	auto node = set.extract(1); node.value() = 3; set.insert(std::move(node));
 
