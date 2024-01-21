bdfc0e1683595e82461503453680de6b6edf8a77
Ivan Makarov <i.s.m.mipt@yandex.ru>
2024-01-21 03:36:33 +0300

    Доработан пример heterogenous_vector.

--- projects/examples/source/section_12/heterogeneous_vector.cpp
+++ projects/examples/source/section_12/heterogeneous_vector.cpp
@@ -1,23 +1,27 @@
 #include <any>
+#include <exception>
 #include <functional>
-#include <iomanip>
 #include <iostream>
-#include <type_traits>
+#include <iterator>
 #include <typeindex>
 #include <typeinfo>
 #include <unordered_map>
+#include <stdexcept>
+#include <string>
+#include <utility>
 #include <vector>
 
 template < typename T, typename F > auto inline make_visitor(F f)
 {
-    return std::make_pair(std::type_index(typeid(T)), [f](const auto & any){ f(std::any_cast < T > (any)); });
+    return std::make_pair(std::type_index(typeid(T)), 
+        [f](const auto & any){ f(std::any_cast < T > (any)); });
 }
 
 void handle(const std::any & any)
 {
-    using type_b = bool;
-    using type_c = char;
-    using type_i = int;
+    using type_b = bool; 
+    using type_c = char; 
+    using type_i = int; 
     using type_d = double;
 
     static std::unordered_map < std::type_index, std::function < void(const std::any &) > > visitors
@@ -28,7 +32,7 @@ void handle(const std::any & any)
         make_visitor < type_d > ([](type_d x){ std::cout << x << std::endl; })
     };
 
-    if (const auto iterator = visitors.find(std::type_index(any.type())); iterator != std::cend(visitors))
+    if (auto iterator = visitors.find(std::type_index(any.type())); iterator != std::cend(visitors))
     {
         iterator->second(any);
     }
@@ -40,10 +44,19 @@ void handle(const std::any & any)
 
 int main()
 {
-    std::vector < std::any > vector = { true, 'a', 42, 3.14 };
+    std::vector < std::any > vector = { true, 'a', 42, 3.14, "hello" };
 
-    for (std::size_t i = 0; i < std::size(vector); ++i)
+    try
     {
-        handle(vector[i]);
+        for (std::size_t i = 0; i < std::size(vector); ++i)
+        {
+            handle(vector[i]);
+        }
     }
+    catch (const std::exception & exception)
+    {
+        std::cerr << exception.what() << '\n';
+    }
+
+    return 0;
 }
