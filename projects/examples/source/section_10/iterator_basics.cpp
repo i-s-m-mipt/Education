f9c3e3d1c76876d46063bc75d52b0db2ca70c205
Ivan Makarov <i.s.m.mipt@yandex.ru>
2024-01-21 03:02:47 +0300

    Доработан пример iterator_basics.

--- projects/examples/source/section_10/iterator_basics.cpp
+++ projects/examples/source/section_10/iterator_basics.cpp
@@ -6,29 +6,29 @@
 
 int main()
 {
-	std::vector v({ 1, 2, 3, 4, 5 });
+	std::vector vector = { 1, 2, 3, 4, 5 };
 
-	using category_t = typename decltype(v)::iterator::iterator_category;
+	using category_t = typename decltype(vector)::iterator::iterator_category;
 
 	static_assert(std::is_same_v < category_t, std::random_access_iterator_tag > );
 
-	auto begin = v.begin(); // note: copy of begin iterator
+	auto begin = vector.begin(); // note: copy of begin iterator
 
 	std::advance(begin, 1); // good: better than += or -= in generic programming
 
 	assert(*begin == 2);
 
-	assert(*std::next(v.begin(), 2) == 3); // good: better than + in generic programming
-	assert(*std::prev(v.end  (), 2) == 4); // good: better than - in generic programming
+	assert(*std::next(vector.begin(), 2) == 3); // good: better than + in generic programming
+	assert(*std::prev(vector.end  (), 2) == 4); // good: better than - in generic programming
 
-	for (auto iterator = v.begin(); iterator != v.end(); ++iterator)
+	for (auto iterator = vector.begin(); iterator != vector.end(); ++iterator)
 	{
 		std::cout << *iterator << ' ';
 	}
 
 	std::cout << std::endl;
 
-	for (auto iterator = v.rbegin(); iterator != v.rend(); ++iterator) // note: reverse iterators
+	for (auto iterator = vector.rbegin(); iterator != vector.rend(); ++iterator) // note: reverse iterators
 	{
 		std::cout << *iterator << ' ';
 	}
