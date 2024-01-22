5dbb3ec3ec56bab9e3cae6c8b5589d8a47a19ba2
Ivan Makarov <i.s.m.mipt@yandex.ru>
2024-01-22 04:42:08 +0300

    Добавлен пример pattern_memento.

---
+++ projects/examples/source/section_05/pattern_memento.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+class Backup 
+{
+public:
+
+    explicit Backup(int data) : m_data(data) {}
+
+    [[nodiscard]] auto data() const { return m_data; }
+
+private:
+
+    int m_data;
+
+}; // class Backup 
+
+struct Computer 
+{
+    auto make_backup() const { return Backup(data); }
+
+    void load_backup(const Backup & backup) { data = backup.data(); }
+
+    int data;
+
+}; // class Computer 
+
+class Storage 
+{
+public:
+
+    void save(const Backup & backup) { m_backups.push_back(backup); }
+
+    auto load(int index) const { return m_backups.at(index); }
+
+private:
+
+    std::vector < Backup > m_backups;
+
+}; // class Storage 
+
+int main() 
+{
+    Computer computer; Storage storage;
+
+    computer.data = 1; storage.save(computer.make_backup());
+    computer.data = 2; 
+    computer.data = 3; storage.save(computer.make_backup());
+    computer.data = 4; 
+    computer.data = 5; storage.save(computer.make_backup());
+
+    computer.load_backup(storage.load(1));
+
+    std::cout << computer.data << std::endl;
+
+    return 0;
+}
