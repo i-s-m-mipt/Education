849dda6bbc0b7808b54fc79d0c0462b7f86195b0
Ivan Makarov <i.s.m.mipt@yandex.ru>
2024-01-25 02:49:13 +0300

    Доработан пример bitwise_operations.

--- projects/examples/source/section_09/bitwise_operations.cpp
+++ projects/examples/source/section_09/bitwise_operations.cpp
@@ -5,6 +5,7 @@
 #include <cstdint>
 #include <iomanip>
 #include <iostream>
+#include <limits>
 #include <span>
 #include <type_traits>
 
@@ -21,7 +22,7 @@ void print(const std::span < const std::byte > & bytes)
     {
         if (auto byte = std::to_integer < int > (bytes[i]); byte != 0)
         {
-            std::cout << std::hex << byte << ' ';
+            std::cout << std::hex << byte << ' '; // note: outputs 0x2a
         }
     }
         
@@ -30,7 +31,7 @@ void print(const std::span < const std::byte > & bytes)
 
 template < typename T > auto distance_in_bytes(T * first, T * last)
 {
-    return (reinterpret_cast < char * > (last) - reinterpret_cast < char * > (first));
+    return (std::bit_cast < std::byte * > (last) - std::bit_cast <std::byte * > (first));
 }
 
 struct Datetime
@@ -50,9 +51,9 @@ int main()
     std::cout << std::showbase;
 
     std::cout << 
-        std::oct << 42 << std::endl << 
-        std::dec << 42 << std::endl << 
-        std::hex << 42 << std::endl;
+        std::oct << 42 << std::endl << // note: outputs 052
+        std::dec << 42 << std::endl << // note: outputs 42
+        std::hex << 42 << std::endl;   // note: outputs 0x2a
 
     [[maybe_unused]] auto b = 0b101010; // note: binary
     [[maybe_unused]] auto o = 052;      // note: octal
@@ -63,13 +64,13 @@ int main()
     auto mask = 0x00f0;
     
     std::cout << 
-        (         mask) << std::endl << 
-        ( data        ) << std::endl << 
-        ( data |  mask) << std::endl << 
-        ( data & ~mask) << std::endl << 
-        ( data &  mask) << std::endl << 
-        ( data ^  mask) << std::endl << 
-        (~data        ) << std::endl;
+        (         mask) << std::endl << // note: outputs 0xf0
+        ( data        ) << std::endl << // note: outputs 0x1234
+        ( data |  mask) << std::endl << // note: outputs 0x12f4
+        ( data & ~mask) << std::endl << // note: outputs 0x1204
+        ( data &  mask) << std::endl << // note: outputs 0x30
+        ( data ^  mask) << std::endl << // note: outputs 0x12c4
+        (~data        ) << std::endl;   // note: outputs 0xffffedcb
 
     auto m = 0x123; assert((m << 1) == 0x246); // good: bit shift as multiplication
     auto n = -4000; assert((n >> 2) == -1000); // good: bit shift as multiplication
@@ -85,7 +86,7 @@ int main()
     bitset |= 0b0010; assert(bitset == 0b1111);
     bitset &= 0b0011; assert(bitset == 0b0011);
 
-    std::cout << std::bitset < 8 > (42) << std::endl;
+    std::cout << std::bitset < 8 > (42) << std::endl; // note: outputs 00101010
    
     assert(std::bitset < 8 > (        42).to_string() == "00101010");
     assert(std::bitset < 8 > ("00101010").to_ullong() ==         42);
@@ -101,15 +102,34 @@ int main()
 
     print(std::as_bytes(std::span < int > (array)));
 
-    auto ptr_int = &x; assert(*ptr_int == 42);
+    auto value = std::numeric_limits < unsigned int > ::max();
 
-    auto ptr_char = reinterpret_cast < char * > (ptr_int); // note: very dangerous
+    auto ptr_uint = &value;
 
-    std::cout << *ptr_char << std::endl;
+    auto ptr_char = std::bit_cast < std::byte * > (ptr_uint); // note: better than reinterpret_cast
+
+    for (std::size_t i = 0; i < sizeof(int); ++i)
+    {
+        std::cout << std::to_integer < int > (*(ptr_char + i)) << ' '; // note: 0xff 4 times
+    }
+
+    std::cout << std::endl;
 
     assert(&array[size - 1] - &array[0] == 9); // note: pointer arithmetic
 
     assert(distance_in_bytes(&array[0], &array[size - 1]) == 36);
+
+    using bin = std::bitset < 8 > ;
+
+    for (unsigned int i = 0; i < 10; ++i)
+    {
+        auto bc = std::bit_ceil (i);
+        auto bf = std::bit_floor(i);
+
+        std::cout << 
+             "ceil(" << bin(i) << ") = " << bin(bc) << ", " <<
+            "floor(" << bin(i) << ") = " << bin(bf) << '\n';
+    }
 
     return 0;
 }
