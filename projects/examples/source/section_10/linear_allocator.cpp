e0e47f1909a1a198ffcfe5aba42bb4dff0fd874e
Ivan Makarov <i.s.m.mipt@yandex.ru>
2024-01-25 19:17:26 +0300

    Доработан пример linear_allocator.

--- projects/examples/source/section_10/linear_allocator.cpp
+++ projects/examples/source/section_10/linear_allocator.cpp
@@ -12,40 +12,33 @@ public:
 
 	explicit Linear_Allocator(std::size_t size) : m_size(size)
 	{
-		m_begin = ::operator new(size);
+		m_begin = ::operator new(m_size);
 	}
 
-	~Linear_Allocator() noexcept
+	~Linear_Allocator() noexcept // note: final deallocation once
 	{
 		::operator delete(m_begin);
 	}
 
 	[[nodiscard]] void * allocate(std::size_t size, std::size_t alignment = alignof(std::max_align_t))
 	{
-		void * first = std::bit_cast < std::byte * > (m_begin) + m_offset;
+		void * first = std::bit_cast < std::byte * > (m_begin) + m_offset; // note: first free byte
 
 		auto space = m_size - m_offset;
 
-		if (first = std::align(alignment, size, first, space); first)
+		if (first = std::align(alignment, size, first, space); first) // note: modifies first and space
 		{
-			m_offset = m_size - space + size;
-
-			return first;
+			m_offset = m_size - space + size; 
+			
+			return first; // note: pointer aligned successfully
 		}
-		else return nullptr;
+		else return nullptr; // note: pointer not aligned
 	}
-
-	void deallocate(void *) noexcept {}
-
-	void clear() noexcept // note: manual operation
-	{ 
-		m_offset = 0; 
-	} 
 
 	void print() const
 	{
 		std::cout << m_begin << ": " << std::setw(std::size(std::to_string(m_size))) << 
-			std::right << std::setfill(' ') << m_offset << " / " << m_size << std::endl;
+			std::right << std::setfill('0') << m_offset << " / " << m_size << std::endl;
 	}
 
 private:
@@ -70,8 +63,6 @@ int main()
 
 	std::cout << allocator.allocate(988   ) << ' '; allocator.print();
 	std::cout << allocator.allocate(  1   ) << ' '; allocator.print(); // note: nullptr
-
-	allocator.clear();
 
 	return 0;
 }
