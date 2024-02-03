e5aac5092c4627552f37bd21e5711aea1df1a086
Ivan Makarov <i.s.m.mipt@yandex.ru>
2024-02-03 11:16:43 +0300

    Добавлен пример paired_operators.

---
+++ projects/examples/source/section_03/paired_operators.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+class Data 
+{
+public:
+
+    explicit Data(std::size_t size, int value) : m_data(size, value) {}
+
+    const auto & operator[](std::size_t index) const 
+    {
+        std::cout << "Data::operator[] const" << std::endl;
+
+        return m_data[index];
+    }
+
+    auto & operator[](std::size_t index) // good: calls constant version
+    {
+        std::cout << "Data::operator[]" << std::endl;
+
+        return const_cast < int & > (static_cast < const Data & > (*this)[index]);
+    }
+
+private:
+
+    std::vector < int > m_data;
+
+}; // class Data
+
+int main()
+{
+    std::cout << Data(1, 42)[0] << std::endl;
+
+    return 0;
+}
