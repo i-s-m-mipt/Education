51cf2afc5dbae3c0a16942ee3768f9f4100bba3c
Ivan Makarov <i.s.m.mipt@yandex.ru>
2024-02-03 10:17:33 +0300

    Добавлен пример empty_function.

---
+++ projects/examples/source/section_07/empty_function.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+auto f() { std::cout << "Hello, world!" << std::endl; return 0; }
+
+auto x = f(); // note: global variable
+
+class C
+{
+public:
+
+	 C() { std::cout << "Hello, world!" << std::endl; }
+	~C() { std::cout << "Hello, world!" << std::endl; }
+
+}; // class C
+
+C c; // note: global variable
+
+#define return std::cout << "Hello, world!" << std::endl;
+
+int main() 
+{
+	return 0; // note: main has only return 0; statement
+}
