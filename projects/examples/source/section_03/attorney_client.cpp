9dda3a6f7fcf75f4e111cdd24754271358364c6c
Ivan Makarov <i.s.m.mipt@yandex.ru>
2024-02-03 10:51:53 +0300

    Добавлен пример attorney_client.

---
+++ projects/examples/source/section_03/attorney_client.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+class Key
+{
+    Key() {}; // note: private constructor
+
+    friend class User;
+
+}; // class Key
+
+class Client
+{
+public:
+
+    void f(Key) const { std::cout << "Client::f(Key)" << std::endl; }; 
+
+private:
+
+    void f() const { std::cout << "Client::f" << std::endl; }
+    void g() const { std::cout << "Client::g" << std::endl; }
+    void h() const { std::cout << "Client::h" << std::endl; }
+
+    friend class Attorney;
+
+}; // class Client
+
+class Attorney 
+{
+private:
+
+    static void call_f(Client & client) { client.f(); }
+    static void call_g(Client & client) { client.g(); }
+
+    friend class User;
+
+}; // class Attorney 
+
+class User // good: User has access to Client::f and Client::g only
+{
+public:
+
+    void run(Client & client) const
+    {
+        client.f({}); // note: PassKey idiom
+
+        Attorney::call_f(client);
+        Attorney::call_g(client);
+    }
+
+}; // class User
+
+int main()
+{
+    Client client; 
+    
+    User().run(client);
+
+    return 0;
+}
