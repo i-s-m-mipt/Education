52186933119451b38f689ee3911fa01a7371cb5e
Ivan Makarov <i.s.m.mipt@yandex.ru>
2024-02-03 11:02:35 +0300

    Добавлен пример covariant_types.

---
+++ projects/examples/source/section_03/covariant_types.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string_view>
+
+class Base // note: polymorphic base class
+{
+public:
+
+	virtual ~Base() {}
+
+	virtual Base * get() { return this; }
+
+	void print() const { std::cout << "Base" << std::endl; }
+
+}; // class Base
+
+class Derived : public Base
+{
+public:
+
+	Derived * get() override { return this; }
+
+	void print() const { std::cout << "Derived" << std::endl; }
+
+}; // class Derived : public Base
+
+int main()
+{
+	auto d = new Derived; Base * b = d;
+
+	d->get()->print(); // note: outputs Derived
+	b->get()->print(); // note: outputs Base
+
+	delete b;
+
+	return 0;
+}
