1f40dabe66d79fa5de07da6a8d57e61a59e5dcc3
Matthias <i.s.m.mipt@yandex.ru>
2024-02-08 17:00:49 +0300

    Обновлен файл education.sh.

--- education.sh
+++ education.sh
@@ -1,14 +1,19 @@
+mkdir -p libraries/library_v1
+mkdir -p libraries/library_v2
+
 cd projects/library_v1
 mkdir -p output
 cd output
 cmake ..
 cmake --build .
+cp liblibrary_v1.a ../../../libraries/library_v1
 
 cd ../../../projects/library_v2
 mkdir -p output
 cd output
 cmake ..
 cmake --build .
+cp liblibrary_v2.so ../../../libraries/library_v2
 
 cd ../../../projects/examples
 mkdir -p output
