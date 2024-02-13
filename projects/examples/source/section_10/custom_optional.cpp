34aa9b32aa35a8fecd4a74d45c06b76e007048a6
Matthias <i.s.m.mipt@yandex.ru>
2024-02-13 14:54:52 +0300

    Добавлен пример custom_optional.

---
+++ projects/examples/source/section_10/custom_optional.cpp
@@ -0,0 +1,143 @@
+#include <cassert>
+#include <new>
+#include <utility>
+
+template < typename T > class Optional 
+{
+public:
+
+    Optional() noexcept = default;
+
+    explicit Optional(T value) { initialize(value); }
+
+    Optional(const Optional & other) 
+    {
+        if (other.has_value()) 
+        {
+            initialize(*other.m_ptr);
+        }
+    }
+
+    Optional(Optional && other) noexcept : m_ptr(other.m_ptr)
+    {
+        other.m_ptr = nullptr;
+    }
+
+    Optional & operator=(const Optional & other) 
+    {
+        if (this != &other)
+        {
+            if (destroy(); other.has_value()) 
+            {
+                construct(*other.m_ptr);
+            } 
+            else deallocate();
+        }
+
+        return *this;
+    }
+
+    Optional & operator=(Optional && other)
+    {
+        if (this != &other)
+		{
+            uninitialize();
+
+            m_ptr = other.m_ptr;
+
+            other.m_ptr = nullptr;
+		}
+
+		return *this;
+    } 
+
+    Optional & operator=(T value) 
+    {
+        destroy(); construct(value); return *this;
+    }
+
+    ~Optional() noexcept { uninitialize(); }
+
+private:
+
+    void initialize(T value) { allocate(); construct(value); }
+
+    void allocate() 
+    { 
+        m_ptr = static_cast < T * > (::operator new(sizeof(T), std::align_val_t(alignof(T)))); 
+    }
+
+    void construct(T value) 
+    {
+        if (!m_ptr) allocate(); 
+        
+        new(m_ptr) T(value); 
+    }
+
+    void uninitialize() { destroy(); deallocate(); }
+
+    void destroy() noexcept { if (m_ptr) m_ptr->~T(); }
+
+    void deallocate() noexcept
+    { 
+        ::operator delete(m_ptr, std::align_val_t(alignof(T))); 
+        
+        m_ptr = nullptr;
+    }
+
+public:
+
+    [[nodiscard]] explicit operator bool() const noexcept 
+    { 
+        return m_ptr; 
+    }
+
+    [[nodiscard]] bool      has_value () const noexcept { return  m_ptr; }
+
+    [[nodiscard]]       T * operator->()       noexcept { return  m_ptr; }
+    [[nodiscard]] const T * operator->() const noexcept { return  m_ptr; }
+
+    [[nodiscard]]       T & operator* ()       noexcept { return *m_ptr; }
+    [[nodiscard]] const T & operator* () const noexcept { return *m_ptr; }
+
+    [[nodiscard]]       T & value     ()       noexcept { return *m_ptr; }
+    [[nodiscard]] const T & value     () const noexcept { return *m_ptr; }
+
+    [[nodiscard]] T value_or(T value) const noexcept 
+    { 
+        return (m_ptr ? *m_ptr : value); 
+    }
+
+private:
+
+    T * m_ptr = nullptr;
+
+}; // template < typename T > class Optional 
+
+int main()
+{
+    Optional < int > o1; assert(!o1.has_value());
+
+    assert(o1.value_or(42) == 42);
+
+    Optional < int > o2(42); assert(o2.has_value() && *o2 == 42);
+    Optional < int > o3(o2); assert(o3.has_value() && *o3 == 42);
+
+    Optional < int > o4(std::move(o3));
+
+    assert(o4.has_value() && *o4 == 42 && !o3.has_value());
+
+    o3 = o2; 
+    
+    assert(o3.has_value() && *o3 == 42);
+
+    o4 = std::move(o3);
+
+    assert(o4.has_value() && *o4 == 42 && !o3.has_value());
+
+    o4 = 43;
+
+    assert(o4.has_value() && *o4 == 43);
+
+    return 0;
+}
