412fbc0d98e1394683adb1c82066178f51430908
Matthias <i.s.m.mipt@yandex.ru>
2024-02-20 17:14:20 +0300

    Добавлен пример advanced_calculator.

---
+++ projects/examples/source/section_14/advanced_calculator.cpp
@@ -0,0 +1,134 @@
+#include <cassert>
+#include <exception>
+#include <iostream>
+#include <list>
+#include <stdexcept>
+#include <string>
+#include <variant>
+
+using namespace std::literals;
+
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/spirit/home/x3.hpp>
+#include <boost/spirit/home/x3/support/ast/variant.hpp> // note: abstract syntax tree
+
+namespace detail
+{
+    struct Sign;
+    struct List;
+
+    struct Operand : public boost::spirit::x3::variant < double, 
+        boost::spirit::x3::forward_ast < Sign > , 
+        boost::spirit::x3::forward_ast < List > > // note: pattern visitor
+    {
+        using base_type::base_type;
+        using base_type::operator=;
+
+    }; // struct Operand : boost::spirit::x3::variant < ... >
+
+    struct Sign { char c{}; Operand operand; };
+    struct Step { char c{}; Operand operand; };
+
+    struct List
+    {
+        Operand head;
+
+        std::list < Step > tail;
+
+    }; // struct List
+
+} // namespace detail
+
+BOOST_FUSION_ADAPT_STRUCT(::detail::Sign, c, operand)
+BOOST_FUSION_ADAPT_STRUCT(::detail::Step, c, operand)
+BOOST_FUSION_ADAPT_STRUCT(::detail::List, head, tail)
+
+struct Calculator
+{
+    [[nodiscard]] double operator()(double n) const noexcept { return n; }
+
+    [[nodiscard]] double operator()(const detail::Sign & sign) const
+    {
+        switch (auto rhs = boost::apply_visitor(*this, sign.operand); sign.c)
+        {
+            case '+': return        rhs;
+            case '-': return -1.0 * rhs;
+
+            default: throw std::runtime_error("invalid sign: "s + sign.c);
+        }
+    }
+
+    [[nodiscard]] double operator()(const detail::Step & step, double lhs) const
+    {
+        switch (auto rhs = boost::apply_visitor(*this, step.operand); step.c)
+        {
+            case '+': return lhs + rhs;
+            case '-': return lhs - rhs;
+            case '*': return lhs * rhs;
+            case '/': return lhs / rhs;
+
+            default: throw std::runtime_error("invalid step: "s + step.c);
+        }
+    }
+
+    [[nodiscard]] double operator()(const detail::List & list) const
+    {
+        auto state = boost::apply_visitor(*this, list.head);
+        
+        for (const auto & step : list.tail)
+        {
+            state = (*this)(step, state);
+        }
+
+        return state;
+    }
+
+}; // struct Calculator
+
+namespace grammar
+{
+    const boost::spirit::x3::rule < class E_tag, detail::List    > expression;
+    const boost::spirit::x3::rule < class T_tag, detail::List    > term;
+    const boost::spirit::x3::rule < class P_tag, detail::Operand > primary;
+
+    const auto expression_def = term >> *(   
+        (boost::spirit::x3::char_('+') >> term) | 
+        (boost::spirit::x3::char_('-') >> term));
+
+    const auto term_def = primary >> *(
+        (boost::spirit::x3::char_('*') >> primary) | 
+        (boost::spirit::x3::char_('/') >> primary));
+
+    const auto primary_def = boost::spirit::x3::double_ | 
+            '(' >> expression >> ')' |   
+        (boost::spirit::x3::char_('-') >> primary) | 
+        (boost::spirit::x3::char_('+') >> primary);
+
+    BOOST_SPIRIT_DEFINE(expression, term, primary);
+
+    auto arithmetic = expression;
+
+} // namespace grammar
+
+[[nodiscard]] int test(const std::string & input)
+{
+    auto begin = std::begin(input), end = std::end(input);
+
+    detail::List list;
+
+    auto result = boost::spirit::x3::phrase_parse(begin, end, 
+        grammar::arithmetic, boost::spirit::x3::ascii::space, list);
+
+    if (!result || begin != end) throw std::runtime_error("invalid input");
+
+    Calculator calculator;
+
+    return calculator(list);
+}
+
+int main()
+{
+    assert(test("1+1") == 2.0);
+
+    return 0;
+}
