affc0a37f8c56563fcc982cc92d6b09fb5d50256
Matthias <i.s.m.mipt@yandex.ru>
2024-02-20 14:29:59 +0300

    Добавлен пример parser_for_structs.

---
+++ projects/examples/source/section_14/parser_for_structs.cpp
@@ -0,0 +1,50 @@
+#include <cassert>
+#include <exception>
+#include <iterator>
+#include <stdexcept>
+#include <string>
+
+using namespace std::literals;
+
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/spirit/home/x3.hpp>
+
+struct Data { char c{}; int i{}; double d{}; }; 
+
+BOOST_FUSION_ADAPT_STRUCT(Data, c, i, d) // note: global scope
+
+namespace parser
+{
+    const boost::spirit::x3::rule < class data_tag, Data > data;
+
+    const auto quote = '\'', separator = ',';
+
+    auto const data_def = '{' >> 
+        boost::spirit::x3::lexeme[ quote >> 
+        boost::spirit::x3::char_ - quote >> quote ] >> separator >> 
+        boost::spirit::x3::int_                     >> separator >> 
+        boost::spirit::x3::double_ >> '}';
+
+    BOOST_SPIRIT_DEFINE(data);
+
+} // namespace parser
+
+int main()
+{
+    auto input = R"({'a',100,1.0})"s;
+
+    auto begin = std::begin(input);
+    auto   end = std::  end(input);
+
+    Data data;
+
+    auto result = boost::spirit::x3::parse(begin, end, parser::data, data);
+
+    if (!result || begin != end) throw std::runtime_error("invalid phrase");
+
+    assert(data.c == 'a');
+    assert(data.i == 100);
+    assert(data.d == 1.0);
+
+    return 0;
+}
