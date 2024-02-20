4662ccbe58bc8dd8ad4dcae3cfcb10417923ac78
Matthias <i.s.m.mipt@yandex.ru>
2024-02-20 17:14:07 +0300

    Доработан пример boost_spirit_basics.

--- projects/examples/source/section_14/boost_spirit_basics.cpp
+++ projects/examples/source/section_14/boost_spirit_basics.cpp
@@ -18,21 +18,15 @@ int main()
     
     auto input_1 = "42"s;
 
-    auto begin_1 = std::begin(input_1);
-    auto   end_1 = std::  end(input_1);
-
-    boost::spirit::x3::parse(begin_1, end_1, boost::spirit::x3::int_, value);
+    boost::spirit::x3::parse(std::begin(input_1), std::end(input_1), boost::spirit::x3::int_, value);
 
     assert(value == 42);
 
     auto input_2 = "42 42"s;
 
-    auto begin_2 = std::begin(input_2);
-    auto   end_2 = std::  end(input_2);
-
     std::pair < int, int > pair;
 
-    boost::spirit::x3::phrase_parse(begin_2, end_2,
+    boost::spirit::x3::phrase_parse(std::begin(input_2), std::end(input_2), 
         boost::spirit::x3::int_ >> 
         boost::spirit::x3::int_, 
         boost::spirit::x3::space, pair); // note: skip space symbols
@@ -42,12 +36,9 @@ int main()
 
     auto input_3 = "(42, 42)"s;
 
-    auto begin_3 = std::begin(input_3);
-    auto   end_3 = std::  end(input_3);
-
     std::tuple < int, int > tuple;
 
-    boost::spirit::x3::phrase_parse(begin_3, end_3, '(' >> 
+    boost::spirit::x3::phrase_parse(std::begin(input_3), std::end(input_3), '(' >> 
         boost::spirit::x3::int_ >> ',' >> 
         boost::spirit::x3::int_ >> ')', 
         boost::spirit::x3::space, tuple); // note: skip space symbols
@@ -57,15 +48,12 @@ int main()
 
     auto input_4 = "42"s;
 
-    auto begin_4 = std::begin(input_4);
-    auto   end_4 = std::  end(input_4);
-
     auto f = [](auto & context)
     { 
         std::cout << boost::spirit::x3::_attr(context) << std::endl; 
     };
 
-    boost::spirit::x3::parse(begin_4, end_4, boost::spirit::x3::int_[f]);
+    boost::spirit::x3::parse(std::begin(input_4), std::end(input_4), boost::spirit::x3::int_[f]);
 
     return 0;
 }
