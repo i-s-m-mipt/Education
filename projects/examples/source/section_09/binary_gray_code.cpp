5ee7fbde9b1fcac39f47d2fde626de6e88ea4394
Matthias <i.s.m.mipt@yandex.ru>
2024-02-24 23:52:32 +0300

    Добавлен пример gray_binary_code.

---
+++ projects/examples/source/section_09/binary_gray_code.cpp
@@ -0,0 +1,39 @@
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+unsigned int gray_encode(unsigned int n)
+{
+    return n ^ (n >> 1);
+}
+
+unsigned int gray_decode(unsigned int code)
+{
+    for (unsigned int bit = 1 << 31; bit > 1; bit >>= 1)
+    {
+        if (code & bit) code ^= bit >> 1;
+    }
+
+    return code;
+}
+
+int main()
+{
+    const std::size_t size = 4;
+
+    using binary = std::bitset < size > ;
+
+    for (unsigned int n = 0; n < static_cast < unsigned int > (std::pow(2, size)); ++n)
+    {
+        auto x = gray_encode(n);
+        auto y = gray_decode(x);
+
+        assert(n == y);
+
+        std::cout << std::setw(2) << std::right << std::setfill('0') << n << ' ' << binary(x) << std::endl;
+    }
+
+    return 0;
+}
