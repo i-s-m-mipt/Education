4cbe2530dabff6101b5b288c8f81bc52fc61dfba
Matthias <i.s.m.mipt@yandex.ru>
2024-03-04 05:37:26 +0300

    Доработан пример insert_iterators.

--- projects/examples/source/section_12/insert_iterators.cpp
+++ projects/examples/source/section_12/insert_iterators.cpp
@@ -12,19 +12,19 @@ int main()
 
 	std::deque < int > deque; // note: std::deque has push_front member function
 
-	std::copy(std::cbegin(vector), std::cend(vector), std::front_inserter(deque));
+	std::ranges::copy(vector, std::front_inserter(deque));
 
 	assert(std::size(deque) == std::size(vector));
 
 	std::list < int > list; // note: std::list has push_back member function
 
-	std::copy(std::cbegin(vector), std::cend(vector), std::back_inserter(list));
+	std::ranges::copy(vector, std::back_inserter(list));
 
 	assert(std::size(list) == std::size(vector));
 
 	std::set < int > set; // note: std::set has only insert member function
 
-	std::copy(std::cbegin(vector), std::cend(vector), std::inserter(set, std::begin(set)));
+	std::ranges::copy(vector, std::inserter(set, std::begin(set)));
 
 	assert(std::size(set) == std::size(vector));
 
