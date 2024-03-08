6dfa5eb15f0845624bcb7d7e70e98b36133197b2
Matthias <i.s.m.mipt@yandex.ru>
2024-03-09 00:08:39 +0300

    Доработан пример python_caller.

--- projects/examples/source/section_18/python_caller.cpp
+++ projects/examples/source/section_18/python_caller.cpp
@@ -1,11 +1,44 @@
+#include <cstdint>
 #include <exception>
 #include <iostream>
+#include <iterator>
+#include <random>
+#include <set>
 #include <stdexcept>
 #include <string>
+#include <unordered_set>
 
 #include "python_wrapper.hpp"
 
 using Python = solution::shared::Python;
+
+std::set < std::string > make_random_words(std::size_t size, const std::size_t length)
+{
+    std::mt19937 engine(std::random_device{}());
+
+	std::uniform_int_distribution <> distribution(97, 122);
+	
+	std::set < std::string > words;
+    
+	for (std::string word(length, '_'); std::size(words) < size; words.insert(word))
+    {
+        for (auto & letter : word) letter = distribution(engine);
+    }
+
+	return words;
+}
+
+std::size_t hash_DEK(const std::string & string) // note: Donald E. Knuth implementation
+{
+	std::uint32_t hash = std::size(string); // note: as with x86 build to make collisions
+
+	for (auto letter : string)
+	{
+		hash = ((hash << 5) ^ (hash >> 27)) ^ letter;
+	}
+
+	return hash;
+}
 
 int main()
 {
@@ -18,6 +51,27 @@ int main()
 		
 		std::cout << boost::python::extract < std::string > (
             python.global()["factorial"](100))() << std::endl;
+
+        const std::size_t size = 5'000'001, length = 10, step = 100'000;
+
+        std::unordered_set < std::size_t > hashes; std::size_t index = 0; std::string points;
+
+        for (const auto & word : make_random_words(size, length))
+        {
+            if (hashes.insert(hash_DEK(word)); index++ % step == 0)
+            {
+                points += 
+                    std::to_string(index - 1                ) + ',' + 
+                    std::to_string(index - std::size(hashes)) + ',';
+            }
+        }
+
+        points.pop_back(); // note: remove last comma in string
+
+        boost::python::exec("from python_script import make_plot", 
+            python.global(), python.global());
+		
+		python.global()["make_plot"](points.c_str(), "DEK");
     }
     catch ([[maybe_unused]] const boost::python::error_already_set & error)
 	{
--- projects/examples/source/section_18/python_script.py
+++ projects/examples/source/section_18/python_script.py
@@ -1,3 +1,23 @@
-import math
+import math, os
+
+import matplotlib.pyplot as plot
 
 def factorial(n: int) -> str: return str(math.factorial(n))
+
+def make_plot(raw_points: str, label: str):
+
+    points = [float(coordinate) for coordinate in raw_points.split(',')]
+
+    x = points[ ::2]
+    y = points[1::2]
+
+    plot.plot(x, y, label = label)
+
+    plot.xlabel("words")
+    plot.ylabel("collisions")
+
+    plot.grid()
+    plot.legend()
+
+    plot.savefig(os.path.abspath(os.path.dirname(os.path.abspath(__file__))))
+    plot.show()
