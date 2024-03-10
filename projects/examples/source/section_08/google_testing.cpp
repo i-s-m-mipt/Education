c74bf94e57ed52c7725a89300a855eab9853a949
Matthias <i.s.m.mipt@yandex.ru>
2024-03-10 22:55:18 +0300

    Доработан пример google_testing.

--- projects/examples/source/section_08/google_testing.cpp
+++ projects/examples/source/section_08/google_testing.cpp
@@ -1,61 +1,68 @@
 #include <iterator>
+#include <limits>
 #include <vector>
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-unsigned int factorial(unsigned int n)
-{
-	return (n < 2 ? 1 : n * factorial(n - 1));
-}
+unsigned int factorial(unsigned int n) {  return (n < 2 ? 1 : n * factorial(n - 1)); }
 
-TEST(factorial_test, zero) 
+TEST(Factorial, Results) // note: group name and test case name
 {
-    EXPECT_EQ(factorial(0), 1);
-}
-
-TEST(factorial_test, positive) 
-{
-    EXPECT_EQ(factorial(1), 1);
-    EXPECT_EQ(factorial(2), 2);
-    EXPECT_EQ(factorial(3), 6);
-    EXPECT_EQ(factorial(5), 120);
+    EXPECT_EQ(factorial(0),     1);
+    EXPECT_EQ(factorial(1),     1);
+    EXPECT_EQ(factorial(2),     2);
+    EXPECT_EQ(factorial(3),     6);
+    EXPECT_EQ(factorial(5),   120);
     EXPECT_EQ(factorial(8), 40320);
+
+    ASSERT_EQ(factorial(4), 25); // note: failed assertion
 }
 
-TEST(factorial_test, fail) 
+testing::AssertionResult is_even(int n) 
 {
-    ASSERT_EQ(factorial(4), 25); // note: failed test
+    return (n % 2 == 0 ? testing::AssertionSuccess() : 
+                         testing::AssertionFailure() << n << " is odd");
 }
 
-class Test_Fixture : public testing::Test 
+TEST(Miscellaneous, Expectations) 
+{ 
+    EXPECT_TRUE(is_even(7)); // note: failed expectation
+
+    EXPECT_THAT("Hello, world!", testing::StartsWith("Hello"));
+
+    EXPECT_THAT("data.txt", testing::MatchesRegex("([a-z]+)\\.txt"));
+
+    EXPECT_DOUBLE_EQ(3.14, 3.15) << "not equal doubles";
+
+    EXPECT_NEAR(3.14, 3.14, std::numeric_limits < double > ::epsilon());
+} 
+
+class Fixture : public testing::Test 
 {
-protected:
+public:
 
-     Test_Fixture() noexcept {} // note:   setup actions
-    ~Test_Fixture() noexcept {} // note: cleanup actions
+     Fixture() noexcept {} // note:   setup actions
+    ~Fixture() noexcept {} // note: cleanup actions
 
-    void SetUp   () override // note: called  after constructor
-    {
-        data.push_back(42);
-    }
-
-    void TearDown() override // note: called before  destructor
-    {
-        data.clear();
-    } 
+    void    SetUp() override { data.push_back(42); } // note: called after constructor
+    void TearDown() override { data.clear      (); } // note: called before destructor
 
     std::vector < int > data;
 
 }; // class Test_Fixture : public testing::Test 
 
-TEST_F(Test_Fixture, test) 
+TEST_F(Fixture, Size) 
 {
-    ASSERT_EQ(std::size(data), 1);
-
-    data.push_back(43);
-
+    ASSERT_EQ(std::size(data), 1); data.push_back(43);
     ASSERT_EQ(std::size(data), 2);
 }
+
+class Parameters : public Fixture, public testing::WithParamInterface < int > {};
+
+TEST_P(Parameters, Resize) { data.resize(GetParam(), 0); ASSERT_EQ(std::size(data), GetParam()); }
+
+INSTANTIATE_TEST_CASE_P(Test, Parameters, testing::Values(0, 1, 2, 4, 8)); // note: testing::Range(0, 10)
 
 int main(int argc, char ** argv) // note: arguments for testing
 {
