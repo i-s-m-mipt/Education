40ff302b9fb9384fba9ae14b5abe03d63a393a98
Matthias <i.s.m.mipt@yandex.ru>
2024-03-11 03:35:56 +0300

    Доработан пример metaprogramming.

--- projects/examples/source/section_04/metaprogramming.cpp
+++ projects/examples/source/section_04/metaprogramming.cpp
@@ -3,8 +3,7 @@
 template < auto N > struct Factorial
 {
 	static const auto value = N * Factorial < N - 1 > ::value;
-
-}; // template < auto N > struct Factorial
+};
 
 template <> struct Factorial < 0 > { static const auto value = 1; };
 
@@ -13,20 +12,17 @@ template < auto N > const auto factorial_v = Factorial < N > ::value;
 template < auto P, auto D > struct Check_Is_Prime
 {
 	static const bool value = (P % D != 0) && Check_Is_Prime < P, D - 1 > ::value;
-
-}; // template < auto P, auto D > struct Check_Is_Prime
+}; 
 
 template < auto P > struct Check_Is_Prime < P, 2 >
 {
 	static const bool value = (P % 2 != 0);
-
-}; // template < auto P > struct Check_Is_Prime < P, 2 >
+}; 
 
 template < auto P > struct Is_Prime
 {
 	static const bool value = Check_Is_Prime < P, P / 2 > ::value;
-
-}; // template < auto p > struct Is_Prime
+}; 
 
 template <> struct Is_Prime < 0 > {	static const bool value = false; };
 template <> struct Is_Prime < 1 > { static const bool value = false; };
