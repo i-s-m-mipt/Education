4157a528af0b8257b3a28778f52f4790f2d586fb
Matthias <i.s.m.mipt@yandex.ru>
2024-03-14 15:29:38 +0300

    Добавлен пример variadic_output.

---
+++ projects/examples/source/section_13/variadic_output.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <ostream>
+
+template < typename T > class Spaced
+{
+public:
+
+    explicit Spaced(const T & reference) noexcept : m_reference(reference) {}
+
+    friend std::ostream & operator<<(std::ostream & stream, Spaced < T > spaced)
+    {
+        return (stream << spaced.m_reference << ' ');
+    }
+
+private:
+
+    const T & m_reference;
+
+}; // template < typename T > class Spaced
+
+template < typename ... Types > void print_v1(const Types & ... args)
+{
+    (std::cout << ... << args) << std::endl; // note: fold expression with no spaces
+}
+
+template < typename ... Types > void print_v2(const Types & ... args)
+{
+    (std::cout << ... << Spaced(args)) << std::endl; // note: fold expression with spaces
+}
+
+int main()
+{
+    print_v1('a', 42, 3.14); // note: outputs a423.14
+    print_v2('a', 42, 3.14); // note: outputs a 42 3.14
+
+    return 0;
+}
