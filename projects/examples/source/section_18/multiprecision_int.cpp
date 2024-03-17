c26349a908a403e098775daa012afdb1202a35f3
Matthias <i.s.m.mipt@yandex.ru>
2024-03-17 13:45:29 +0300

    Доработан пример multiprecision_int.

--- projects/examples/source/section_18/multiprecision_int.cpp
+++ projects/examples/source/section_18/multiprecision_int.cpp
@@ -11,7 +11,7 @@ int main()
     std::cout << std::numeric_limits < boost::multiprecision::uint256_t > ::max() << std::endl;
     std::cout << std::numeric_limits < boost::multiprecision::uint512_t > ::max() << std::endl;
 
-    std::cout << boost::multiprecision::uint128_t(0) - 1 << std::endl;
+    std::cout << boost::multiprecision::uint128_t(0) - 1 << std::endl; // note: outputs same as max()
 
     try
     {
@@ -24,7 +24,7 @@ int main()
 
     boost::multiprecision::cpp_int result = 1; for (auto i = 1; i < 101; ++i) result *= i; 
    
-    std::cout << result << std::endl;
+    std::cout << result << std::endl; // note: outputs 100!
 
     return 0;
 }
