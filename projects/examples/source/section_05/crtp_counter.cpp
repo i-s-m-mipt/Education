6d521b4abc3ca55bd39621d9256ee894348e17cd
Matthias <i.s.m.mipt@yandex.ru>
2024-03-19 14:01:32 +0300

    Доработан пример crtp_counter.

--- projects/examples/source/section_05/crtp_counter.cpp
+++ projects/examples/source/section_05/crtp_counter.cpp
@@ -12,7 +12,7 @@ protected:
 	{
 		if (++counter > M)
 		{
-			std::cout << "Error: too many objects" << std::endl;
+			std::cerr << "too many objects\n"; // good: unbuffered output stream
 		}
 	}
 
@@ -28,18 +28,15 @@ private:
 
 }; // template < typename T > class Counter
 
-template < typename T >
-class Container_1 : public Counter < Container_1 < T > , 1 > {};
-
-template < typename T >
-class Container_2 : public Counter < Container_2 < T > , 2 > {};
+template < typename T > class Container_1 : public Counter < Container_1 < T > , 1 > {};
+template < typename T > class Container_2 : public Counter < Container_2 < T > , 2 > {};
 
 int main()
 {
-	Container_1 < int > ci_1;
-	Container_1 < int > ci_2(ci_1); // note: second instance
+	const Container_1 < int > ci_1;
+	const Container_1 < int > ci_2(ci_1); // note: second instance
 
-	Container_2 < int > ci_3;
+	const Container_2 < int > ci_3;
 
 	std::cout << Container_1 < int > ::get_counter() << std::endl;
 	std::cout << Container_2 < int > ::get_counter() << std::endl;
