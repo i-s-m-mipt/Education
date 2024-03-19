0b7b31f406220bba59b1314c429ee6a9d3aa6f57
Matthias <i.s.m.mipt@yandex.ru>
2024-03-19 17:30:35 +0300

    Доработан пример pimpl_idiom.

--- projects/examples/source/section_07/pimpl_idiom.cpp
+++ projects/examples/source/section_07/pimpl_idiom.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <utility>
 
 #include <boost/noncopyable.hpp>
 
@@ -7,19 +8,19 @@ class Computer : private boost::noncopyable // note: placed in the header file
 {
 public:
 
-    explicit Computer(std::string name); // note: definition in source file
+    constexpr explicit Computer(std::string name); // note: definition in source file
 
-    Computer(Computer && other) : m_pimpl(other.m_pimpl)
+    constexpr Computer(Computer && other) : m_pimpl(other.m_pimpl)
     {
         other.m_pimpl = nullptr;
     }
 
-    Computer & operator=(Computer && other)
+    constexpr Computer & operator=(Computer && other)
     {
-        m_pimpl = other.m_pimpl; other.m_pimpl = nullptr; return *this;
+        m_pimpl = std::exchange(other.m_pimpl, nullptr); return *this;
     }
 
-    ~Computer();
+    constexpr ~Computer();
 
     void run() const; // note: call forwarded to implementation, no inline abilities
 
@@ -27,7 +28,7 @@ private:
 
     class Implementation; // note: implemetation hidden in the source file
 
-    Implementation * m_pimpl; // note: use std::experimental::propagate_const
+    const Implementation * m_pimpl; // note: use std::experimental::propagate_const
 
 }; // class Computer : private boost::noncopyable
 
@@ -35,19 +36,19 @@ class Computer::Implementation // note: placed in the source file
 {
 public:
 
-    explicit Implementation(std::string name) : m_name(std::move(name)) {}
+    constexpr explicit Implementation(std::string name) : m_name(std::move(name)) {}
 
     void run() const { std::cout << m_name << std::endl; }
 
 private:
 
-    std::string m_name;
+    const std::string m_name;
 
 }; // class Computer::Implementation
 
-Computer::Computer(std::string name) : m_pimpl(new Implementation(std::move(name))) {}
+constexpr Computer:: Computer(std::string name) : m_pimpl(new Implementation(std::move(name))) {}
 
-Computer::~Computer() { delete m_pimpl; }
+constexpr Computer::~Computer() { delete m_pimpl; }
 
 void Computer::run() const { m_pimpl->run(); }
 
