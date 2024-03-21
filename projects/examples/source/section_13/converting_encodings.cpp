a4cec99d38286262950a923faf11abd29cd18b3e
Matthias <i.s.m.mipt@yandex.ru>
2024-03-21 23:36:58 +0300

    Доработан пример converting_encodings.

--- projects/examples/source/section_13/converting_encodings.cpp
+++ projects/examples/source/section_13/converting_encodings.cpp
@@ -1,6 +1,7 @@
 #include <exception>
 #include <iomanip>
 #include <iostream>
+#include <ostream>
 #include <stdexcept>
 #include <string>
 
@@ -17,76 +18,56 @@
 	return boost::locale::conv::to_utf < char > (string, locale);
 }
 
+[[nodiscard]] std::string convert_utf_to_locale(const std::string & string)
+{
+	boost::locale::generator generator;
+
+	generator.locale_cache_enabled(true);
+
+	const auto locale = generator(boost::locale::util::get_system_locale());
+
+	return boost::locale::conv::from_utf < char > (string, locale);
+}
+
 int main()
 {
-	try
-	{
-		std::string string; std::cin >> string;
+    std::cout << boost::locale::util::get_system_locale() << std::endl;
 
-        const std::string u8string = convert_locale_to_utf(string); // note: for Windows cp1251
+	std::string string; std::cin >> string; // note: can be not UTF-8, cp1251 on Windows
 
-		std::cout << std::hex << 
-            (static_cast < int > (  string[0]) & 0xFF) << ' ' <<
-            (static_cast < int > (  string[1]) & 0xFF) << ' ' <<
-			(static_cast < int > (u8string[0]) & 0xFF) << ' ' <<
-			(static_cast < int > (u8string[1]) & 0xFF) << std::dec << std::endl;
+    auto u8string = convert_locale_to_utf(string);
 
-        std::cout <<   string << ' ' <<   string.length() << std::endl;
-		std::cout << u8string << ' ' << u8string.length() << std::endl;
+	std::cout << std::hex << 
+        (static_cast < int > (  string[0]) & 0xFF) << ' ' <<
+        (static_cast < int > (  string[1]) & 0xFF) << ' ' <<
+		(static_cast < int > (u8string[0]) & 0xFF) << ' ' <<
+		(static_cast < int > (u8string[1]) & 0xFF) << std::dec << std::endl;
 
-		for (const auto c : u8string) std::cout << c << ' ';
+    std::cout <<   string << ' ' <<   string.length() << std::endl;
+    std::cout << u8string << ' ' << u8string.length() << std::endl;
 
-		std::cout << std::endl;
+    const auto u16string = boost::locale::conv::utf_to_utf < char16_t, char > (u8string);
+    const auto u32string = boost::locale::conv::utf_to_utf < char32_t, char > (u8string);
 
-	//	std::u16string u16string = 
-	//		boost::locale::conv::utf_to_utf < char16_t, char > (u8string);
+//  SetConsoleOutputCP(65001); // note: Windows only, change console code page to UTF-8
 
-	//	for (auto c : u16string)
-	//	{
-	//		std::cout << c << ' ';
-	//	}
-	//		
-	//	std::cout << std::endl;
+	for (const auto c :  u8string) std::cout << static_cast < int > (c) << ' '; // note: no support
 
-	//	std::u32string u32string = 
-	//		boost::locale::conv::utf_to_utf < char32_t, char > (u8string);
-	//	
-	//	for (auto c : u32string)
-	//	{
-	//		std::cout << c << ' ';
-	//	}
+	std::cout << std::endl;
 
-	//	std::cout << std::endl;
+	for (const auto c : u16string) std::cout << static_cast < int > (c) << ' '; // note: no support
+			
+	std::cout << std::endl;
 
-	//	string = boost::locale::conv::utf_to_utf < char, char32_t > (u32string);
+	for (const auto c : u32string) std::cout << static_cast < int > (c) << ' '; // note: no support
 
-	//	std::cout << string << std::endl;
+	std::cout << std::endl;
 
-	//	std::u16string u16string_cmp = u"ïðèâåò";
+	u8string = boost::locale::conv::utf_to_utf < char, char32_t > (u32string);
 
-	//	for (auto c : u16string_cmp)
-	//	{
-	//		std::cout << c << ' ';
-	//	}
+	std::cout << convert_utf_to_locale(u8string) << std::endl;
 
-	//	std::cout << std::endl;
-
-	//	std::cout << std::boolalpha << (u16string == u16string_cmp) << std::endl;
-
-	//	string = u8"ÝÒÎ ÊÎÄÈÐÎÂÊÀ UNICODE UTF-8! \x00AA \x00BB \x00CC \x00DD \x00EE \x00FF";
-
-	//	std::cout << string << ' ' << string.length() << std::endl;
-
-	//	string = convert_utf_to_locale(string);
-
-	//	system("chcp 1251");
-
-	//	std::cout << string << ' ' << string.length() << std::endl;
-	}
-	catch (const std::exception & exception)
-	{
-		std::cerr << exception.what() << '\n';
-	}
+	std::cout << convert_utf_to_locale("∃y ∀x ¬(x ≺ y)") << std::endl; // note: problem with char8_t 
 
 	return 0;
 }
