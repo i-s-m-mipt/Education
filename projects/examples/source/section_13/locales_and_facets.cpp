41bf324a08e587b2954f054ad1627003ca14dd5e
Matthias <i.s.m.mipt@yandex.ru>
2024-03-23 01:52:36 +0300

    Доработан пример locales_and_facets.

--- projects/examples/source/section_13/locales_and_facets.cpp
+++ projects/examples/source/section_13/locales_and_facets.cpp
@@ -1,27 +1,66 @@
+#include <chrono>
+#include <cstring>
 #include <iostream>
+#include <iterator>
 #include <locale>
 
 int main()
 {
-	std::cout.imbue(std::locale("ru_RU.utf8")); // note: use locale -a to list all installed locales
+	const auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 
-	for (double value{}; std::cin >> value; ) std::cout << value << std::endl; // note: . -> ,
+	const auto data = std::localtime(&time);
+
+	const auto format = "%A %x"; // note: weekday followed by date
 	
-	const std::locale locale_1("en_US.utf8"); 
+	const std::locale locale_1("en_US.utf8"); // note: use locale -a to list all installed locales
     
     std::cout.imbue(locale_1);
 
 	std::cout << 1'000'000'000 << std::endl; // note: separator is comma
 
-	std::cout << std::use_facet < std::numpunct < char > > (locale_1).thousands_sep() << std::endl;
+	std::cout << std::use_facet < std::numpunct   < char        > > (locale_1).thousands_sep() << std::endl;
+	std::cout << std::use_facet < std::moneypunct < char, false > > (locale_1).curr_symbol  () << std::endl;
 
-	const std::locale locale_2("ru_RU.utf8");
+	const auto & time_put_1 = std::use_facet < std::time_put < char > > (locale_1);
+
+	time_put_1.put(std::cout, std::cout, ' ', data, format, format + std::strlen(format));
+
+	std::cout << std::endl;
+
+	const std::locale locale_2("ru_RU.utf8"); // note: use locale -a to list all installed locales
 
 	std::cout.imbue(locale_2);
 
 	std::cout << 1'000'000'000 << std::endl; // note: separator is space
 
-	std::cout << std::use_facet < std::numpunct < char > > (locale_2).thousands_sep() << std::endl;
+	std::cout << std::use_facet < std::numpunct   < char        > > (locale_2).thousands_sep() << std::endl;
+	std::cout << std::use_facet < std::moneypunct < char, false > > (locale_2).curr_symbol  () << std::endl;
+
+	const auto & time_put_2 = std::use_facet < std::time_put < char > > (locale_2);
+
+	time_put_2.put(std::cout, std::cout, ' ', data, format, format + std::strlen(format));
+
+	std::cout << std::endl;
+
+	const auto & time_get_C = std::use_facet < std::time_get < char > > (std::locale::classic()); // note: C locale
+
+	const auto dateorder = time_get_C.date_order();
+
+	std::cout << "dateorder: "
+			  << (dateorder == std::time_base::no_order || 
+			      dateorder == std::time_base::mdy ? "mdy" :
+				  dateorder == std::time_base::dmy ? "dmy" :
+				  dateorder == std::time_base::ymd ? "ymd" :
+				  dateorder == std::time_base::ydm ? "ydm" : "unknown") << std::endl;
+
+	std::ios_base::iostate state = std::ios_base::goodbit; tm input; // note: stream state
+
+	time_get_C.get(
+		std::istreambuf_iterator < char > (std::cin), 
+		std::istreambuf_iterator < char > (),
+			std::cin, state, &input, format, format + std::strlen(format));
+
+	if (state != std::ios_base::goodbit) std::cerr << "invalid format\n";
 
     return 0;
 }
