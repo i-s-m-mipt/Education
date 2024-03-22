b58da09875caafa6515080f02d084df0a75012b5
Matthias <i.s.m.mipt@yandex.ru>
2024-03-22 04:22:17 +0300

    Добавлен пример string_string_view.

--- projects/examples/source/section_13/string_string_view.cpp
+++ projects/examples/source/section_13/string_string_view.cpp
@@ -1,12 +1,27 @@
-import std;
-
-const char* f()
-{
-    return "Hello";
-}
+#include <iomanip>
+#include <iostream>
+#include <string>
 
 int main()
 {
+    std::cout << "Enter string 1: "; std::string string_1; std::cin >> string_1;
+    std::cout << "Enter string 2: "; std::string string_2; 
+    
+    std::getline(std::cin >> std::ws, string_2); // good: skip space characters
+
+    std::cout << std::quoted(string_1) << ' ' << 
+                 std::quoted(string_2) << std::endl; // note: see arguments
+
+    using namespace std::literals;
+
+    const auto string_3 = "Hello, world!"s; // good: standard string literal for auto
+
+    if (const auto index = string_3.find(','); index != std::string::npos)
+    {
+        std::cout << string_3.substr(0, index) << std::endl;
+    }
+
+    /*
     const char char_array[]{ 'H', 'e', 'l', 'l', 'o' };
 
     const char   string_1[]{ 'H', 'e', 'l', 'l', 'o', '\0' };
@@ -35,6 +50,7 @@ int main()
     auto string_7 = u8"Hello"s;
     auto string_8 = u"Hello"s;
     auto string_9 = U"Hello"s;
+    */
 
     return 0;
 }
