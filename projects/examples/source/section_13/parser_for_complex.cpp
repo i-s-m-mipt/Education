76a469b01422a701a0ce3e8f49759931603cb7ae
Matthias <i.s.m.mipt@yandex.ru>
2024-03-25 19:29:19 +0300

    Доработан пример parser_for_complex.

--- projects/examples/source/section_13/parser_for_complex.cpp
+++ projects/examples/source/section_13/parser_for_complex.cpp
@@ -12,20 +12,20 @@ using namespace std::literals;
 
 [[nodiscard]] std::complex < double > parse_complex(std::string_view input)
 {
-    auto begin = std::begin(input), end = std::end(input);
+    auto begin = std::cbegin(input), end = std::cend(input);
 
     using boost::spirit::x3::double_;
 
     auto real = 0.0, imag = 0.0;
 
-    auto f_real   = [&real](auto & context){ real =        boost::spirit::x3::_attr(context); };
-    auto f_imag_p = [&imag](auto & context){ imag = +1.0 * boost::spirit::x3::_attr(context); };
-    auto f_imag_n = [&imag](auto & context){ imag = -1.0 * boost::spirit::x3::_attr(context); };
+    const auto f_real   = [&real](auto & context){ real =        boost::spirit::x3::_attr(context); };
+    const auto f_imag_p = [&imag](auto & context){ imag = +1.0 * boost::spirit::x3::_attr(context); };
+    const auto f_imag_n = [&imag](auto & context){ imag = -1.0 * boost::spirit::x3::_attr(context); };
 
-    auto result = boost::spirit::x3::phrase_parse(begin, end,
+    const auto result = boost::spirit::x3::phrase_parse(begin, end,
         (double_[f_real] >> -((
             ('+' >> double_[f_imag_p]) | 
-            ('-' >> double_[f_imag_n])) >> 'i') | double_[f_real]), boost::spirit::x3::ascii::space);
+            ('-' >> double_[f_imag_n])) >> 'i')), boost::spirit::x3::ascii::space);
 
     if (!result || begin != end) 
     {
@@ -37,10 +37,10 @@ using namespace std::literals;
 
 int main()
 {
-    auto complex_1 = parse_complex("1.0"sv          );
-    auto complex_2 = parse_complex("1.0+1.0i"sv     );
-    auto complex_3 = parse_complex("1.0-1.0i"sv     );
-    auto complex_4 = parse_complex(" 1.0 + 1.0 i "sv);
+    const auto complex_1 = parse_complex("1.0"sv          );
+    const auto complex_2 = parse_complex("1.0+1.0i"sv     );
+    const auto complex_3 = parse_complex("1.0-1.0i"sv     );
+    const auto complex_4 = parse_complex(" 1.0 + 1.0 i "sv);
 
     assert(complex_1.real() == 1.0 && complex_1.imag() == +0.0);
     assert(complex_2.real() == 1.0 && complex_2.imag() == +1.0);
