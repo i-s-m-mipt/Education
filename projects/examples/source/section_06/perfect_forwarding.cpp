c9d4cd1d620dc75d2b10816df51c2044dccc3071
Matthias <i.s.m.mipt@yandex.ru>
2024-03-30 02:34:15 +0300

    Добавлен пример perfect_forwarding.

---
+++ projects/examples/source/section_06/perfect_forwarding.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <utility>
+
+class X {};
+
+inline void g(      X & ) { std::cout << "      X & " << std::endl; }
+inline void g(const X & ) { std::cout << "const X & " << std::endl; }
+inline void g(      X &&) { std::cout << "      X &&" << std::endl; }
+
+/*
+inline void f(      X &  x) { g(          x ); } // bad: multiple overloads for all cases
+inline void f(const X &  x) { g(          x ); }
+inline void f(      X && x) 
+{ 
+    g(std::move(x)); // note: local argument x is lvalue, move semantics is not transmitted
+} 
+*/
+
+template < typename T > inline void f(T && x) // good: forwarding reference, T && or auto &&
+{
+    g(std::forward < T > (x)); // good: perfect forwarding, maintaining fundamental properties
+}
+
+template < typename F, typename ... Types > 
+[[nodiscard]] inline constexpr auto invoke(F && f, Types && ... args) // note: see std::invoke
+{ 
+	return f(std::forward < Types > (args)...); 
+}
+
+int main()
+{
+	X x; const X cx;
+
+	f(          x ); // note: f -> g for       X &  x
+	f(         cx ); // note: f -> g for const X &  x
+	f(std::move(x)); // note: f -> g for       X && x
+
+	return 0;
+}
