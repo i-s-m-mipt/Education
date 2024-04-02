a34618a0dc0a1ad99a3244ac66d3349a46673820
Matthias <i.s.m.mipt@yandex.ru>
2024-04-02 14:22:15 +0300

    Доработан пример standard_concepts.

--- projects/examples/source/section_06/standard_concepts.cpp
+++ projects/examples/source/section_06/standard_concepts.cpp
@@ -13,6 +13,20 @@ template < typename T > concept arithmetic = (integral < T > || floating_point <
 
 template < typename T > concept addable = requires (T x, T y) { x + y; };
 
+template < typename T > concept equable = requires (T x, T y)
+{
+    { x == y } -> std::convertible_to < bool > ;
+    { x != y } -> std::convertible_to < bool > ;
+};
+
+template < typename T > concept ordable = equable < T > && requires (T x, T y)
+{
+    { x <  y } -> std::convertible_to < bool > ;
+    { x <= y } -> std::convertible_to < bool > ;
+    { x >  y } -> std::convertible_to < bool > ;
+    { x >= y } -> std::convertible_to < bool > ;
+};
+
 // =================================================================================================
 
 template < typename T > [[nodiscard]] inline constexpr T max_v1(T x, T y) requires integral < T >
@@ -25,7 +39,7 @@ template < typename T > [[nodiscard]] inline constexpr T max_v1(T x, T y) requir
 	return (x < y ? y : x);
 }
 
-template < addable ... Ts > [[nodiscard]] inline constexpr arithmetic auto sum_v1(Ts ... args)
+template < integral ... Ts > [[nodiscard]] inline constexpr integral auto sum_v1(Ts ... args)
 {
 	return (... + args);
 }
