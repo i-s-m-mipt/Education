352c3fa06a9a8e363f7988e30b67613a9084f07d
Matthias <i.s.m.mipt@yandex.ru>
2024-04-13 09:21:56 +0300

    Доработаны примеры модуля 13.

---
+++ projects/examples/source/13.05.iterator.stream.input.output.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <utility>
+#include <vector>
+
+int main()
+{
+	std::ios::sync_with_stdio(false); // note: no synchronization with C-streams
+
+	std::istreambuf_iterator < char > istreambuf_iterator(std::cin );
+	std::ostreambuf_iterator < char > ostreambuf_iterator(std::cout);
+
+	while (istreambuf_iterator != std::istreambuf_iterator < char > ()) 
+	{
+		*ostreambuf_iterator++ = *istreambuf_iterator++;
+	}
+
+	std::clearerr(stdin); std::cin.clear(); // note: clearerr to handle Ctrl+D
+
+	std::vector < int > vector;
+
+	std::ranges::copy(std::istream_iterator < int > (std::cin), 
+					  std::istream_iterator < int > (        ), std::back_inserter(vector)); 
+
+	std::ranges::sort(vector);
+
+	std::ranges::copy(std::as_const(vector), std::ostream_iterator < int > (std::cout, "\n"));
+
+	return 0;
+}
