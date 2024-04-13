4c6297363c5c89d366fb7f3a843ac6b35933020e
Matthias <i.s.m.mipt@yandex.ru>
2024-04-14 00:46:23 +0300

    Доработан пример 04.15.

--- projects/examples/source/04.15.implementation.utility.tlist.cpp
+++ projects/examples/source/04.15.implementation.utility.tlist.cpp
@@ -102,6 +102,27 @@ template < typename L, auto N > using nth = typename Nth < L, N > ::type;
 
 // =================================================================================================
 
+template < typename L, bool E = empty_v < L > > struct Max_Type {};
+
+template < typename L > struct Max_Type < L, true  > { using type = char; };
+template < typename L > struct Max_Type < L, false >
+{
+private:
+
+    using contender = front < L > ;
+
+    using best = typename Max_Type < pop_front < L > > ::type;
+
+public:
+
+    using type = std::conditional_t < (sizeof(contender) >= sizeof(best)), contender, best > ;
+
+}; // template < typename L > class Max_Type < L, false >
+
+template < typename L > using max_type = typename Max_Type < L > ::type;
+
+// =================================================================================================
+
 int main()
 {
     using tlist_0 = Tlist <            > ; 
@@ -136,6 +157,8 @@ int main()
     static_assert(std::is_same_v < nth < tlist_3, 0 > , bool > );
     static_assert(std::is_same_v < nth < tlist_3, 1 > , char > );
     static_assert(std::is_same_v < nth < tlist_3, 2 > , int  > );
+
+    static_assert(std::is_same_v < max_type < tlist_3 > , int > );
 
     return 0;
 }
