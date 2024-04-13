79878f1ea57ebf4881d1caf714789ca7833a7c19
Matthias <i.s.m.mipt@yandex.ru>
2024-04-13 09:30:27 +0300

    Доработан пример 12.06.

--- projects/examples/source/12.06.locale.facet.utf.converter.cpp
+++ projects/examples/source/12.06.locale.facet.utf.converter.cpp
@@ -80,6 +80,49 @@
 
 // =================================================================================================
 
+[[nodiscard]] inline const auto & dictionary() // note: transliteration table
+{
+    static std::unordered_map < char32_t, std::u32string > dictionary = 
+	{ 
+        { U'А', U"A"   }, { U'а', U"a"   },
+        { U'Б', U"B"   }, { U'б', U"b"   },
+        { U'В', U"V"   }, { U'в', U"v"   },
+        { U'Г', U"G"   }, { U'г', U"g"   },
+        { U'Д', U"D"   }, { U'д', U"d"   },
+        { U'Е', U"E"   }, { U'е', U"e"   },
+        { U'Ё', U"Yo"  }, { U'ё', U"yo"  },
+        { U'Ж', U"Zh"  }, { U'ж', U"zh"  },
+        { U'З', U"Z"   }, { U'з', U"z"   },
+        { U'И', U"I"   }, { U'и', U"i"   },
+        { U'Й', U"J"   }, { U'й', U"j"   },
+        { U'К', U"K"   }, { U'к', U"k"   },
+        { U'Л', U"L"   }, { U'л', U"l"   },
+        { U'М', U"M"   }, { U'м', U"m"   },
+        { U'Н', U"N"   }, { U'н', U"n"   },
+        { U'О', U"O"   }, { U'о', U"o"   },
+        { U'П', U"P"   }, { U'п', U"p"   },
+        { U'Р', U"R"   }, { U'р', U"r"   },
+        { U'С', U"S"   }, { U'с', U"s"   },
+        { U'Т', U"T"   }, { U'т', U"t"   },
+        { U'У', U"U"   }, { U'у', U"u"   },
+        { U'Ф', U"Ph"  }, { U'ф', U"ph"  },
+        { U'Х', U"H"   }, { U'х', U"h"   },
+        { U'Ц', U"Ts"  }, { U'ц', U"ts"  },
+        { U'Ч', U"Ch"  }, { U'ч', U"ch"  },
+        { U'Ш', U"Sh"  }, { U'ш', U"sh"  },
+        { U'Щ', U"Sch" }, { U'щ', U"sch" },
+        { U'Ы', U"I"   }, { U'ы', U"i"   },
+        { U'ъ', U""    }, { U'ь', U"'"   },
+        { U'Э', U"E"   }, { U'э', U"e"   },
+        { U'Ю', U"Yu"  }, { U'ю', U"yu"  },
+        { U'Я', U"Ya"  }, { U'я', U"ya"  } 
+	};
+
+    return dictionary;
+}
+
+// =================================================================================================
+
 int main()
 {
     std::cout << boost::locale::util::get_system_locale() << std::endl;
