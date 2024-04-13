e4dec79456a3c28bdcf53d8d472d6edd34d177b4
Matthias <i.s.m.mipt@yandex.ru>
2024-04-13 06:13:03 +0300

    Добавлен пример 13.04.

---
+++ projects/examples/source/13.04.stream.class.string.cpp
@@ -0,0 +1,33 @@
+#include <cassert>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
+int main()
+{
+	std::stringstream stream;
+
+    stream << std::showbase;
+
+	stream << std::setw(8) << std::right << std::setfill('_') << std::oct << 42;
+
+	assert(stream.str() == "_____052");
+
+	stream.seekp(0);
+
+	stream << std::setw(8) << std::right << std::setfill('_') << std::hex << 42;
+
+	assert(stream.str() == "____0x2a");
+
+    for (char c{}; stream.get(c); ) // note: consider calculator implementation
+    {
+        if (c == '0')
+        {
+            stream.unget(); int x{}; stream >> std::hex >> x; assert(x == 42);
+        }
+    }
+
+    assert(stream.eof()); // note: all characters from stream processed successfully
+
+	return 0;
+}
