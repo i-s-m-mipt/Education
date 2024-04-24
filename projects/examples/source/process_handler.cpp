612e37b4004cc7a7f5870c55864f3bd5a4ecf499
Matthias <i.s.m.mipt@yandex.ru>
2024-04-24 07:34:08 +0300

    Доработан пример process_handler.

--- projects/examples/source/process_handler.cpp
+++ projects/examples/source/process_handler.cpp
@@ -9,9 +9,9 @@ int main(int argc, char ** argv)
 {
     assert(argc == 3); // note: program name and 2 arguments
 
-    std::cout << argv[0] << ' ' << 
-                 argv[1] << ' ' << 
-                 argv[2] << std::endl;
+    std::cout << argv[0] << ' ';
+    std::cout << argv[1] << ' ';
+    std::cout << argv[2] << std::endl;
 
     std::this_thread::sleep_for(10s);
 
