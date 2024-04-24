9aec3b674d567f7c6743c45257e647100beb5679
Matthias <i.s.m.mipt@yandex.ru>
2024-04-24 05:50:55 +0300

    Доработан пример 04.09.

--- projects/examples/source/04.09.template.metaprogramming.constexpr.hybrid.cpp
+++ projects/examples/source/04.09.template.metaprogramming.constexpr.hybrid.cpp
@@ -28,8 +28,7 @@ public:
 
 }; // template < typename R1, typename R2 > class Add
 
-template < typename R1, typename R2 > using add_t = 
-typename Add < R1, R2 > ::type; // note: typename required, static object instead
+template < typename R1, typename R2 > using add_t = typename Add < R1, R2 > ::type;
 
 // =================================================================================================
 
@@ -42,9 +41,8 @@ template < typename T1, typename R1, typename T2, typename R2 >
 {
 	using unit_t = Ratio < 1, add_t < R1, R2 > ::den > ; // note: compile-time
 
-	const auto value = 
-		lhs.value * unit_t::den / R1::den * R1::num +
-		rhs.value * unit_t::den / R2::den * R2::num; // note: runtime
+	const auto value = lhs.value * unit_t::den / R1::den * R1::num +
+					   rhs.value * unit_t::den / R2::den * R2::num; // note: runtime
 
 	return Duration < decltype(value), unit_t > { value };
 }
@@ -53,17 +51,15 @@ template < typename T1, typename R1, typename T2, typename R2 >
 
 int main()
 {
-	constexpr auto result_1 = (
-		Duration < int, Ratio < 2, 5 > > (1) +
-		Duration < int, Ratio < 3, 7 > > (2)).value;
+	constexpr auto result_1 = (Duration < int, Ratio < 2, 5 > > (1) +
+							   Duration < int, Ratio < 3, 7 > > (2)).value;
 
 	std::cout << result_1 << std::endl; // note: compile-time result
 
 	auto x = 1, y = 2; // note: runtime non-constant variable
 
-	auto result_2 = (
-		Duration < int, Ratio < 2, 5 > > (x) +
-		Duration < int, Ratio < 3, 7 > > (y)).value;
+	auto result_2 = (Duration < int, Ratio < 2, 5 > > (x) +
+					 Duration < int, Ratio < 3, 7 > > (y)).value;
 
 	std::cout << result_2 << std::endl; // note: hybrid-time result
 
