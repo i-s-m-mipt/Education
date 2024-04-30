6cdf6ad5734af2583776b23d86d17fb4489d78e4
Matthias <i.s.m.mipt@yandex.ru>
2024-04-30 19:53:37 +0300

    Переименованы примеры модуля 7.

---
+++ projects/examples/source/07.08.project.tool.debugger.cpp
@@ -0,0 +1,26 @@
+#include <cassert>
+#include <numeric>
+#include <vector>
+
+[[nodiscard]] constexpr bool binary_search(const std::vector < int > & vector, int key) noexcept
+{
+	std::size_t l = 0, r = std::size(vector);
+
+	if (r == 0) return false;
+
+	if (r == 1) return (vector[0] == key);
+
+	while (l < r)
+	{
+		const auto m = std::midpoint(l, r); vector[m] < key ? l = m + 1 : r = m;
+	}
+
+	return (vector[l] == key);
+}
+
+int main()
+{
+	assert(binary_search({1, 3, 4, 5, 6, 7, 8 }, 7));
+
+	return 0;
+}
