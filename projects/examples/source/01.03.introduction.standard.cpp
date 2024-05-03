a90a9c2f4ef9f4836549e9aac179c40440f46637
Matthias <i.s.m.mipt@yandex.ru>
2024-05-03 16:24:52 +0300

    Доработан пример 01.03.

--- projects/examples/source/01.03.introduction.standard.cpp
+++ projects/examples/source/01.03.introduction.standard.cpp
@@ -7,15 +7,15 @@
 
 int main()
 {
-//	using namespace std; // bad: dangerous in big projects
+//	using namespace std; // bad: неудобно в больших проектах
 
 //  ================================================================================================
 	
 	std::cout << "Enter two integers separated by a space character: ";
 
-	int x{}, y{}; // good: zero value initialized variables
+	int x{}, y{}; // good: инициализированные нулем переменные
 
-	std::cin >> x >> y; // note: chaining operators
+	std::cin >> x >> y; // note: цепочка операторов
 
 	std::cout << "You have entered " << x << " and " << y << std::endl;
 
@@ -23,7 +23,7 @@ int main()
 
 	std::cout << "Enter string with your name: ";
 
-	std::string name; // note: consider for complex operations
+	std::string name; // note: удобно для сложных операций со строками
 
 	std::cin >> name;
 
@@ -31,7 +31,7 @@ int main()
 
 //  ================================================================================================
 
-	std::vector < int > vector { 1, 2, 3 }; // note: default container
+	std::vector < int > vector { 1, 2, 3 }; // note: контейнер по умолчанию
 
 	std::cout << "Vector size is " << std::size(vector) << std::endl;
 
@@ -39,7 +39,7 @@ int main()
 
 	std::cout << "Square root of 4 is " << std::sqrt(4) << std::endl;
 
-	std::swap(x, y); // note: consider for sorting algorithms
+	std::swap(x, y); // note: используется в алгоритмах сортировки
 
 	std::cout << "Now x is " << x << " and y is " << y << std::endl;
 
