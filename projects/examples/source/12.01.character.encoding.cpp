d00ceb77b74fa3b94f99cfe1d1f7063cbe197235
Matthias <i.s.m.mipt@yandex.ru>
2024-05-05 23:19:37 +0300

    Доработан пример 12.01.

--- projects/examples/source/12.01.character.encoding.cpp
+++ projects/examples/source/12.01.character.encoding.cpp
@@ -17,53 +17,39 @@
 
 int main()
 {
-	auto c = 'a'; // note: auto -> char, consider for UTF-8
+	auto c = 'a'; // note: auto -> char
 
-	assert(static_cast < int > (c) == 97); // note: ASCII code
+	assert(static_cast < int > (c) == 97);
 
-	assert(sizeof(char) == 1); // note: guaranteed size 8 bits, remember std::byte
+	assert(sizeof(char) == 1); // note: см. также std::byte
 
 //  ================================================================================================
 
 	std::cout << std::boolalpha << std::is_signed_v < char > << std::endl;
 
-//	c = 255; // bad: undefined behavior, overflow conversion from 255 to -1
+//	c = 255; // bad: неопределенное поведение
+
+	for (auto i = -128; i < 128; ++i) std::cout << static_cast < char > (i) << std::endl;
+	for (auto i =    0; i < 256; ++i) std::cout << static_cast < char > (i) << std::endl;
 
 	[[maybe_unused]] constexpr   signed char sc = 'a';
 	[[maybe_unused]] constexpr unsigned char uc = 'a';
-	
-	for (auto i = -128; i < 128; ++i) // note: two's complement representation
-	{
-		std::cout << static_cast < char > (i);
-	}
-
-	std::cout << std::endl;
-
-	for (auto i = 0; i < 256; ++i) // note: two's complement representation
-	{
-		std::cout << static_cast < char > (i);
-	}
-
-	std::cout << std::endl;
 
 //  ================================================================================================
 
-	for (c = '!'; c <= '~'; ++c) std::cout << c; // note: printable ASCII characters
+	for (c = '!'; c <= '~'; ++c) std::cout << c;
 
 	std::cout << std::endl;
 	
-	assert('8' - '0' == 8); // good: obtain integer digit from character digit
+	assert('8' - '0' == 8);
 
 //  ================================================================================================
+
+	std::cout << sizeof(wchar_t) << std::endl; // bad: проблема переносимости
 
 	[[maybe_unused]] constexpr auto c8 = u8'a'; // note: auto -> char8_t
 
-	assert(c8 == 'a'); // note: consider char for UTF-8 for better std support
-
-	assert(sizeof(char16_t) == 2); // note: u'a' for UTF-16, problems with support
-	assert(sizeof(char32_t) == 4); // note: U'a' for UTF-32, problems with support
-
-	std::cout << sizeof(wchar_t) << std::endl; // note: consider for wide encodings on Windows
+	assert(sizeof(char8_t) == 1); // bad: слабая поддержка в стандартной библиотеке
 
 //  ================================================================================================
 
