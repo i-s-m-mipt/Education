009dfcf436a77b1ea2d507abbd81256d26818ac6
Matthias <i.s.m.mipt@yandex.ru>
2024-09-16 13:29:52 +0300

    Доработаны примеры модуля 1.

--- projects/examples/source/01.02.introduction.function.main.cpp
+++ projects/examples/source/01.02.introduction.function.main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	std::cout << "Hello, world!" << std::endl;
+	std::cout << "Hello, World!" << std::endl;
 
 	return 0;
 }
--- projects/examples/source/01.03.introduction.standard.cpp
+++ projects/examples/source/01.03.introduction.standard.cpp
@@ -1,6 +1,9 @@
+#include <algorithm>
+#include <cassert>
 #include <cmath>
 #include <iostream>
 #include <iterator>
+#include <numeric>
 #include <string>
 #include <vector>
 
@@ -10,23 +13,21 @@ using namespace std; // bad
 
 int main()
 {	
-	std::cout << "Enter two integers: "; int x{}, y{}; std::cin >> x >> y;
+	std::cout << "Enter 2 integers: "; int x{}, y{}; std::cin >> x >> y;
 
-	std::cout << "You have entered integers " << x << " and " << y << std::endl;
-
-//  ================================================================================================
-
-	std::cout << "Enter string: "; std::string string; std::cin >> string;
-
-	std::cout << "You have entered string " + string << std::endl;
+	std::cout << "Entered integers: " << x << ' ' << y << std::endl;
 
 //  ================================================================================================
 
-	std::vector < int > vector { 1, 2, 3, 4, 5 }; vector.push_back(42);
+	assert(std::abs(std::sin(std::numbers::pi / 2.0) - 1.0) < 0.000001);
 
-	std::cout << "Vector has " << std::size(vector) << " elements" << std::endl;
+	const std::string s = "01234"; assert(s + "56789" == "0123456789");
 
-	std::cout << "|x| ^ 0.5 = " << std::sqrt(std::abs(x)) << std::endl;
+//  ================================================================================================
+
+	std::vector < int > vector { 1, 2, 3, 4, 5 }; vector.push_back(1);
+
+	std::ranges::sort(vector); assert(std::ranges::is_sorted(vector));
 
 	return 0;
 }
