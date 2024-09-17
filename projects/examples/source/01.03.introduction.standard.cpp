26a283dd91e3df8dd7e3f421bb1e54547cc77edb
Matthias <i.s.m.mipt@yandex.ru>
2024-09-17 12:52:08 +0300

    Доработан пример 01.03.

--- projects/examples/source/01.03.introduction.standard.cpp
+++ projects/examples/source/01.03.introduction.standard.cpp
@@ -2,8 +2,7 @@
 #include <cassert>
 #include <cmath>
 #include <iostream>
-#include <iterator>
-#include <numeric>
+#include <numbers>
 #include <string>
 #include <vector>
 
@@ -19,11 +18,11 @@ int main()
 
 	assert(std::abs(std::sin(std::numbers::pi / 2.0) - 1.0) < 0.000001);
 
+	const std::string s = "01234"; assert(s + "56789" == "0123456789");
+
 	std::vector < int > vector { 1, 2, 3, 4, 5 }; vector.push_back(1);
 
 	std::ranges::sort(vector); assert(std::ranges::is_sorted(vector));
-
-	const std::string s = "01234"; assert(s + "56789" == "0123456789");
 
 	return 0;
 }
