5ac4287f716544edd3e3435b58439e5576b9760a
Matthias <i.s.m.mipt@yandex.ru>
2024-09-20 13:31:39 +0300

    Доработан пример 01.03.

--- projects/examples/source/01.03.introduction.standard.cpp
+++ projects/examples/source/01.03.introduction.standard.cpp
@@ -6,21 +6,21 @@
 #include <string>
 #include <vector>
 
-/*
-using namespace std; // bad
-*/
+//  using namespace std; // bad
 
 int main()
-{	
+{
 	std::cout << "Enter 2 integers: "; int x{}, y{}; std::cin >> x >> y;
 
 	std::cout << "Entered integers: " << x << ' ' << y << std::endl;
 
 	assert(std::abs(std::sin(std::numbers::pi / 2.0) - 1.0) < 0.000001);
 
-	const std::string s = "01234"; assert(s + "56789" == "0123456789");
+	const std::string string = "aaa"; assert(string + "bbb" == "aaabbb");
 
 	std::vector < int > vector { 1, 2, 3, 4, 5 }; vector.push_back(1);
+
+	assert(std::size(vector) == 6 && vector.front() == vector.back());
 
 	std::ranges::sort(vector); assert(std::ranges::is_sorted(vector));
 
