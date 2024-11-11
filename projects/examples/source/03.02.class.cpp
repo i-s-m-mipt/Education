deb6af288911836c1eca2cd5fe76c2b9ae9c2b46
Matthias <i.s.m.mipt@yandex.ru>
2024-11-11 05:01:58 +0300

    Доработан пример 03.02.

--- projects/examples/source/03.02.class.cpp
+++ projects/examples/source/03.02.class.cpp
@@ -4,156 +4,151 @@
 
 using namespace std::literals;
 
-//  ================================================================================================
+//  ===========================================================================
 
 class Entity
 {
-private:
-
-	struct Data_Cache 
-	{ 
-		void update(const Entity & entity)
-		{
-			data = std::to_string(entity.m_data);
-
-			is_invalid = false;
-		}
-
-		std::string data; bool is_invalid = true;
-	};
-
 public:
 
-//	Entity() { m_name = ""; m_data = 0; } // error
+	Entity() : m_data_1(s_data_1), m_data_2(0), m_data_3(0) {}
 
-//	Entity() : m_name("") { m_data = 0; } // bad
+//	Entity() : m_data_1(s_data_1), m_data_3(0), m_data_2(0) {} // error
 
-//	Entity() : m_data(0), m_name("") {} // warning
+//  -------------------------------------------------------------------
 
-	Entity() : m_name(""), m_data(0) {}
+	Entity(const int & data_1, int data_2, int data_3) 
+	: 
+		m_data_1(data_1), 
+		m_data_2(data_2),
+		m_data_3(data_3)
+	{}
 
-	Entity(const std::string & name, int data) : m_name(name)
-	{
-		set_data(data);
-	}
+	Entity(const int & data_1, int data_2) : Entity(data_1, data_2, 0) {}
 
-	Entity(const std::string & name) : Entity(name, 0) {}
+//  ---------------------------------------------------------------------
 
    ~Entity()
 	{
 		std::clog << "Entity::~Entity\n";
 	}
 
-//  -------------------------------------
+//  ----------------------------------------
 
-	void test_v1() const
-	{ 
-		std::clog << "Entity::test_v1\n"; 
+	auto data_1() const { return m_data_1; }
+	auto data_2() const { return m_data_2; }
+	auto data_3() const { return m_data_3; }
 
-//		m_data = s_data; // error
-	}
-
-	void test_v2() const;
-
-//  --------------------------------------------
-
-	const auto & name() const { return m_name; }
-
-		  auto   data() const { return m_data; }
-
-//  --------------------------------------------
+//  ----------------------------------------
 
 	const auto & data_as_string() const
 	{
-		if (m_data_cache.is_invalid)
+	//	m_data_3 = 0; // error
+
+		if (m_cached_data.is_invalid)
 		{
-			m_data_cache.update(*this);
+			m_cached_data.update(*this);
 		}
-		
-		return m_data_cache.data;
+				
+		return m_cached_data.string;
 	}
 
-//  -------------------------------------------------
+//  -----------------------------------------------------
 
-//	void set_data(int data) { m_data = data; } // bad
+//	void set_data_3(int data) { m_data_3 = data; } // bad
 
-	void set_data(int data)
-	{
-		m_data = data < s_data ? s_data : data; 
+	void set_data_3(int data) 
+	{ 
+		m_data_3 = data; 
 		
-		m_data_cache.is_invalid = true;
+		m_cached_data.is_invalid = true; 
 	}
 
-//  -----------------------------------
+//  ------------------------------------
 
-	static void test_v3()
+	static void test()
 	{
-		std::clog << "Entity::test_v3\n";
-
-//		m_data = s_data; // error
+		std::clog << "Entity::test : s_data_1 = " << s_data_1 << '\n';
+		
+//		std::clog << "Entity::test : m_data_3 = " << m_data_3 << '\n'; // error
 	}
 
 private:
 
-	static inline auto s_data = 0;
+	struct Cached_Data 
+	{ 
+		void update(const Entity & entity)
+		{
+			string = 
+			(
+				std::to_string(entity.m_data_1) + '/' +
+				std::to_string(entity.m_data_2) + '/' +
+				std::to_string(entity.m_data_3)
+			);
 
-//  ------------------------------
+			is_invalid = false;
+		}
 
-	const std::string m_name; 
+		std::string string; bool is_invalid = true;
+	};
 
-//  -------------------------
+//  -----------------------------------------------
 
-	int m_data = 0;
+	static inline       auto s_data_1 = 0;
 
-//  --------------------------------
+	static        const auto s_data_2 = 0;
 
-	mutable Data_Cache m_data_cache;
+//	static        const auto s_data_3 = "aaaaa"s; // error
+
+	static inline const auto s_data_4 = "aaaaa"s;
+
+//  ---------------------------------------------
+
+	const int & m_data_1 = 0;
+
+	const int   m_data_2 = 0; 
+
+		  int   m_data_3 = 0;
+
+//  ----------------------------------
+
+	mutable Cached_Data m_cached_data;
 };
 
-//  ================================================================================================
-
-void Entity::test_v2() const
-{ 
-	std::clog << "Entity::test_v2\n"; 
-}
-
-//  ================================================================================================
+//  ===========================================================================
 
 int main()
 {
 	Entity entity_1;
 
-//	entity_1.m_data = 1; // error
+//	entity_1.m_data_3 = 1; // error
 
-	entity_1.set_data(1); 
+	entity_1.set_data_3(1); 
 	
-	assert(entity_1.data() == 1);
+	assert(entity_1.data_3() == 1);
 
-//  -----------------------------
+	assert(entity_1.data_as_string() == "0/0/1");
+
+//  ---------------------------------------------
 
 	const Entity entity_2;
 
-//  entity_2.set_data(1); // error
+//  entity_2.set_data_3(1); // error
 
-	assert(entity_2.data() == 0);
+	assert(entity_2.data_3() == 0);
 
-	assert(entity_2.data_as_string() == "0");
+	assert(entity_2.data_as_string() == "0/0/0");
 
-//  -----------------------------------------
+//  ---------------------------------------------
 
 	Entity entity_3;
 
-	Entity entity_4("entity_4", 1);
+	Entity entity_4(1, 2, 3);
 	
-	Entity entity_5("entity_5");
+	Entity entity_5(1, 2);
 
-//	Entity entity_6(); // error
+//	Entity entity_6(); // warning
 
-	{
-		Entity entity_7;
-	}
+//  --------------------------------
 
-//  ------------------
-
-	Entity::test_v3();
+	entity_1.test(); Entity::test();
 }
