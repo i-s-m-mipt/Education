75da2832f9fd4cdc3fd718adeb268d61bc0e53bf
Matthias <i.s.m.mipt@yandex.ru>
2024-11-21 01:55:03 +0300

    Доработан пример 05.09.

--- projects/examples/source/05.09.pattern.memento.cpp
+++ projects/examples/source/05.09.pattern.memento.cpp
@@ -1,55 +1,88 @@
 #include <cassert>
 #include <string>
+#include <tuple>
 #include <vector>
 
-//  ================================================================================================
+//  =================================================================
 
 class Entity 
 {
 public:
 
-    struct Memento 
-    { 
-        int data = 0; 
-    };
+    explicit Entity(int data = 0) : m_states(1, State(data)) {}
 
-//  ------------------------------------------------------------------------------------------------
+//  -----------------------------------------------------------
 
-    auto data() const 
-    { 
-        return m_data; 
+    auto get() const
+    {
+        return m_states.front().data;
     }
 
-    void set_data(int data) 
-    { 
-        m_data = data < 0 ? 0 : data; 
+    void set(int data)
+    {
+        State state(data);
+        
+        m_deltas.push_back(state - m_states.front());
+
+        m_states.front() = state;
     }
 
     void save()
     { 
-        m_mementos.emplace_back(m_data); 
+        m_states.push_back(m_states.front()); 
     }
 
-    void load(std::size_t index) 
-    { 
-        m_data = m_mementos.at(index).data;
+    void load_v1(std::size_t index)
+    {
+        m_states.front() = m_states.at(index);
+    }
+
+    void load_v2(std::size_t shift)
+    {
+        State state;
+
+        for (auto i = 0uz; i < shift && i < std::size(m_deltas); ++i)
+        {
+            state = state + m_deltas[i];
+        }
+
+        m_states.front() = state;
     }
 
 private:
 
-    int m_data = 0; std::vector < Memento > m_mementos;
+    struct State { int data = 0; };
+
+    struct Delta { int data = 0; };
+
+//  ------------------------------------------------------------
+
+    friend Delta operator-(const State & lhs, const State & rhs) 
+    { 
+        return Delta(lhs.data - rhs.data); 
+    }
+
+    friend State operator+(const State & lhs, const Delta & rhs) 
+    { 
+        return State(lhs.data + rhs.data); 
+    }
+
+//  -------------------------------
+
+    std::vector < State > m_states;
+
+    std::vector < Delta > m_deltas;
 };
 
-//  ================================================================================================
+//  =================================================================
 
 int main() 
 {
     Entity entity; 
     
-    entity.set_data(1); entity.save();
-    entity.set_data(2); entity.save();
+    entity.set(1); entity.save();
+    entity.set(2); entity.save();
     
-    entity.load(0); 
-    
-    assert(entity.data() == 1);
+    entity.load_v1(1); assert(entity.get() == 1);
+    entity.load_v2(2); assert(entity.get() == 2);
 }
