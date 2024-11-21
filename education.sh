9d917f78f8fddb9670c25f83a803e961027d41dc
Matthias <i.s.m.mipt@yandex.ru>
2024-11-21 04:56:17 +0300

    Обновлен скрипт education.sh.

--- education.sh
+++ education.sh
@@ -26,8 +26,12 @@ cp ../source/consolas.ttf .
 cp ../source/13.07.algorithm.erase.comment.source.txt .
 cp ../../../libraries/library_v2/liblibrary_v2.so .
 
-g++ -c -std=c++23              -Wall -Wextra -pedantic -O0 -m64 ../source/06.06.project.header.precompiled.hpp -o 06.06.project.header.precompiled.hpp.gch
-g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 -m64 ../source/06.10.project.module.submodule.cxx
-g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 -m64 ../source/06.08.project.module.cxx
-g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 -m64 ../source/06.09.project.module.source.cxx
-g++    -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 -m64 ../source/06.03.project.source.cpp ../source/06.04.project.source.main.cpp 06.08.project.module.o 06.09.project.module.source.o 06.10.project.module.submodule.o -o 06.04.project
+g++ -c -std=c++23              -Wall -Wextra -pedantic -O0 ../source/06.03.project.header.precompiled.hpp -o 06.03.project.header.precompiled.hpp.gch
+
+g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 -xc++-system-header iostream
+
+g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 ../source/06.08.project.module.submodule.cxx
+g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 ../source/06.06.project.module.cxx
+g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 ../source/06.07.project.module.source.cxx
+
+g++    -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 ../source/06.04.project.source.cpp ../source/06.05.project.source.main.cpp 06.06.project.module.o 06.07.project.module.source.o 06.08.project.module.submodule.o -o 06.05.project
