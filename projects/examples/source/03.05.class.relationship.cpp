d7fdf6efea126823d634290effa4e0f7d4fee6b3
Matthias <i.s.m.mipt@yandex.ru>
2024-11-23 01:11:26 +0300

    Доработан пример 03.05.

---
+++ projects/examples/source/03.05.class.relationship.cpp
@@ -0,0 +1,44 @@
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+//  =======================================================
+
+struct Entity
+{
+	void test() const
+	{
+		std::clog << "Entity::test\n";
+	}
+
+//  --------------------------------------------
+
+	int data = 0; static inline auto s_data = 0;
+};
+
+//  =======================================================
+
+struct Client { std::vector < struct Server * > servers; };
+struct Server { std::vector < struct Client * > clients; };
+
+//  =======================================================
+
+int main()
+{
+	Entity entity;
+
+	assert(entity.data == 0 && Entity::s_data == 0);
+
+//  ------------------------------------------------
+
+	auto size = 5uz;
+
+	std::vector < Client > clients(size);
+	std::vector < Server > servers(size);
+	
+	for (auto i = 0uz; i < size; ++i)
+	{
+		clients[i].servers.push_back(&servers[i]);
+		servers[i].clients.push_back(&clients[i]);
+	}
+}
