55b45e7b46871ffa3bd97f428f1e56b7556699a7
Matthias <i.s.m.mipt@yandex.ru>
2024-12-15 15:28:54 +0300

    Доработаны примеры библиотек.

--- projects/library_v1/source/library.cpp
+++ projects/library_v1/source/library.cpp
@@ -4,6 +4,6 @@ namespace library_v1
 {
 	void test()
 	{ 
-		std::clog << "library_v1::test\n"; 
+		std::cout << "library_v1::test\n"; 
 	}
 }
--- projects/library_v2/source/library.cpp
+++ projects/library_v2/source/library.cpp
@@ -6,8 +6,8 @@
 
 namespace library_v2
 {
-	API void test_v1() { std::clog << "library_v2::test_v1\n"; }
-	API void test_v2() { std::clog << "library_v2::test_v2\n"; }
+	API void test_v1() { std::cout << "library_v2::test_v1\n"; }
+	API void test_v2() { std::cout << "library_v2::test_v2\n"; }
 }
 
 BOOST_DLL_ALIAS(library_v2::test_v2, test_v3);
