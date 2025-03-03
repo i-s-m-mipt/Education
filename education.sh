3f2bccf2de8d9286714a4cfe20a5dc2090f616ff
Matthias <i.s.m.mipt@yandex.ru>
2025-03-04 01:23:20 +0300

    Обновлен скрипт education.sh.

--- education.sh
+++ education.sh
@@ -1,37 +1,47 @@
-mkdir -p libraries/library_v1
-mkdir -p libraries/library_v2
+mkdir -p libraries/library_v1 && cd projects/library_v1
 
-cd projects/library_v1
-mkdir -p output
-cd output
-cmake ..
-cmake --build .
-cp liblibrary_v1.a ../../../libraries/library_v1
+mkdir -p output && cd "$_"
 
-cd ../../../projects/library_v2
-mkdir -p output
-cd output
-cmake ..
-cmake --build .
-cp liblibrary_v2.so ../../../libraries/library_v2
+cmake .. && cmake --build .
 
-cd ../../../projects/examples
-mkdir -p output
-cd output
-cmake ..
-cmake --build .
-cp ../source/script.py .
-cp ../source/matthias.jpg .
-cp ../source/consolas.ttf .
-cp ../source/13.07.algorithm.erase.comment.source.txt .
+cp liblibrary_v1.a ../../../libraries/library_v1 && cd ../../../
+
+##############################################################################################################################
+
+mkdir -p libraries/library_v2 && cd projects/library_v2
+
+mkdir -p output && cd "$_"
+
+cmake .. && cmake --build .
+
+cp liblibrary_v2.so ../../../libraries/library_v2 && cd ../../../
+
+##############################################################################################################################
+
+cd projects/examples
+
+mkdir -p output && cd "$_"
+
+cmake .. && cmake --build .
+
+##############################################################################################################################
+
 cp ../../../libraries/library_v2/liblibrary_v2.so .
 
-g++ -c -std=c++23              -Wall -Wextra -pedantic -O0 ../source/06.03.project.header.precompiled.hpp -o 06.03.project.header.precompiled.hpp.gch
+##############################################################################################################################
+
+g++ -c -std=c++23              -Wall -Wextra -pedantic -O0 ../source/06.01.06.hpp -o 06.01.06.hpp.gch
+
+g++    -std=c++23              -Wall -Wextra -pedantic -O0 ../source/06.01.07.cpp ../source/06.01.08.cpp -o 06.01.08
+
+##############################################################################################################################
 
 g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 -xc++-system-header iostream
 
-g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 ../source/06.08.project.module.submodule.cxx
-g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 ../source/06.06.project.module.cxx
-g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 ../source/06.07.project.module.source.cxx
+g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 ../source/06.02.03.cxx
 
-g++    -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 ../source/06.04.project.source.cpp ../source/06.05.project.source.main.cpp 06.06.project.module.o 06.07.project.module.source.o 06.08.project.module.submodule.o -o 06.05.project
+g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 ../source/06.02.01.cxx
+
+g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 ../source/06.02.02.cxx
+
+g++    -std=c++23 -fmodules-ts -Wall -Wextra -pedantic -O0 ../source/06.02.04.cpp 06.02.01.o 06.02.02.o 06.02.03.o -o 06.02.04
