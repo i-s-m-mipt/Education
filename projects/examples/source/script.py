8a59a684ff206c8e9054c68a1299098b37fb702e
Matthias <i.s.m.mipt@yandex.ru>
2025-04-16 14:02:30 +0300

    Добавлены вспомогательные файлы для примеров.

---
+++ projects/examples/source/script.py
@@ -0,0 +1,35 @@
+####################################################################
+
+import math
+
+import matplotlib.pyplot as plot
+
+####################################################################
+
+def factorial(x: int) -> str:
+
+    return str(math.factorial(x))
+
+####################################################################
+
+def make_plot(string: str, label: str):
+
+    points = [float(coordinate) for coordinate in string.split(',')]
+
+    plot.plot(points[::2], points[1::2], label = label)
+
+#   ----------------------------------------------------------------
+
+    plot.xlabel("strings"   )
+
+    plot.ylabel("collisions")
+
+#   ----------------------------------------------------------------
+
+    plot.grid  ()
+
+    plot.legend()
+
+    plot.show  ()
+
+####################################################################
