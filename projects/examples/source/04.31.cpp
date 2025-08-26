49ea7c0a4e5804a9a46e1fae8c577105a6b2032c
Matthias <i.s.m.mipt@yandex.ru>
2025-08-26 18:50:45 +0300

    Доработаны примеры 04.30 и 04.31.

--- projects/examples/source/04.31.cpp
+++ projects/examples/source/04.31.cpp
@@ -1,32 +1,36 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
 
 #include <type_traits>
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
 
-template < typename T > struct add_lvalue_reference { using type = T &;  };
+template < typename T > struct remove_reference          { using type = T; };
 
-template < typename T > struct add_rvalue_reference { using type = T &&; };
+template < typename T > struct remove_reference < T &  > { using type = T; };
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+template < typename T > struct remove_reference < T && > { using type = T; };
 
-template < typename T > using  add_lvalue_reference_t = typename add_lvalue_reference < T > ::type;
+///////////////////////////////////////////////////////////////////////////////////////////
 
-template < typename T > using  add_rvalue_reference_t = typename add_rvalue_reference < T > ::type;
+template < typename T > using  remove_reference_t = typename remove_reference < T > ::type;
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
-    static_assert(std::is_same_v <      add_lvalue_reference_t < int > , int &  > );
+	static_assert(std::is_same_v <      remove_reference_t < int    > , int > );
 
-	static_assert(std::is_same_v <      add_rvalue_reference_t < int > , int && > );
+	static_assert(std::is_same_v <      remove_reference_t < int &  > , int > );
 
-//  --------------------------------------------------------------------------------
+	static_assert(std::is_same_v <      remove_reference_t < int && > , int > );
 
-    static_assert(std::is_same_v < std::add_lvalue_reference_t < int > , int &  > );
+//  ----------------------------------------------------------------------------
 
-	static_assert(std::is_same_v < std::add_rvalue_reference_t < int > , int && > );
+	static_assert(std::is_same_v < std::remove_reference_t < int    > , int > );
+
+	static_assert(std::is_same_v < std::remove_reference_t < int &  > , int > );
+
+	static_assert(std::is_same_v < std::remove_reference_t < int && > , int > );
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
