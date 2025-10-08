75519b6c8cea938cdee616a315f26fc964227112
Matthias <i.s.m.mipt@yandex.ru>
2025-10-08 22:03:06 +0300

    Добавлен пример 03.17.

--- projects/examples/source/03.29.cpp
+++ projects/examples/source/03.29.cpp
@@ -1,177 +1,31 @@
-////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
 
-#include <algorithm>
-#include <cstddef>
-#include <initializer_list>
 #include <iostream>
-#include <iterator>
-#include <utility>
 
-////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
 
-class Vector
+class Entity
 {
 public :
 
-	Vector() : m_array(nullptr), m_size(0) 
-	{
-		std::cout << "Vector::Vector (1)\n";
-	}
+	void test() const &  { std::cout << "Entity::test (1)\n"; }
 
-//  --------------------------------------------------------------------------------
-
-	Vector(std::initializer_list < int > list) : Vector()
-	{
-		std::cout << "Vector::Vector (2)\n";
-
-		m_array = (m_size = std::size(list)) ? new int[m_size]{} : nullptr;
-
-		std::ranges::copy(list, m_array);
-	}
-
-//  --------------------------------------------------------------------------------
-
-	Vector(Vector const & other) : Vector() 
-	{
-		std::cout << "Vector::Vector (3)\n";
-
-		m_array = (m_size = other.m_size) ? new int[m_size]{} : nullptr;
-
-		std::ranges::copy(other.m_array, other.m_array + other.m_size, m_array);
-	}
-
-//  --------------------------------------------------------------------------------
-
-	Vector(Vector && other)
-	: 
-		m_array(std::exchange(other.m_array, nullptr)), 
-		
-		m_size (std::exchange(other.m_size,  0      ))
-	{
-		std::cout << "Vector::Vector (4)\n";
-	}
-
-//  --------------------------------------------------------------------------------
-
-   ~Vector()
-	{
-		std::cout << "Vector::~Vector\n";
-
-		if (m_array) 
-		{
-			delete[] m_array;
-		}
-	}
-	
-//  --------------------------------------------------------------------------------
-
-//	auto & operator=(Vector const & other) // error
-//	{
-//		std::cout << "Vector::operator= (1)\n";
-//
-//		if (this != &other)
-//		{
-//			if (m_array)
-//			{
-//				delete[] m_array;
-//			}
-//
-//			m_array = (m_size = other.m_size) ? new int[m_size]{} : nullptr;
-//
-//			std::ranges::copy(other.m_array, other.m_array + other.m_size, m_array);
-//		}
-//
-//		return *this;
-//	}
-
-//  --------------------------------------------------------------------------------
-
-//	auto & operator=(Vector const & other) // bad
-//	{
-//		std::cout << "Vector::operator= (2)\n";
-//
-//		if (this != &other)
-//		{
-//			auto array = other.m_size ? new int[other.m_size]{} : nullptr;
-//
-//			std::ranges::copy(other.m_array, other.m_array + other.m_size, array);		
-//
-//			if (array = std::exchange(m_array, array); array)
-//			{
-//				delete[] array;
-//			}
-//
-//			m_size = other.m_size;
-//		}
-//
-//		return *this;
-//	}
-
-//  --------------------------------------------------------------------------------
-
-//	auto & operator=(Vector && other) // bad
-//	{
-//		std::cout << "Vector::operator= (3)\n";
-//
-//		if (this != &other)
-//		{
-//			if (m_array) 
-//			{
-//				delete[] m_array;
-//			}
-//			
-//			m_array = std::exchange(other.m_array, nullptr);
-//
-//			m_size  = std::exchange(other.m_size,  0      );
-//		}
-//
-//		return *this;
-//	}
-
-//  --------------------------------------------------------------------------------
-
-	auto & operator=(Vector other)
-	{
-		std::cout << "Vector::operator= (4)\n";
-
-		swap(other);
-
-		return *this;
-	}
-
-//  --------------------------------------------------------------------------------
-
-	void swap(Vector & other)
-	{
-		std::swap(m_array, other.m_array);
-		
-		std::swap(m_size,  other.m_size );
-	}
-
-private :
-
-	int * m_array = nullptr;
-
-	std::size_t m_size = 0;
+	void test() const && { std::cout << "Entity::test (2)\n"; }
 };
 
-////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+
+auto   make_entity_v1() {        Entity entity; return entity; }
+	  
+auto & make_entity_v2() { static Entity entity; return entity; }
+
+////////////////////////////////////////////////////////////////
 
 int main()
-{
-	Vector vector_1;
-
-	Vector vector_2 = { 1, 2, 3, 4, 5 };
-
-	Vector vector_3 = vector_2;
-
-	Vector vector_4 = std::move(vector_3);
-
-//  --------------------------------------
-
-	vector_3 = vector_2;
-
-	vector_4 = std::move(vector_3);
+{		
+	make_entity_v1().test();
+		
+	make_entity_v2().test();
 }
 
-////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
--- projects/examples/source/03.32.cpp
+++ projects/examples/source/03.32.cpp
@@ -1,74 +1,224 @@
-//////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <cassert>
 #include <cmath>
+#include <istream>
+#include <numeric>
+#include <ostream>
 #include <sstream>
+#include <vector>
 
-//////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
 
-#include <boost/rational.hpp>
+class Rational
+{
+public :
 
-//////////////////////////////////////////////////////////
+	/* explicit */ Rational(int num = 0, int den = 1) : m_num(num), m_den(den)
+	{
+		reduce();
+	}
+
+//  -------------------------------------------------------------------------------------------
+
+	explicit operator double() const
+	{ 
+		return 1.0 * m_num / m_den;
+	}
+
+//  -------------------------------------------------------------------------------------------
+
+	auto & operator+=(Rational const & other)
+	{
+		auto lcm = std::lcm(m_den, other.m_den);
+
+		m_num = m_num * (lcm / m_den) + other.m_num * (lcm / other.m_den);
+		
+		m_den = lcm;
+
+		reduce();
+
+		return *this;
+	}
+
+//  -------------------------------------------------------------------------------------------
+
+	auto & operator-=(Rational const & other) 
+	{ 
+		return *this += other.m_num * -1;
+	}
+
+//  -------------------------------------------------------------------------------------------
+
+	auto & operator*=(Rational const & other)
+	{
+		m_num *= other.m_num;
+
+		m_den *= other.m_den;
+
+		reduce();
+
+		return *this;
+	}
+
+//  -------------------------------------------------------------------------------------------
+	
+	auto & operator/=(Rational const & other) 
+	{ 
+		return *this *= Rational(other.m_den, other.m_num);
+	}
+
+//  -------------------------------------------------------------------------------------------
+
+	auto const operator++(int) { auto x = *this; *this += 1; return x; }
+
+	auto const operator--(int) { auto x = *this; *this -= 1; return x; }
+
+//  -------------------------------------------------------------------------------------------
+
+	auto & operator++() { *this += 1; return *this; }
+
+	auto & operator--() { *this -= 1; return *this; }
+
+//  -------------------------------------------------------------------------------------------
+
+	friend auto operator+ (Rational lhs, Rational const & rhs) { return lhs += rhs; }
+
+	friend auto operator- (Rational lhs, Rational const & rhs) { return lhs -= rhs; }
+
+	friend auto operator* (Rational lhs, Rational const & rhs) { return lhs *= rhs; }
+
+	friend auto operator/ (Rational lhs, Rational const & rhs) { return lhs /= rhs; }
+
+//  -------------------------------------------------------------------------------------------
+
+	friend auto operator< (Rational const & lhs, Rational const & rhs)
+	{
+		return lhs.m_num * rhs.m_den < rhs.m_num * lhs.m_den;
+	}
+
+//  -------------------------------------------------------------------------------------------
+
+	friend auto operator> (Rational const & lhs, Rational const & rhs) { return  (rhs < lhs); }
+	
+	friend auto operator<=(Rational const & lhs, Rational const & rhs) { return !(lhs > rhs); }
+
+	friend auto operator>=(Rational const & lhs, Rational const & rhs) { return !(lhs < rhs); }
+
+//  -------------------------------------------------------------------------------------------
+
+	friend auto operator==(Rational const & lhs, Rational const & rhs)
+	{
+		return !(lhs < rhs) && !(rhs < lhs);
+	}
+
+//  -------------------------------------------------------------------------------------------
+
+	friend auto & operator>>(std::istream & stream, Rational & rational)
+	{
+		return (stream >> rational.m_num).ignore() >> rational.m_den;
+	}
+
+//  -------------------------------------------------------------------------------------------
+
+	friend auto & operator<<(std::ostream & stream, Rational const & rational)
+	{
+		return stream << rational.m_num << '/' << rational.m_den;
+	}
+
+private :
+
+	void reduce()
+	{
+		auto gcd = std::gcd(m_num, m_den);
+
+		m_num /= gcd;
+
+		m_den /= gcd;
+	}
+
+//  -------------------------------------------------------------------------------------------
+
+	int m_num = 0, m_den = 1;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 auto equal(double x, double y, double epsilon = 1e-6)
 {
 	return std::abs(x - y) < epsilon;
 }
 
-//////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
-    boost::rational < int > x = 1, y = 2;
+	Rational x = 1, y(2, 1);
 
-//  ------------------------------------------------------
+//  -------------------------------------------------
 
-	assert(equal(boost::rational_cast < double > (x), 1));
+//	std::vector < int > vector = 1; // error
 
-//  ------------------------------------------------------
+//  -------------------------------------------------
 
-	assert((x += y) == boost::rational < int > (+3, 1));
-	
-	assert((x -= y) == boost::rational < int > (+1, 1));
+	assert(equal(static_cast < double > (x), 1));
 
-	assert((x *= y) == boost::rational < int > (+2, 1));
+//  -------------------------------------------------
 
-	assert((x /= y) == boost::rational < int > (+1, 1));
+//	assert(x.operator+=(y) == Rational(3, 1)); // bad
 
-//  ------------------------------------------------------
+//  -------------------------------------------------
 
-	assert((x ++  ) == boost::rational < int > (+1, 1));
+	assert((x += y) == Rational(+3, 1));
 
-	assert((x --  ) == boost::rational < int > (+2, 1));
+	assert((x -= y) == Rational(+1, 1));
 
-	assert((  ++ y) == boost::rational < int > (+3, 1));
+	assert((x *= y) == Rational(+2, 1));
 
-	assert((  -- y) == boost::rational < int > (+2, 1));
+	assert((x /= y) == Rational(+1, 1));
 
-//  ------------------------------------------------------
+//  -------------------------------------------------
 
-	assert((x +  y) == boost::rational < int > (+3, 1));
+	assert((x ++  ) == Rational(+1, 1));
 
-	assert((x -  y) == boost::rational < int > (-1, 1));
+	assert((x --  ) == Rational(+2, 1));
 
-	assert((x *  y) == boost::rational < int > (+2, 1));
+	assert((  ++ y) == Rational(+3, 1));
 
-	assert((x /  y) == boost::rational < int > (+1, 2));
+	assert((  -- y) == Rational(+2, 1));
 
-//  ------------------------------------------------------
+//  -------------------------------------------------
 
-	assert((x += 1) == boost::rational < int > (+2, 1));
+//	x++++; // error
 
-	assert((x +  1) == boost::rational < int > (+3, 1));
+//  -------------------------------------------------
 
-	assert((1 +  y) == boost::rational < int > (+3, 1));
+//	assert(operator+(x, y) == Rational(3, 1)); // bad
 
-	assert((1 +  1) == boost::rational < int > (+2, 1));
+//  -------------------------------------------------
 
-//  ------------------------------------------------------
+	assert((x +  y) == Rational(+3, 1));
+
+	assert((x -  y) == Rational(-1, 1));
+
+	assert((x *  y) == Rational(+2, 1));
+
+	assert((x /  y) == Rational(+1, 2));
+
+//  -------------------------------------------------
+
+	assert((x += 1) == Rational(+2, 1));
+
+	assert((x +  1) == Rational(+3, 1));
+
+	assert((1 +  y) == Rational(+3, 1));
+
+	assert((1 +  1) == Rational(+2, 1));
+
+//  -------------------------------------------------
 
 	assert((x <  y) == 0);
-
+	
 	assert((x >  y) == 0);
 
 	assert((x <= y) == 1);
@@ -79,21 +229,21 @@ int main()
 
 	assert((x != y) == 0);
 
-//  ------------------------------------------------------
+//  -------------------------------------------------
 
 	std::stringstream stream_1("1/2");
 
 	std::stringstream stream_2;
-
-//  ------------------------------------------------------
+	
+//  -------------------------------------------------
 
 	stream_1 >> x;
 
 	stream_2 << x;
 
-//  ------------------------------------------------------
+//  -------------------------------------------------
 
 	assert(stream_2.str() == stream_1.str());
 }
 
-//////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
