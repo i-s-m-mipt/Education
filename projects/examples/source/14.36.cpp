6c69597b5c7776da1efb0e20a5c2c820f64cc6ba
Matthias <i.s.m.mipt@yandex.ru>
2025-10-19 01:54:51 +0300

    Добавлен пример 14.36.

---
+++ projects/examples/source/14.36.cpp
@@ -0,0 +1,50 @@
+/////////////////////////////////////////////////////////////
+
+#include <atomic>
+#include <cassert>
+#include <thread>
+
+/////////////////////////////////////////////////////////////
+
+class Entity
+{
+public :
+
+    void test_v1()
+    {
+        m_x.store(true, std::memory_order::relaxed);
+
+        m_y.store(true, std::memory_order::release);
+    }
+
+//  ---------------------------------------------------------
+
+    void test_v2()
+    {
+        while (m_y.load(std::memory_order::acquire) == false)
+        {
+            std::this_thread::yield();
+        }
+
+        assert(m_x.load(std::memory_order::relaxed));
+    }
+
+private :
+
+    std::atomic < bool > m_x = false, m_y = false;
+};
+
+/////////////////////////////////////////////////////////////
+
+int main()
+{
+    Entity entity;
+
+//  --------------------------------------------------
+
+    std::jthread jthread_1(&Entity::test_v1, &entity);
+
+    std::jthread jthread_2(&Entity::test_v2, &entity);
+}
+
+/////////////////////////////////////////////////////////////
