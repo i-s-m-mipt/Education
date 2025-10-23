dcc7b18a1679b27ec9fa8e46ceef56203cd05297
Matthias <i.s.m.mipt@yandex.ru>
2025-10-23 19:15:47 +0300

    Доработаны примеры модуля 12.

--- projects/examples/source/12.28.cpp
+++ projects/examples/source/12.28.cpp
@@ -1,140 +1,75 @@
-///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
 #include <cassert>
 #include <iterator>
 #include <string_view>
 
-///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
+#include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/spirit/home/x3.hpp>
 
-///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
+struct Entity 
+{ 
+    int x = 0, y = 0;
+};
+
+/////////////////////////////////////////////////////////////////////
+
+BOOST_FUSION_ADAPT_STRUCT(Entity, x, y)
+
+/////////////////////////////////////////////////////////////////////
 
 namespace parser
 {
-    class Handler_v1 : public boost::spirit::x3::symbols < int >
-    {
-    public :
+    boost::spirit::x3::rule < struct rule_tag, Entity > rule;
 
-        Handler_v1()
-        {
-            add("C",    100);
-            add("CC",   200);
-            add("CCC",  300);
-            add("CD",   400);
-            add("D",    500);
-            add("DC",   600);
-            add("DCC",  700);
-            add("DCCC", 800);
-            add("CM",   900);
-        }
-    };
-
-//  -----------------------------------------------------------------------------------
-
-    class Handler_v2 : public boost::spirit::x3::symbols < int >
-    {
-    public :
-
-        Handler_v2()
-        {
-            add("X",    10);
-            add("XX",   20);
-            add("XXX",  30);
-            add("XL",   40);
-            add("L",    50);
-            add("LX",   60);
-            add("LXX",  70);
-            add("LXXX", 80);
-            add("XC",   90);
-        }
-    };
-    
-//  -----------------------------------------------------------------------------------
-
-    class Handler_v3 : public boost::spirit::x3::symbols < int >
-    {
-    public :
-
-        Handler_v3()
-        {
-            add("I",    1);
-            add("II",   2);
-            add("III",  3);
-            add("IV",   4);
-            add("V",    5);
-            add("VI",   6);
-            add("VII",  7);
-            add("VIII", 8);
-            add("IX",   9);
-        }
-    };
-
-//  -----------------------------------------------------------------------------------
-
-    boost::spirit::x3::rule < struct rule_tag, int > rule;
-
-//  -----------------------------------------------------------------------------------
-
-    using boost::spirit::x3::_val;
-
-    using boost::spirit::x3::_attr;
-
-//  -----------------------------------------------------------------------------------
-
-    auto lambda_1 = [](auto const & context){ _val(context) = 0; };
-
-    auto lambda_2 = [](auto const & context){ _val(context) += 1'000; };
-
-    auto lambda_3 = [](auto const & context){ _val(context) += _attr(context); };
-
-//  -----------------------------------------------------------------------------------
-
-    Handler_v1 handler_v1;
-    
-    Handler_v2 handler_v2;
-    
-    Handler_v3 handler_v3;
-
-//  -----------------------------------------------------------------------------------
+//  ---------------------------------------------------------
 
     auto rule_def = 
     (
-        boost::spirit::x3::eps[lambda_1] >> *boost::spirit::x3::char_('M')[lambda_2] >>
-        (
-            -handler_v1[lambda_3] >> 
-            
-            -handler_v2[lambda_3] >> 
-            
-            -handler_v3[lambda_3]
-        )
+        '{' 
+            >> boost::spirit::x3::int_ >> ','
+                
+            >> boost::spirit::x3::int_ >> 
+        '}'
     );
 
-//  -----------------------------------------------------------------------------------
+//  ---------------------------------------------------------
 
     BOOST_SPIRIT_DEFINE(rule);
 }
 
-///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
 auto parse(std::string_view view)
 {
     auto begin = std::begin(view), end = std::end(view);
 
-    auto space = boost::spirit::x3::ascii::space;
+    using parser::rule;
 
-    auto x = 0;
+    using boost::spirit::x3::ascii::space;
 
-    boost::spirit::x3::phrase_parse(begin, end, parser::rule, space, x);
-    
-    return x;
+    Entity entity;
+
+    boost::spirit::x3::phrase_parse(begin, end, rule, space, entity);
+
+    return entity;
 }
 
-///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
 int main()
 {
-    assert(parse("MCMLXX") == 1'970);
+    auto entity = parse("{ 1, 1 }");
+
+//  --------------------------------
+
+    assert(entity.x == 1);
+    
+    assert(entity.y == 1);
 }
 
-///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
