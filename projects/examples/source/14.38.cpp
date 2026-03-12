a2946f5be3d3b207b9e03bcecc38b9a1489f95d4
Matthias <i.s.m.mipt@yandex.ru>
2026-03-12 15:36:47 +0300

    Добавлен пример 14.38.

---
+++ projects/examples/source/14.38.cpp
@@ -0,0 +1,187 @@
+////////////////////////////////////////////////////////////////////////
+
+#include <atomic>
+#include <functional>
+#include <memory>
+#include <thread>
+#include <tuple>
+
+////////////////////////////////////////////////////////////////////////
+
+#include <boost/noncopyable.hpp>
+
+////////////////////////////////////////////////////////////////////////
+
+template < typename T > class Stack : private boost::noncopyable
+{
+private :
+
+    struct Node
+    {
+        std::shared_ptr < T > x;
+
+        Node * next = nullptr;
+    };
+
+public :
+
+    void push(T x)
+    {
+        Node * node = new Node(std::make_shared < T > (x), nullptr);
+
+        node->next = m_head.load();
+
+        while (!m_head.compare_exchange_weak(node->next, node));
+    }
+
+//  --------------------------------------------------------------------
+
+//  void top_and_pop_v1(T & x) // error
+//	{
+//		Node * head = m_head.load();
+//
+//		while (!m_head.compare_exchange_weak(head, head->next));
+//
+//		x = head->x;
+//	}
+
+//  --------------------------------------------------------------------
+
+//  auto top_and_pop_v2() // error
+//  {
+//      Node * head = m_head.load();
+//
+//      while (head && !m_head.compare_exchange_weak(head, head->next));
+//
+//      return head ? head->x : std::shared_ptr < T > ();
+//  }
+
+//  --------------------------------------------------------------------
+
+    auto top_and_pop_v3()
+    {
+        ++m_counter;
+
+        Node * head = m_head.load();
+
+        while (head && !m_head.compare_exchange_weak(head, head->next));
+
+        std::shared_ptr < T > x;
+
+        if (head)
+        {
+            x.swap(head->x);
+        }
+
+        try_clear(head);
+
+        return x;
+    }
+
+private :
+
+    void try_clear(Node * head)
+    {
+        if (m_counter == 1)
+        {
+            Node * tail = m_tail.exchange(nullptr);
+
+            if (!--m_counter)
+            {
+                clear(tail);
+            }
+            else if (tail)
+            {
+                save_nodes(tail);
+            }
+
+            delete head;
+        }
+        else
+        {
+            save_node(head);
+
+            --m_counter;
+        }
+    }
+
+//  --------------------------------------------------------------------
+
+    void clear(Node * nodes) const
+    {
+        while (nodes)
+        {
+            auto next = nodes->next;
+
+            delete nodes;
+
+            nodes = next;
+        }
+    }
+
+//  --------------------------------------------------------------------
+
+    void save_nodes(Node * nodes)
+    {
+        auto end = nodes;
+
+        while(auto next = end->next)
+        {
+            end = next;
+        }
+
+        save_nodes(nodes, end);
+    }
+
+//  --------------------------------------------------------------------
+
+    void save_nodes(Node * begin, Node * end)
+    {
+        end->next = m_tail;
+
+        while (!m_tail.compare_exchange_weak(end->next, begin));
+    }
+
+//  --------------------------------------------------------------------
+
+    void save_node(Node * node)
+    {
+        save_nodes(node, node);
+    }
+
+//  --------------------------------------------------------------------
+
+    std::atomic < Node * > m_head = nullptr, m_tail = nullptr;
+
+    std::atomic < std::size_t > m_counter = 0;
+};
+
+////////////////////////////////////////////////////////////////////////
+
+void top_and_pop(Stack < int > & stack)
+{
+    std::ignore = stack.top_and_pop_v3();
+}
+
+////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    Stack < int > stack;
+
+//  ---------------------------------------------------------
+    
+    stack.push(1);
+
+    stack.push(2);
+
+//  ---------------------------------------------------------
+
+    {
+        std::jthread jthread_1(top_and_pop, std::ref(stack));
+
+        std::jthread jthread_2(top_and_pop, std::ref(stack));
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
