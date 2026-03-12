6630bcb43ee541ff6dc7b3fba51ae65ef45422a6
Matthias <i.s.m.mipt@yandex.ru>
2026-03-12 19:52:17 +0300

    Добавлен пример 14.39.

---
+++ projects/examples/source/14.39.cpp
@@ -0,0 +1,103 @@
+////////////////////////////////////////////////////////////////////////////////////
+
+#include <atomic>
+#include <functional>
+#include <memory>
+#include <thread>
+#include <tuple>
+
+////////////////////////////////////////////////////////////////////////////////////
+
+#include <boost/noncopyable.hpp>
+
+////////////////////////////////////////////////////////////////////////////////////
+
+template < typename T > class Stack : private boost::noncopyable
+{
+private :
+
+    struct Node
+    {
+        std::shared_ptr < T > x;
+
+        std::atomic < std::shared_ptr < Node > > next;
+    };
+
+public :
+
+   ~Stack()
+    {
+        while (top_and_pop());
+    }
+
+//  --------------------------------------------------------------------------------
+
+    void push(T x)
+    {
+        auto node = std::make_shared < Node > (std::make_shared < T > (x), nullptr);
+
+        auto expected = m_head.load();
+
+        do
+        {
+            node->next = expected;
+        }
+        while (!m_head.compare_exchange_weak(expected, node));
+    }
+
+//  --------------------------------------------------------------------------------
+
+    auto top_and_pop()
+    {
+        auto head = m_head.load();
+
+        while (head && !m_head.compare_exchange_weak(head, head->next.load()));
+
+        if (head)
+        {
+            head->next = std::shared_ptr < Node > ();
+
+            return head->x;
+        }
+
+        return std::shared_ptr < T > ();
+    }
+
+private :
+
+    std::atomic < std::shared_ptr < Node > > m_head = nullptr;
+};
+
+////////////////////////////////////////////////////////////////////////////////////
+
+void top_and_pop(Stack < int > & stack)
+{
+    std::ignore = stack.top_and_pop();
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    static_assert(!std::atomic < std::shared_ptr < int > >::is_always_lock_free);
+
+//  -----------------------------------------------------------------------------
+
+    Stack < int > stack;
+
+//  -----------------------------------------------------------------------------
+    
+    stack.push(1);
+
+    stack.push(2);
+
+//  -----------------------------------------------------------------------------
+
+    {
+        std::jthread jthread_1(top_and_pop, std::ref(stack));
+
+        std::jthread jthread_2(top_and_pop, std::ref(stack));
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////
