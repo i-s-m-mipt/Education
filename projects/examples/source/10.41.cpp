23ad30995aa3047e84e5fdd2ab8553163ca42744
Matthias <i.s.m.mipt@yandex.ru>
2026-03-16 14:49:03 +0300

    Доработан пример 10.41.

--- projects/examples/source/10.41.cpp
+++ projects/examples/source/10.41.cpp
@@ -1,12 +1,12 @@
-////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 
 // chapter : Data Structures
 
-////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 
 // section : Associative Containers
 
-////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 
 // content : Associative Containers
 //
@@ -17,15 +17,67 @@
 // content : Object std::piecewise_construct
 //
 // content : Function std::forward_as_tuple
+//
+// content : Microbenchmarking
 
-////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 
 #include <map>
 #include <string>
 #include <tuple>
 #include <utility>
 
-////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+
+#include <benchmark/benchmark.h>
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+void test(benchmark::State & state) 
+{
+	auto argument = state.range(0);
+
+	std::map < int, std::string > map;
+
+	map.try_emplace(1, 1'000, 'a');
+
+    for (auto element : state)
+    {
+        switch (argument)
+		{
+			case 1 : { map.emplace(std::make_pair(1, std::string(1'000, 'a'))); break; }
+
+			case 2 : { map.emplace(               1, std::string(1'000, 'a') ); break; }
+
+			case 3 :
+			{ 
+				map.emplace
+				(
+					std::piecewise_construct,
+
+					std::forward_as_tuple(1),
+
+					std::forward_as_tuple(1'000, 'a')
+				);
+
+				break;
+			}
+
+			case 4 :
+			{
+				map.try_emplace(1, 1'000, 'a');
+			}
+		}
+
+        benchmark::DoNotOptimize(map);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+BENCHMARK(test)->Arg(1)->Arg(2)->Arg(3)->Arg(4);
+
+////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
@@ -40,7 +92,7 @@ int main()
 	map.emplace(std::make_pair(2, std::string(5, 'a')));
 
 //  ----------------------------------------------------
-	
+
 	map.emplace(3, std::string(5, 'a'));
 
 //  ----------------------------------------------------
@@ -48,19 +100,23 @@ int main()
 //	map.emplace(4, 5, 'a'); // error
 
 //  ----------------------------------------------------
-	
+
 	map.emplace
 	(
-		std::piecewise_construct, 
-			
-		std::forward_as_tuple(5), 
-		
+		std::piecewise_construct,
+
+		std::forward_as_tuple(5),
+
 		std::forward_as_tuple(5, 'a')
 	);
 
 //  ----------------------------------------------------
 
 	map.try_emplace(6, 5, 'a');
+
+//  ----------------------------------------------------
+
+	benchmark::RunSpecifiedBenchmarks();
 }
 
-////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
