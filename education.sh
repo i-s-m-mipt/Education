9e67a83559799ad608ace6eef02b2a79c7d40705
Matthias <i.s.m.mipt@yandex.ru>
2026-07-20 19:59:59 +0300

    Доработан файл education.sh.

--- education.sh
+++ education.sh
@@ -214,14 +214,14 @@ rm ../source/06.06.hpp.gch
 
 files="../source/06.16.cpp 06.13.o 06.14.o 06.15.o"
 
-g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -Wpedantic -O0 -xc++-system-header print
+g++ -c -std=c++23 -Wall -Wextra -Wpedantic -O0 -fmodules-ts -xc++-system-header print
 
-g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -Wpedantic -O0 ../source/06.15.cxx
+g++ -c -std=c++23 -Wall -Wextra -Wpedantic -O0 -fmodules-ts ../source/06.15.cxx
 
-g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -Wpedantic -O0 ../source/06.13.cxx
+g++ -c -std=c++23 -Wall -Wextra -Wpedantic -O0 -fmodules-ts ../source/06.13.cxx
 
-g++ -c -std=c++23 -fmodules-ts -Wall -Wextra -Wpedantic -O0 ../source/06.14.cxx
+g++ -c -std=c++23 -Wall -Wextra -Wpedantic -O0 -fmodules-ts ../source/06.14.cxx
 
-g++    -std=c++23 -fmodules-ts -Wall -Wextra -Wpedantic -O0 $files -o 06.16
+g++    -std=c++23 -Wall -Wextra -Wpedantic -O0 -fmodules-ts $files -o 06.16
 
 ##############################################################################################
